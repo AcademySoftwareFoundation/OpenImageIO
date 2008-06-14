7504a0aa4c0e2394240db6886c19e0510a234383
Larry Gritz <lg@larrygritz.com>
2008-06-14 15:19:53 +0000

    OpenGL
    git-svn-id: http://svn.openimageio.org/oiio/trunk@100 31904243-a364-42ac-a3db-9305396518ba

--- src/make/macosx.mk
+++ src/make/macosx.mk
@@ -19,6 +19,7 @@ LINK_QT := /Library/Frameworks/QtGui.framework/QtGui
 LINK_QT += /Library/Frameworks/QtOpenGL.framework/QtOpenGL
 LINK_QT += /Library/Frameworks/QtCore.framework/QtCore
 
+OPENGL_INCLUDE := 
 LINK_OPENGL := -L/System/Library/Frameworks/OpenGL.framework/Versions/A/Libraries/ -lGL
 
 
