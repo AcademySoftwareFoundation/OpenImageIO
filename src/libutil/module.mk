8c01b37123eec8a485b3b449d70fc0d7197a0a81
Larry Gritz <lg@larrygritz.com>
2008-07-15 17:45:52 +0000

    Add paramlist.cpp to compilation
    git-svn-id: http://svn.openimageio.org/oiio/trunk@147 31904243-a364-42ac-a3db-9305396518ba

--- src/libutil/module.mk
+++ src/libutil/module.mk
@@ -9,7 +9,8 @@ local_name := libutil
 
 # Name of all source files in this directory
 local_src := argparse.cpp filesystem.cpp \
-	     paramtype.cpp plugin.cpp strutil.cpp token.cpp
+	     paramlist.cpp paramtype.cpp \
+	     plugin.cpp strutil.cpp token.cpp ustring.cpp
 
 # Extra static libs needed to compile this binary (leave blank if this
 # module is not for a binary executable)
