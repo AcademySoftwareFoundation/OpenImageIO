38d330e3839fdb3e6d4177d4cd36d2c5b71216dd
Larry Gritz <lg@larrygritz.com>
2008-09-23 15:39:41 +0000

    Fix license now that there's no NV code left
    git-svn-id: http://svn.openimageio.org/oiio/trunk@235 31904243-a364-42ac-a3db-9305396518ba

--- src/include/typedesc.h
+++ src/include/typedesc.h
@@ -1,33 +1,26 @@
 /////////////////////////////////////////////////////////////////////////////
-// Copyright 2008 Larry Gritz.
-// All Rights Reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
+// Copyright (c) 2008 Larry Gritz
 // 
-// * Redistributions of source code must retain the above copyright
-//   notice, this list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the
-//   documentation and/or other materials provided with the distribution.
-// * Neither the name of NVIDIA nor the names of its contributors
-//   may be used to endorse or promote products derived from this software
-//   without specific prior written permission.
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
 // 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// (This is the Modified BSD License)
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+// (this is the MIT license)
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -109,6 +102,12 @@ struct DLLPUBLIC TypeDesc {
     /// "float", "int[5]", "normal"
     const char *c_str() const;
 
+    /// Return the number of elements: 1 if not an array, or the array
+    /// length.
+    size_t numelements () const {
+        return (arraylen >= 1 ? arraylen : 1);
+    }
+
     /// Return the size, in bytes, of this type.
     ///
     size_t size () const {
@@ -151,14 +150,6 @@ struct DLLPUBLIC TypeDesc {
     /// Demote the type to a non-array
     ///
     void unarray (void) { arraylen = 0; }
-
-    // Return the number of scalars comprising the type (e.g., 3
-    // for PT_POINT).  Return 0 for all types not comprised of floats.
-    // extern DLLPUBLIC int nscalars (ParamBaseType t);
-
-    // Return the number of floats comprising a ParamBaseType (e.g., 3 for
-    // PT_POINT).  Return 0 for all types not comprised of floats.
-    // extern DLLPUBLIC int nfloats (ParamBaseType t);
 
     static const TypeDesc TypeFloat, TypeColor, TypeString, TypeInt;
 };
