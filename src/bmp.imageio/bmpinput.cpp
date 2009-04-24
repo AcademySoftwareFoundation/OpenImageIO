14637a1db85df1690eea7e817758b0525443835a
Robert Matusewicz <matekm@gmail.com>
2009-04-24 08:11:14 +0000

    With this fix BmpInput read correct 4-, 8- nad 24-bit images that have scanlines padded with 0 to 4 byte boundary
    git-svn-id: http://svn.openimageio.org/oiio/trunk@625 31904243-a364-42ac-a3db-9305396518ba

--- src/bmp.imageio/bmpinput.cpp
+++ src/bmp.imageio/bmpinput.cpp
@@ -243,23 +243,40 @@ BmpInput::read_bmp_image4 (void)
 {
     ColorTable *colors_table = read_color_table ();
     // each byte store info about two pixels
-    const int scanline_size = m_dbmp->width >> 1;
-    const int data_size = scanline_size * m_dbmp->height;
-    std::vector<unsigned char> tmp_image (data_size);
-    fread (&tmp_image[0], 1, data_size, m_fd);
-    for (int x = 0; x < data_size; ++x) {
-        // choosing apprioprate color table and then converting
-        // color index to RGB value;
+    const int width = (m_spec.width % 2) ? (m_spec.width >> 1) + 1
+                                : m_spec.width >> 1;
+    const int pad_size = (width % 4) ? 4 - (width % 4) : 0;
+    const int scanline_size = width + pad_size;
+    std::vector<unsigned char> scanline (scanline_size);
+    for (int i = 0; i < m_spec.height; ++i) {
+        fread (&scanline[0], 1, scanline_size, m_fd);
         ColorTable *current_ct = NULL;
-        current_ct = &colors_table[(tmp_image[x] & 0xF0) >> 4];
-        m_buf[x * 6 + 0] = current_ct->red;
-        m_buf[x * 6 + 1] = current_ct->green;
-        m_buf[x * 6 + 2] = current_ct->blue;
-        // second pixel in this byte
-        current_ct = &colors_table[tmp_image[x] & 0x0F];
-        m_buf[x * 6 + 3] = current_ct->red;
-        m_buf[x * 6 + 4] = current_ct->green;
-        m_buf[x * 6 + 5] = current_ct->blue;
+        const int current_scanline_off = i * m_spec.width * m_spec.nchannels;
+        for (int j = 0; j < width - 1; ++j) {
+            // choosing apprioprate color table and then converting
+            // color index to RGB value;
+            current_ct = &colors_table[(scanline[j] & 0xF0) >> 4];
+            m_buf[current_scanline_off + j* 6] = current_ct->red;
+            m_buf[current_scanline_off + j* 6 + 1] = current_ct->green;
+            m_buf[current_scanline_off + j* 6 + 2] = current_ct->blue;
+            // second pixel in this byte
+            current_ct = &colors_table[scanline[j] & 0x0F];
+            m_buf[current_scanline_off + j* 6 + 3] = current_ct->red;
+            m_buf[current_scanline_off + j* 6 + 4] = current_ct->green;
+            m_buf[current_scanline_off + j* 6 + 5] = current_ct->blue;
+        }
+        // last two pixels
+        const int off = current_scanline_off + (width - 1) * 6;
+        current_ct = &colors_table[(scanline[width-1] & 0xF0) >> 4]; 
+        m_buf[off] = current_ct->red;
+        m_buf[off + 1] = current_ct->green;
+        m_buf[off + 2] = current_ct->blue;
+        if ( ! (m_spec.width % 2)){
+           current_ct = &colors_table[scanline[width-1] & 0x0F]; 
+           m_buf[off + 3] = current_ct->red;
+           m_buf[off + 4] = current_ct->green;
+           m_buf[off + 5] = current_ct->blue;
+        }
     }
     delete colors_table;
     return true;
@@ -271,19 +288,24 @@ bool
 BmpInput::read_bmp_image8 (void)
 {
     ColorTable *colors_table = read_color_table ();
-    // each byte store info about two pixels
-    const int scanline_size = m_dbmp->width;
-    const int data_size = scanline_size * m_dbmp->height;
-    std::vector<unsigned char> tmp_image (data_size);
-    fread (&tmp_image[0], 1, data_size, m_fd);
-    for (int x = 0; x < data_size; ++x) {
-        // choosing apprioprate color table and then converting
-        // color index to RGB value;
-        ColorTable *current_ct = NULL;
-        current_ct = &colors_table[(tmp_image[x])];
-        m_buf[x * 3 + 0] = current_ct->red;
-        m_buf[x * 3 + 1] = current_ct->green;
-        m_buf[x * 3 + 2] = current_ct->blue;
+    //scanlines are padded to 4 byte boundary
+    const int pad_size = (m_spec.width % 4) ? 4 - (m_spec.width % 4) : 0;
+    // each byte store info about one pixels
+    const int scanline_size = m_spec.width + pad_size;
+    std::vector<unsigned char> scanline (scanline_size);
+    ColorTable *current_ct = NULL;
+    for (int i = 0; i < m_spec.height; ++i) {
+        fread (&scanline[0], 1, scanline_size, m_fd);
+        // where to save current scanline in m_buf
+        const int current_scanline_off = i * m_spec.width * m_spec.nchannels;
+        for (int j = 0; j < m_spec.width; ++j) {
+            // choosing apprioprate color table and then converting
+            // color index to RGB value;
+            current_ct = &colors_table[scanline[j]];
+            m_buf[current_scanline_off + j * 3] = current_ct->red;
+            m_buf[current_scanline_off + j * 3 + 1] = current_ct->green;
+            m_buf[current_scanline_off + j * 3 + 2] = current_ct->blue;
+        }
     }
     delete colors_table;
     return true;
@@ -294,19 +316,29 @@ BmpInput::read_bmp_image8 (void)
 bool
 BmpInput::read_bmp_imageRGB (void)
 {
-    fseek(m_fd, m_hbmp.offset , SEEK_SET);
-    const int buf_size = m_dbmp->height * m_dbmp->width * m_spec.nchannels;
+    if (fseek(m_fd, m_hbmp.offset , SEEK_SET))
+        return false;
 
-    // Assuming pixel size of 24- (RGB) or 32-bits (RGBA)
-    fread(&m_buf[0], 1, buf_size, m_fd);
+    // scanlines are padded to 4 byte boudary
+    const int pad_size = (m_spec.width % 4) ? 4 - (m_spec.width % 4) : 0;
+    const int scanline_size = (m_spec.width + pad_size) * m_spec.nchannels;
+    std::vector<unsigned char> scanline (scanline_size);
+    //width of the image without padding
+    const int width = m_spec.width * m_spec.nchannels;
+    for (int i = 0; i < m_spec.height; ++i) {
+        fread (&scanline[0], 1, scanline_size, m_fd);
+        // In the disk, the pixel values are in BGR format (3 channels)
+        // or in BGRA (4 channels), so we need to convert to RGB or RGBA
+        const int current_scanline = i * width;
+        for (int j = 0; j < width; j += m_spec.nchannels) {
+            m_buf[current_scanline + j] = scanline[j+2];
+            m_buf[current_scanline + j + 1] = scanline[j+1];
+            m_buf[current_scanline + j + 2] = scanline[j];
 
-    // In the disk, the pixel values are in BGR format (3 channels)
-    // or in BGRA (4 channels), so we need to convert to RGB or RGBA
-    for (int j=0; j < buf_size; j+=m_spec.nchannels){
-        unsigned char aux = m_buf[j];
-        m_buf[j] = m_buf[j+2];
-        m_buf[j+2] = aux;
-        // if m_spec.nchannels==4 the alpha channel is in good place
+            // move this after 'for' loop ?
+            if (m_spec.nchannels == 4)
+                m_buf[current_scanline + j + 4] = scanline[j+4];
+        }
     }
     return true;
 }
