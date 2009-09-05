fb0aa7e1fd3f842012f805240f69b46264a9cec9
Larry Gritz <lg@larrygritz.com>
2009-09-05 04:57:55 +0000

    Fix to 'format' -- turns out that vsnprintf as we were using it requires C99 compliance, which VS on Windows does not have.  So we rewrite these routines to work for both varieties of vsnprintf behavior.
    git-svn-id: http://svn.openimageio.org/oiio/trunk@1195 31904243-a364-42ac-a3db-9305396518ba

--- src/libutil/ustring.cpp
+++ src/libutil/ustring.cpp
@@ -84,34 +84,30 @@ ustring::format (const char *fmt, ...)
     va_start (ap, fmt);
 
     // Allocate a buffer on the stack that's big enough for us almost
-    // all the time.
+    // all the time.  Be prepared to allocate dynamically if it doesn't fit.
     size_t size = 1024;
-    char buf[1024];
+    char stackbuf[1024];
+    std::vector<char> dynamicbuf;
+    char *buf = &stackbuf[0];
+    
+    while (1) {
+        // Try to vsnprintf into our buffer.
+        int needed = vsnprintf (buf, size, fmt, ap);
+        // NB. C99 (which modern Linux and OS X follow) says vsnprintf
+        // failure returns the length it would have needed.  But older
+        // glibc and current Windows return -1 for failure, i.e., not
+        // telling us how much was needed.
 
-    // Try to vsnprintf into our buffer.
-    va_list apcopy;
-#ifdef _WIN32
-    apcopy = ap;
-#else
-    va_copy (apcopy, ap);
-#endif
-    size_t needed = (size_t) vsnprintf (&buf[0], size, fmt, ap);
+        if (needed <= (int)size && needed >= 0) {
+            // It fit fine so we're done.
+            return ustring (buf);
+        }
 
-    if (needed <= size) {
-        // It fit fine the first time, we're done.
-        tok.assign (&buf[0]);
-    } else {
         // vsnprintf reported that it wanted to write more characters
-        // than we allotted.  So do a malloc of the right size and try again.
-        // This doesn't happen very often if we chose our initial size
-        // well.
-        std::vector <char> buf;
-        size = needed;
-        buf.resize (size);
-        needed = vsnprintf (&buf[0], size, fmt, apcopy);
-        tok.assign (&buf[0]);
+        // than we allotted.  So try again using a dynamic buffer.  This
+        // doesn't happen very often if we chose our initial size well.
+        size = (needed > 0) ? (needed+1) : (size*2);
+        dynamicbuf.resize (size);
+        buf = &dynamicbuf[0];
     }
-
-    va_end (ap);
-    return tok;
 }
