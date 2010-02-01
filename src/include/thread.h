8b1b8e5a3a5cbd64adfb79bb74e218916d20aab3
Larry Gritz <lg@larrygritz.com>
2010-02-01 04:18:46 +0000

    Windows fix: when including windows.h, use the extra macros to limit the stupid global defines it will introduce.
    git-svn-id: http://svn.openimageio.org/oiio/trunk@1386 31904243-a364-42ac-a3db-9305396518ba

--- src/include/thread.h
+++ src/include/thread.h
@@ -43,6 +43,8 @@
 // and std::numeric_limits<type>::min()/std::numeric_limits<type>::max()
 // when boost include windows.h
 #ifdef _WIN32
+# define WIN32_LEAN_AND_MEAN
+# define VC_EXTRALEAN
 # define NOMINMAX
 #endif
 
