ba0220adfb8e66827aca29043d582cdc4769ed2c
Leszek Godlewski <lg@inequation.org>
2011-08-15 21:39:47 +0200

    updated the test suite to use the new test image repository name

--- testsuite/texture-pointsample/run.py
+++ testsuite/texture-pointsample/run.py
@@ -13,7 +13,7 @@ sys.path = [".."] + sys.path
 import runtest
 
 # A command to run
-command = path + runtest.oiio_app("testtex") + " -width 0 ../../../oiio-testimages/grid.tx ; "
+command = path + runtest.oiio_app("testtex") + " -width 0 ../../../oiio-images/grid.tx ; "
 command = command + path + runtest.oiio_app("idiff") + " out.exr ref/out.exr > out.txt"
 # Outputs to check against references
 outputs = [  ]
