2f1cc0a64334f62f53d42f46b7a7882078f580b0
brechtvanlommel@gmail.com <brechtvanlommel@gmail.com>
2011-12-01 22:28:27 +0100

    Arbitrary unix platform detection, use uint64_t for imagesize_t.

--- src/include/imageio.h
+++ src/include/imageio.h
@@ -55,6 +55,7 @@
 #include <cmath>
 
 #include "export.h"
+#include "fmath.h"
 #include "typedesc.h"   /* Needed for TypeDesc definition */
 #include "paramlist.h"
 #include "colortransfer.h"
@@ -73,11 +74,7 @@ typedef ptrdiff_t stride_t;
 /// Type we use to express how many pixels (or bytes) constitute an image,
 /// tile, or scanline.  Needs to be large enough to handle very big images
 /// (which we presume could be > 4GB).
-#if defined(LINUX64) || defined(_WIN64) /* add others if we know for sure size_t is ok */
-typedef size_t imagesize_t;
-#else
-typedef unsigned long long imagesize_t;
-#endif
+typedef uint64_t imagesize_t;
 
 
 
