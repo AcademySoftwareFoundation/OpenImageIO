789e38859bead1620008495ee6d181effbc75d9f
Chris Foster <chris42f@gmail.com>
2012-04-17 23:47:20 +1000

    Use tinyformat for typesafe printf-like formatting
    All varargs error() functions are replaced here by an equivalent
    function using tfm::format to do the formatting instead.  An effort has
    been made to keep implementation details out of the headers by
    introducing private non-formatting functions like
    ImageInput::append_error() to be called by error().
    
    tfm::format has been brought into the Strutil namespace to replace
    Strutil::format().  Strutil::vformat() remains as-is, because it is
    useful in certain circumstances where external libraries pass a va_list.
    
    The error formatters inside ErrorHandler have not been changed yet,
    since it's not quite clear what to replace the va_list functions like
    ErrorHandler::vError() with.

--- src/libOpenImageIO/imageoutput.cpp
+++ src/libOpenImageIO/imageoutput.cpp
@@ -176,16 +176,13 @@ ImageOutput::send_to_client (const char *format, ...)
 
 
 void
-ImageOutput::error (const char *format, ...)
+ImageOutput::append_error (const std::string& message) const
 {
-    va_list ap;
-    va_start (ap, format);
     ASSERT (m_errmessage.size() < 1024*1024*16 &&
             "Accumulated error messages > 16MB. Try checking return codes!");
     if (m_errmessage.size())
         m_errmessage += '\n';
-    m_errmessage += Strutil::vformat (format, ap);
-    va_end (ap);
+    m_errmessage += message;
 }
 
 
