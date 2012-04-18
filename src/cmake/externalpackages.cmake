272f33d936cb854ccb6904a40f1f55073d439b86
Larry Gritz <lg@larrygritz.com>
2012-04-17 22:36:28 -0700

    Fix or suppress warnings with clang

--- src/cmake/externalpackages.cmake
+++ src/cmake/externalpackages.cmake
@@ -82,11 +82,9 @@ endmacro ()
 
 message (STATUS "BOOST_ROOT ${BOOST_ROOT}")
 
-set (Boost_ADDITIONAL_VERSIONS "1.45" "1.44" 
+set (Boost_ADDITIONAL_VERSIONS "1.49" "1.48" "1.47" "1.46" "1.45" "1.44" 
                                "1.43" "1.43.0" "1.42" "1.42.0" 
-                               "1.41" "1.41.0" "1.40" "1.40.0"
-                               "1.39" "1.39.0" "1.38" "1.38.0"
-                               "1.37" "1.37.0" "1.34.1" "1_34_1")
+                               "1.41" "1.41.0" "1.40" "1.40.0")
 if (LINKSTATIC)
     set (Boost_USE_STATIC_LIBS   ON)
 endif ()
@@ -96,7 +94,7 @@ if (BOOST_CUSTOM)
     # N.B. For a custom version, the caller had better set up the variables
     # Boost_VERSION, Boost_INCLUDE_DIRS, Boost_LIBRARY_DIRS, Boost_LIBRARIES.
 else ()
-    find_package (Boost 1.34 REQUIRED 
+    find_package (Boost 1.40 REQUIRED 
                   COMPONENTS filesystem regex system thread
                  )
     # Try to figure out if this boost distro has Boost::python.  If we
@@ -158,8 +156,12 @@ if (NOT oiio_boost_PYTHON_FOUND)
     set (PYTHONLIBS_FOUND OFF)
 endif ()
 
-include_directories ("${Boost_INCLUDE_DIRS}")
+include_directories (SYSTEM "${Boost_INCLUDE_DIRS}")
 link_directories ("${Boost_LIBRARY_DIRS}")
+
+#if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+#    add_definitions ("-Wno-parentheses")
+#endif ()
 
 # end Boost setup
 ###########################################################################
