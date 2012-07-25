abc1041511f4dab2627dccc060ede958b6758336
Stefan Stavrev <stavrevstefan@gmail.com>
2012-07-26 01:35:33 +0200

    histogram test

--- testsuite/oiiotool/run.py
+++ testsuite/oiiotool/run.py
@@ -19,12 +19,24 @@ command += (oiio_app ("oiiotool") + " "
             + parent + "/oiio-images/grid.tif"
             + " --resize 25% -o resize2.tif >> out.txt ;\n")
 
+command += (oiio_app ("oiiotool") + " "
+            + parent + "/oiio-images/grid.tif"
+            + " --histogram:cumulative=0 256x256 0 "
+            + "-o histogram_regular.tif >> out.txt ;\n")
+
+command += (oiio_app ("oiiotool") + " "
+            + parent + "/oiio-images/grid.tif"
+            + " --histogram:cumulative=1 256x256 0"
+            + "-o histogram_cumulative.tif >> out.txt ;\n")
+
 # To add more tests, just append more lines like the above and also add
 # the new 'feature.tif' (or whatever you call it) to the outputs list,
 # below.
 
 
 # Outputs to check against references
-outputs = [ "resize.tif", "resize2.tif", "out.txt" ]
+outputs = [ "resize.tif", "resize2.tif",
+            "histogram_regular.tif", "histogram_cumulative.tif",
+            "out.txt" ]
 
 #print "Running this command:\n" + command + "\n"
