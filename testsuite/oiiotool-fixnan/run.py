2821a0ba4c49894863e282aa5709deb3675686dc
Larry Gritz <lg@larrygritz.com>
2013-01-22 23:48:03 -0800

    Fix testsuite reference output

--- testsuite/oiiotool-fixnan/run.py
+++ testsuite/oiiotool-fixnan/run.py
@@ -4,9 +4,9 @@ command += (oiio_app ("oiiotool") +
                      " bad.exr --fixnan black -o black.exr >> out.txt ;\n")
 command += (oiio_app ("oiiotool") + 
                      " bad.exr --fixnan box3 -o box3.exr >> out.txt ;\n")
-command += info_command ("bad.exr", "--stats")
-command += info_command ("black.exr", "--stats")
-command += info_command ("box3.exr", "--stats")
+command += info_command ("bad.exr", "--stats", safematch=True)
+command += info_command ("black.exr", "--stats", safematch=True)
+command += info_command ("box3.exr", "--stats", safematch=True)
 
 # Outputs to check against references
 outputs = [ "black.exr", "box3.exr", "out.txt" ]
