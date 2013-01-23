4fbd1c1c8e114b82958a0a807ef9ae9002b5860f
Larry Gritz <lg@larrygritz.com>
2013-01-22 23:39:55 -0800

    Fix SHA1.cpp build problem on some platforms -- issue #473

--- src/libutil/SHA1.cpp
+++ src/libutil/SHA1.cpp
@@ -8,9 +8,9 @@
 
 // If compiling with MFC, you might want to add #include "StdAfx.h"
 
+#include "SHA1.h"
 #include "hash.h"
 #include "dassert.h"
-#include "SHA1.h"
 
 #ifdef SHA1_UTILITY_FUNCTIONS
 #define SHA1_MAX_FILE_BUFFER 8000
