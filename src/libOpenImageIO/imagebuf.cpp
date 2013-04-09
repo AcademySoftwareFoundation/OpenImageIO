0effe4589fdbf991c2bd863fde3a9aa03ea1916e
Larry Gritz <lg@larrygritz.com>
2013-04-08 22:05:58 -0700

    Bug fix in ImageBuf::interppixel -- wrong size alloca

--- src/libOpenImageIO/imagebuf.cpp
+++ src/libOpenImageIO/imagebuf.cpp
@@ -1067,7 +1067,7 @@ interppixel_ (const ImageBuf &img, float x, float y, float *pixel,
               ImageBuf::WrapMode wrap)
 {
     int n = img.spec().nchannels;
-    float *localpixel = ALLOCA (float, n);
+    float *localpixel = ALLOCA (float, n*4);
     float *p[4] = { localpixel, localpixel+n, localpixel+2*n, localpixel+3*n };
     x -= 0.5f;
     y -= 0.5f;
