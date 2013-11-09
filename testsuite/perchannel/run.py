56965e11f03dc994849bd7fe64e49d29a64975ef
Larry Gritz <lg@larrygritz.com>
2013-11-08 23:07:50 -0800

    Fix openexr-images directory path

--- testsuite/perchannel/run.py
+++ testsuite/perchannel/run.py
@@ -5,7 +5,7 @@
 
 # Few image formats support per-channel formats, but OpenEXR is one
 # of them, and the OpenEXR test images contains one such image:
-image = parent + "/openexr-images-1.5.0/ScanLines/Blobbies.exr"
+image = parent + "/openexr-images/ScanLines/Blobbies.exr"
 
 # iconvert reads and writes native format, test that.
 command += (oiio_app("iconvert") + image + " blob1.exr ;\n")
@@ -18,7 +18,7 @@ command += diff_command (image, "blob2.exr")
 
 
 # Now we do a similar test for a tiled OpenEXR file with per-channel formats
-image = parent + "/openexr-images-1.5.0/Tiles/Spirals.exr"
+image = parent + "/openexr-images/Tiles/Spirals.exr"
 
 # iconvert reads and writes native format, test that.
 command += (oiio_app("iconvert") + image + " spiral1.exr ;\n")
