b6295a7b5fadd9cf09a782d6fb23eb1c5de60b37
Larry Gritz <lg@larrygritz.com>
2014-05-10 11:55:47 -0700

    Our own simplified FindFreetype replaces buggy cmake version. Finally add a test for oiiotool --text to be sure we don't break it again.

---
+++ testsuite/oiiotool-text/run.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+# test --text
+command += oiiotool ("--create 320x240 3 --text:x=25:y=50 'Hello, world' "
+            "--text:x=50:y=100:font=Arial:color=1,0,0:size=30 \"Go Big Red!\" "
+            "-d uint8 -o text.tif >> out.txt")
+
+
+# To add more tests, just append more lines like the above and also add
+# the new 'feature.tif' (or whatever you call it) to the outputs list,
+# below.
+
+
+# Outputs to check against references
+outputs = [ "text.tif" ]
+
+#print "Running this command:\n" + command + "\n"
