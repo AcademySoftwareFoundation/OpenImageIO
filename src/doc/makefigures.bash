74f02a2ca4a28c1338c2e8fc4967d3ac9bd8a489
Larry Gritz <lg@larrygritz.com>
2014-06-06 11:54:35 -0700

    Make figures using this tree's built oiiotool rather than from the /usr/local/cuda/bin:/usr/local/opt/ccache/libexec:/Users/lg/work/osl/osl.lg/dist/macosx/bin:/Users/lg/bin:/Users/lg/lg/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/texbin

--- src/doc/makefigures.bash
+++ src/doc/makefigures.bash
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-oiiotool ../../../../oiio-images/tahoe-gps.jpg --colorconvert sRGB linear --resize 320x240 --colorconvert linear sRGB -o tahoe-small.jpg
-oiiotool ../../../../oiio-images/grid.tif --resize 256x256 --colorconvert linear sRGB -o grid-small.jpg
+if [ ${OIIOTOOL} == "" ] ; then
+    OIIOTOOL=oiiotool
+fi
 
-oiiotool tahoe-small.jpg --tocolorspace linear --cadd 0.2 --tocolorspace sRGB -o cadd.jpg
-oiiotool tahoe-small.jpg --tocolorspace linear --cmul 0.5 --tocolorspace sRGB -o cmul.jpg
-oiiotool tahoe-small.jpg --tocolorspace linear --chsum:weight=.2126,.7152,.0722 --ch 0,0,0 --tocolorspace sRGB -o luma.jpg
-oiiotool grid-small.jpg --flip -o flip.jpg
-oiiotool grid-small.jpg --flop -o flop.jpg
-oiiotool grid-small.jpg --flipflop -o flipflop.jpg
-oiiotool grid-small.jpg --transpose -o transpose.jpg
-oiiotool grid-small.jpg --cshift +70+30 -o cshift.jpg
-oiiotool --pattern constant:color=1,0.7,0.7 320x240 3 --fill:color=1,0,0 50x100+50+75 --tocolorspace sRGB -o fill.jpg
-oiiotool --pattern checker:color1=0.1,0.1,0.1:color2=0.4,0.4,0.4:width=32:height=32 320x240 3 --tocolorspace sRGB -o checker.jpg
-oiiotool --create 320x240 3 --text:x=25:y=50 "Hello, world" \
+echo "Using OIIOTOOL=${OIIOTOOL}"
+
+${OIIOTOOL} ../../../../oiio-images/tahoe-gps.jpg --colorconvert sRGB linear --resize 320x240 --colorconvert linear sRGB -o tahoe-small.jpg
+${OIIOTOOL} ../../../../oiio-images/grid.tif --resize 256x256 --colorconvert linear sRGB -o grid-small.jpg
+
+${OIIOTOOL} tahoe-small.jpg --tocolorspace linear --cadd 0.2 --tocolorspace sRGB -o cadd.jpg
+${OIIOTOOL} tahoe-small.jpg --tocolorspace linear --cmul 0.5 --tocolorspace sRGB -o cmul.jpg
+${OIIOTOOL} tahoe-small.jpg --tocolorspace linear --chsum:weight=.2126,.7152,.0722 --ch 0,0,0 --tocolorspace sRGB -o luma.jpg
+${OIIOTOOL} grid-small.jpg --flip -o flip.jpg
+${OIIOTOOL} grid-small.jpg --flop -o flop.jpg
+${OIIOTOOL} grid-small.jpg --flipflop -o flipflop.jpg
+${OIIOTOOL} grid-small.jpg --transpose -o transpose.jpg
+${OIIOTOOL} grid-small.jpg --cshift +70+30 -o cshift.jpg
+${OIIOTOOL} --pattern constant:color=1,0.7,0.7 320x240 3 --fill:color=1,0,0 50x100+50+75 --tocolorspace sRGB -o fill.jpg
+${OIIOTOOL} --pattern checker:color1=0.1,0.1,0.1:color2=0.4,0.4,0.4:width=32:height=32 320x240 3 --tocolorspace sRGB -o checker.jpg
+${OIIOTOOL} --create 320x240 3 --text:x=25:y=50 "Hello, world" \
             --text:x=50:y=100:font="Arial Bold":color=1,0,0:size=30 "Go Big Red!" --tocolorspace sRGB -o text.jpg
-#oiiotool ../../../testsuite/oiiotool/tahoe-small.tif
+#${OIIOTOOL} ../../../testsuite/oiiotool/tahoe-small.tif
 
