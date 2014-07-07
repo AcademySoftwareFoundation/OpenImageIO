5c97412e6983ca61052f306f62d2a67272a3e41b
Larry Gritz <lg@larrygritz.com>
2014-07-06 18:32:24 -0700

    Fix compilation problem -- shouldn't return m_file as a bool, use m_file.good() instead.

--- src/psd.imageio/psdinput.cpp
+++ src/psd.imageio/psdinput.cpp
@@ -389,7 +389,7 @@ private:
         // For debugging, numeric_cast will throw if precision is lost:
         // value = boost::numeric_cast<TVariable>(buffer);
         value = buffer;
-        return m_file;
+        return m_file.good();
     }
 
     int read_pascal_string (std::string &s, uint16_t mod_padding);
