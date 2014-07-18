a99f9c9d7a9c2f58c4c5d5ed6b116284c077c4f5
YangYangTL <yang.yang@topazlabs.com>
2014-07-18 14:48:27 -0500

    fix indent

--- src/jpeg.imageio/jpeginput.cpp
+++ src/jpeg.imageio/jpeginput.cpp
@@ -158,22 +158,18 @@ JpgInput::open (const std::string &name, ImageSpec &newspec)
     }
 
     // Check magic number to assure this is a JPEG file
-
     uint8_t magic[2] = {0, 0};
     if (fread (magic, sizeof(magic), 1, m_fd) != 1) {
         error ("Empty file \"%s\"", name.c_str());
         close_file ();
-
         return false;
     }
 
     rewind (m_fd);
     if (magic[0] != JPEG_MAGIC1 || magic[1] != JPEG_MAGIC2) {
-
         close_file ();
         error ("\"%s\" is not a JPEG file, magic number doesn't match (was 0x%x)",
                name.c_str(), magic);
-
         return false;
     }
 
