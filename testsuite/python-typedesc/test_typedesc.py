7bf086d2983e4f67c879e9fe39fceca0f28df841
Larry Gritz <lg@larrygritz.com>
2015-04-05 18:21:39 -0700

    Add TypeDesc::TypeHalf (pre-assembled 'half' type descriptor)

--- testsuite/python-typedesc/test_typedesc.py
+++ testsuite/python-typedesc/test_typedesc.py
@@ -138,6 +138,7 @@ try:
     breakdown_test (oiio.TypeDesc.TypeTimeCode, "TypeTimeCode", verbose=False)
     breakdown_test (oiio.TypeDesc.TypeKeyCode,  "TypeKeyCode",  verbose=False)
     breakdown_test (oiio.TypeDesc.TypeFloat4,   "TypeFloat4",   verbose=False)
+    breakdown_test (oiio.TypeDesc.TypeHalf,     "TypeHalf",     verbose=False)
     print
 
     print "Done."
