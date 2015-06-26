78dc6128dae3c9d4589fcc5585648d17c76e01ac
sambler <Develop@Shaneware.Biz>
2015-06-27 00:49:09 +0930

    Separate test for SSE3 and SSSE3 nocona cpu's have SSE3 without SSSE3 which breaks by including tmmintrin.h

--- src/include/OpenImageIO/simd.h
+++ src/include/OpenImageIO/simd.h
@@ -52,8 +52,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #if (defined(__SSE2__) || (_MSC_VER >= 1300 && !_M_CEE_PURE)) && !defined(OIIO_NO_SSE)
 #  include <xmmintrin.h>
 #  include <emmintrin.h>
-#  if defined(__SSE3__) || defined(__SSSE3__)
+#  if defined(__SSE3__)
 #    include <pmmintrin.h>
+#  endif
+#  if defined(__SSSE3__)
 #    include <tmmintrin.h>
 #  endif
 #  if (defined(__SSE4_1__) || defined(__SSE4_2__))
