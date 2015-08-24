5d05f1247fc702ab52c511cc397dc2be716c935e
Larry Gritz <lg@larrygritz.com>
2015-08-24 11:58:07 -0700

    Make maketx reject input images that have overscan.
    It's hard to know how to handle them sensibly. Because display and data
    window origins and sizes are integers (in all file formats we know of),
    there's no way to downsize them for MIP levels and preserve that
    property. This makes for misalignments or bleeding at edges or other
    undesirable behavior.  Rather than have people perplexed at the results,
    just have maketx reject overscan files.
    
    Skip texture-overscan and maketx-cropover tests, we don't expect them to work.

--- testsuite/texture-cropover/run.py
+++ testsuite/texture-cropover/run.py
@@ -2,6 +2,9 @@
 
 # Tests input image which is partial crop, partial overscan!
 
+# Skip this test. For now, we don't support overscan as texture input.
+exit ()
+
 command += oiiotool(parent + "/oiio-images/grid.tif " +
                     "--crop 500x1000+250+0 --fullsize 1000x800+0+100 -o grid-cropover.exr")
 command += maketx_command ("grid-cropover.exr",
