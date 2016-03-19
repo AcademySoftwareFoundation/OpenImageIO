27f17211c61bf1891f51629e79d029f45f4bf598
Larry Gritz <lg@larrygritz.com>
2016-03-18 21:41:23 -0400

    ImageBuf: be kinder to memory when writing huge files from ImageCache.
    When writing an IB to disk, look out for situations where the IB is
    backed by ImageCache rather than in local RAM. In the IC case, the old
    method of doing a cache->get_pixels() of the whole image, followed by an
    out->write_image() is rather wasteful for truly huge files because it
    will force the whole image into memory. Instead, if the file is
    IC-backed and bigger than a reasonable amount, read and write it in
    strips of scanlines or tiles.

--- src/libOpenImageIO/imagebuf.cpp
+++ src/libOpenImageIO/imagebuf.cpp
@@ -945,22 +945,74 @@ ImageBuf::write (ImageOutput *out,
     bool ok = true;
     const ImageBufImpl *impl = this->impl();
     ok &= impl->validate_pixels ();
-    const ImageSpec &m_spec (impl->m_spec);
+    const ImageSpec &bufspec (impl->m_spec);
+    const ImageSpec &outspec (out->spec());
+    TypeDesc bufformat = spec().format;
     if (impl->m_localpixels) {
         // In-core pixel buffer for the whole image
-        ok = out->write_image (m_spec.format, impl->m_localpixels, as, as, as,
+        ok = out->write_image (bufformat, impl->m_localpixels, as, as, as,
                                progress_callback, progress_callback_data);
     } else if (deep()) {
         // Deep image record
         ok = out->write_deep_image (impl->m_deepdata);
     } else {
-        // Backed by ImageCache
-        boost::scoped_array<char> tmp (new char [m_spec.image_bytes()]);
-        ok = get_pixels (roi(), m_spec.format, &tmp[0]);
-        ok &= out->write_image (m_spec.format, &tmp[0], as, as, as,
-                                progress_callback, progress_callback_data);
-        // FIXME -- not good for huge images.  Instead, we should read
-        // little bits at a time (scanline or tile blocks).
+        // The image we want to write is backed by ImageCache -- we must be
+        // immediately writing out a file from disk, possibly with file
+        // format or data format conversion, but without any ImageBufAlgo
+        // functions having been applied.
+        const imagesize_t budget = 1024*1024*64; // 64 MB
+        imagesize_t imagesize = bufspec.image_bytes();
+        if (imagesize <= budget) {
+            // whole image can fit within our budget
+            boost::scoped_array<char> tmp (new char [imagesize]);
+            ok &= get_pixels (roi(), bufformat, &tmp[0]);
+            ok &= out->write_image (bufformat, &tmp[0], as, as, as,
+                                    progress_callback, progress_callback_data);
+        } else if (outspec.tile_width) {
+            // Big tiled image: break up into tile strips
+            size_t pixelsize = bufspec.pixel_bytes();
+            size_t chunksize = pixelsize * outspec.width * outspec.tile_height * outspec.tile_depth;
+            boost::scoped_array<char> tmp (new char [chunksize]);
+            for (int z = 0;  z < outspec.depth;  z += outspec.tile_depth) {
+                int zend = std::min (z+outspec.z+outspec.tile_depth,
+                                     outspec.z+outspec.depth);
+                for (int y = 0;  y < outspec.height && ok;  y += outspec.tile_height) {
+                    int yend = std::min (y+outspec.y+outspec.tile_height,
+                                         outspec.y+outspec.height);
+                    ok &= get_pixels (ROI(outspec.x, outspec.x+outspec.width,
+                                          outspec.y+y, yend,
+                                          outspec.z+z, zend),
+                                      bufformat, &tmp[0]);
+                    ok &= out->write_tiles (outspec.x, outspec.x+outspec.width,
+                                            y+outspec.y, yend, z+outspec.z, zend,
+                                            bufformat, &tmp[0]);
+                    if (progress_callback &&
+                        progress_callback (progress_callback_data,
+                                           (float)(z*outspec.height+y)/(outspec.height*outspec.depth)))
+                        return ok;
+                }
+            }
+        } else {
+            // Big scanline image: break up into scanline strips
+            imagesize_t slsize = bufspec.scanline_bytes();
+            int chunk = clamp (round_to_multiple(budget/slsize, 64), 1, 1024);
+            boost::scoped_array<char> tmp (new char [chunk*slsize]);
+            for (int z = 0;  z < outspec.depth;  ++z) {
+                for (int y = 0;  y < outspec.height && ok;  y += chunk) {
+                    int yend = std::min (y+outspec.y+chunk, outspec.y+outspec.height);
+                    ok &= get_pixels (ROI(outspec.x, outspec.x+outspec.width,
+                                          outspec.y+y, yend,
+                                          outspec.z, outspec.z+outspec.depth),
+                                      bufformat, &tmp[0]);
+                    ok &= out->write_scanlines (y+outspec.y, yend, z+outspec.z,
+                                                bufformat, &tmp[0]);
+                    if (progress_callback &&
+                        progress_callback (progress_callback_data,
+                                           (float)(z*outspec.height+y)/(outspec.height*outspec.depth)))
+                        return ok;
+                }
+            }
+        }
     }
     if (! ok)
         error ("%s", out->geterror ());
