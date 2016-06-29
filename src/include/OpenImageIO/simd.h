ba950bb2d71aef91e3700615b7f0db9ed4734449
Michel Lerenard <michelisotropix@gmail.com>
2016-06-28 17:05:29 -0700

    Compilation / warning / windows fixes (1443)

--- src/include/OpenImageIO/simd.h
+++ src/include/OpenImageIO/simd.h
@@ -49,6 +49,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <OpenImageIO/platform.h>
 #include <OpenEXR/ImathVec.h>
 
+#if defined(_MSC_VER)
+#include <algorithm>
+#endif
+
 #if (defined(__SSE2__) || (_MSC_VER >= 1300 && !_M_CEE_PURE)) && !defined(OIIO_NO_SSE)
 #if OIIO_GNUC_VERSION >= 40400 || __clang__
 #  include <immintrin.h>
