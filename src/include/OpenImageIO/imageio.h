910d744314be77c746c568e9b429d4d2b5ceeb79
marsupial <marsupial@users.noreply.github.com>
2017-01-20 18:35:11 -0500

    Fix declare_imageio_format declaration. (#1609)

--- src/include/OpenImageIO/imageio.h
+++ src/include/OpenImageIO/imageio.h
@@ -1324,7 +1324,8 @@ OIIO_API void declare_imageio_format (const std::string &format_name,
                                       ImageInput::Creator input_creator,
                                       const char **input_extensions,
                                       ImageOutput::Creator output_creator,
-                                      const char **output_extensions);
+                                      const char **output_extensions,
+                                      const char *lib_version);
 
 
 /// Helper function: convert contiguous arbitrary data between two
