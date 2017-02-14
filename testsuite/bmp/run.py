1d5a25f004357923fb806802817fad777766a598
Simon Boorer <sboorer@localhost.localdomain>
2017-02-15 00:54:50 +1100

    BMP: Added support for BITMAPV5HEADER (version 5 of the format header) (1616)

--- testsuite/bmp/run.py
+++ testsuite/bmp/run.py
@@ -5,3 +5,6 @@ files = [ "g01bg.bmp", "g04.bmp", "g08.bmp",
           "g16bf555.bmp", "g24.bmp", "g32bf.bmp" ]
 for f in files :
     command += rw_command (imagedir, f)
+
+# Test BMR version 5
+command += rw_command ("src", "g01bg2-v5.bmp")
