b9b8b00ce23839419661f2704dc87fa401edcc3f
Larry Gritz <lg@larrygritz.com>
2017-08-09 16:21:52 -0700

    RLA bug fix: potential buffer overrun -- AspectRatio[] only 8 chars wide

--- src/rla.imageio/rlaoutput.cpp
+++ src/rla.imageio/rlaoutput.cpp
@@ -377,7 +377,7 @@ RLAOutput::open (const std::string &name, const ImageSpec &userspec,
     // I think it's safe not to care until someone complains
     STRING_FIELD (Aspect, "rla:Aspect");
 
-    snprintf (m_rla.AspectRatio, sizeof(m_rla.AspectRatio), "%.10f",
+    snprintf (m_rla.AspectRatio, sizeof(m_rla.AspectRatio), "%.6f",
         m_spec.get_float_attribute ("PixelAspectRatio", 1.f));
     Strutil::safe_strcpy (m_rla.ColorChannel, m_spec.get_string_attribute ("rla:ColorChannel",
         "rgb"), sizeof(m_rla.ColorChannel));
