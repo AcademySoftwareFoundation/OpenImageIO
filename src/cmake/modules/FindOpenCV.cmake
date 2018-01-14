64aeb00c3a3fe1f2ef8343256622bdbfe7813912
Richard Shaw <hobbes1069@gmail.com>
2018-01-14 07:51:38 -0600

    Linking with OpenCV highgui componenet is not needed for 3.X and newer.

--- src/cmake/modules/FindOpenCV.cmake
+++ src/cmake/modules/FindOpenCV.cmake
@@ -51,8 +51,10 @@ set (libdirs "${PROJECT_SOURCE_DIR}/lib"
              )
 
 
-set (opencv_components opencv_highgui opencv_imgproc opencv_core)
+set (opencv_components opencv_imgproc opencv_core)
 if (NOT ${OpenCV_VERSION} VERSION_LESS 3.0.0)
+    set (opencv_components opencv_videoio ${opencv_components})
+else (NOT ${OpenCV_VERSION} VERSION_LESS 3.0.0)
     set (opencv_components opencv_videoio ${opencv_components})
 endif ()
 foreach (component ${opencv_components})
