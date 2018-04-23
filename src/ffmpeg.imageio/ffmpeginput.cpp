3c9bd27c79711da39f7f69d3dc022bea65b2757f
Larry Gritz <lg@larrygritz.com>
2018-04-23 15:50:14 -0700

    Changes to allow building against the new ffmpeg 4.0 (#1926)

--- src/ffmpeg.imageio/ffmpeginput.cpp
+++ src/ffmpeg.imageio/ffmpeginput.cpp
@@ -124,6 +124,15 @@ inline int receive_frame(AVCodecContext *avctx, AVFrame *picture,
 }
 #endif
 
+
+// Changes for ffmpeg 4.0
+#define USE_FFMPEG_4_0 (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(58, 18, 100))
+
+#if USE_FFMPEG_4_0
+#  define CODEC_CAP_DELAY AV_CODEC_CAP_DELAY
+#endif
+
+
 #include <boost/thread/once.hpp>
 
 #include <OpenImageIO/imageio.h>
