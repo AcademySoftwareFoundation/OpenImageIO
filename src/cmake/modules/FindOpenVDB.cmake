803ece56bb5e1c6660c7d5854878eda6cdb24fbd
Larry Gritz <lg@larrygritz.com>
2018-09-24 21:27:00 -0700

    Fix builds on OSX
    The original FindOpenVDB.cmake was unreliable. Replaced with one found
    elsewhere on the net (from Blender, bearing a BSD license).

--- src/cmake/modules/FindOpenVDB.cmake
+++ src/cmake/modules/FindOpenVDB.cmake
@@ -1,96 +1,73 @@
-# Copyright (c) 2012-2016 DreamWorks Animation LLC
+# - Find OPENVDB library
+# Find the native OPENVDB includes and library
+# This module defines
+#  OPENVDB_INCLUDE_DIRS, where to find openvdb.h, Set when
+#                            OPENVDB_INCLUDE_DIR is found.
+#  OPENVDB_LIBRARIES, libraries to link against to use OPENVDB.
+#  OPENVDB_ROOT_DIR, The base directory to search for OPENVDB.
+#                        This can also be an environment variable.
+#  OPENVDB_FOUND, If false, do not try to use OPENVDB.
 #
-# All rights reserved. This software is distributed under the
-# Mozilla Public License 2.0 ( http://www.mozilla.org/MPL/2.0/ )
+# also defined, but not for general use are
+#  OPENVDB_LIBRARY, where to find the OPENVDB library.
+
+#=============================================================================
+# Copyright 2015 Blender Foundation.
 #
-# Redistributions of source code must retain the above copyright
-# and license notice and the following restrictions and disclaimer.
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
 #
-# *     Neither the name of DreamWorks Animation nor the names of
-# its contributors may be used to endorse or promote products derived
-# from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-# IN NO EVENT SHALL THE COPYRIGHT HOLDERS' AND CONTRIBUTORS' AGGREGATE
-# LIABILITY FOR ALL CLAIMS REGARDLESS OF THEIR BASIS EXCEED US$250.00.
-#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
 
-# -*- cmake -*-
-# - Find OpenVDB
-#
-# Author : Fredrik Salomonsson fredriks@d2.com
-#
-# OpenVDB_FOUND                 Set if OpenVDB is found.
-# OpenVDB_INCLUDE_DIR           OpenVDB's include directory
-# OpenVDB_LIBRARY_DIR           OpenVDB's library directory
-# OpenVDB_<C>_LIBRARY           Specific openvdb library (<C> is upper-case)
-# OpenVDB_LIBRARIES             All openvdb libraries
-# OpenVDB_MAJOR_VERSION         Major version number
-# OpenVDB_MINOR_VERSION         Minor version number
-# OpenVDB_PATCH_VERSION         Patch version number
-#
-# This module read hints about search locations from variables::
-#
-# OPENVDB_ROOT                  Preferred installtion prefix
+# If OPENVDB_ROOT_DIR was defined in the environment, use it.
+IF(NOT OPENVDB_ROOT_DIR AND NOT $ENV{OPENVDB_ROOT_DIR} STREQUAL "")
+  SET(OPENVDB_ROOT_DIR $ENV{OPENVDB_ROOT_DIR})
+ENDIF()
 
-FIND_PACKAGE( PackageHandleStandardArgs )
+SET(_openvdb_SEARCH_DIRS
+  ${OPENVDB_ROOT_DIR}
+  /usr/local
+  /sw # Fink
+  /opt/local # DarwinPorts
+  /opt/openvdb
+  /opt/lib/openvdb
+)
 
-FIND_PATH( OPENVDB_LOCATION include/openvdb/version.h
-  "$ENV{OPENVDB_ROOT}"
-  NO_SYSTEM_ENVIRONMENT_PATH
-  )
+FIND_PATH(OPENVDB_INCLUDE_DIR
+  NAMES
+    openvdb/openvdb.h
+  HINTS
+    ${_openvdb_SEARCH_DIRS}
+  PATH_SUFFIXES
+    include
+)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( OpenVDB
-  REQUIRED_VARS OPENVDB_LOCATION
-  )
+FIND_LIBRARY(OPENVDB_LIBRARY
+  NAMES
+    openvdb
+  HINTS
+    ${_openvdb_SEARCH_DIRS}
+  PATH_SUFFIXES
+    lib64 lib
+)
 
-IF( OpenVDB_FOUND )
-  SET( OpenVDB_INCLUDE_DIR ${OPENVDB_LOCATION}/include
-    CACHE PATH "OpenVDB include directory")
+# handle the QUIETLY and REQUIRED arguments and set OPENVDB_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OPENVDB DEFAULT_MSG
+    OPENVDB_LIBRARY OPENVDB_INCLUDE_DIR)
 
-  SET( OpenVDB_LIBRARY_DIR ${OPENVDB_LOCATION}/lib
-    CACHE PATH "OpenVDB library directory" )
+IF(OPENVDB_FOUND)
+  SET(OPENVDB_LIBRARIES ${OPENVDB_LIBRARY})
+  SET(OPENVDB_INCLUDE_DIRS ${OPENVDB_INCLUDE_DIR})
+ENDIF(OPENVDB_FOUND)
 
-  FIND_LIBRARY( OpenVDB_OPENVDB_LIBRARY openvdb
-    PATHS ${OpenVDB_LIBRARY_DIR}
-    NO_DEFAULT_PATH
-    NO_SYSTEM_ENVIRONMENT_PATH
-    )
+MARK_AS_ADVANCED(
+  OPENVDB_INCLUDE_DIR
+  OPENVDB_LIBRARY
+)
 
-  SET( OpenVDB_LIBRARIES "")
-  LIST( APPEND OpenVDB_LIBRARIES ${OpenVDB_OPENVDB_LIBRARY} )
-
-  SET( OPENVDB_VERSION_FILE ${OpenVDB_INCLUDE_DIR}/openvdb/version.h )
-
-  FILE( STRINGS "${OPENVDB_VERSION_FILE}" openvdb_major_version_str
-    REGEX "^#define[\t ]+OPENVDB_LIBRARY_MAJOR_VERSION_NUMBER[\t ]+.*")
-  FILE( STRINGS "${OPENVDB_VERSION_FILE}" openvdb_minor_version_str
-    REGEX "^#define[\t ]+OPENVDB_LIBRARY_MINOR_VERSION_NUMBER[\t ]+.*")
-  FILE( STRINGS "${OPENVDB_VERSION_FILE}" openvdb_patch_version_str
-    REGEX "^#define[\t ]+OPENVDB_LIBRARY_PATCH_VERSION_NUMBER[\t ]+.*")
-
-  STRING( REGEX REPLACE "^.*OPENVDB_LIBRARY_MAJOR_VERSION_NUMBER[\t ]+([0-9]*).*$" "\\1"
-    _openvdb_major_version_number "${openvdb_major_version_str}")
-  STRING( REGEX REPLACE "^.*OPENVDB_LIBRARY_MINOR_VERSION_NUMBER[\t ]+([0-9]*).*$" "\\1"
-    _openvdb_minor_version_number "${openvdb_minor_version_str}")
-  STRING( REGEX REPLACE "^.*OPENVDB_LIBRARY_PATCH_VERSION_NUMBER[\t ]+([0-9]*).*$" "\\1"
-    _openvdb_patch_version_number "${openvdb_patch_version_str}")
-
-  SET( OpenVDB_MAJOR_VERSION ${_openvdb_major_version_number}
-    CACHE STRING "OpenVDB major version number" )
-  SET( OpenVDB_MINOR_VERSION ${_openvdb_minor_version_number}
-    CACHE STRING "OpenVDB minor version number" )
-  SET( OpenVDB_PATCH_VERSION ${_openvdb_patch_version_number}
-    CACHE STRING "OpenVDB patch version number" )
-
-ENDIF( OpenVDB_FOUND )
+UNSET(_openvdb_SEARCH_DIRS)
