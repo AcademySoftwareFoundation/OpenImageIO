2e77b7f7f74b3f87fcb6616c8435c49acb4adfe3
Larry Gritz <lg@larrygritz.com>
2018-11-20 14:46:55 -0800

    Fix overlooked Python ImageOutput.open call for multiple subimages
    It was documented, but left out of the python binding implementation.

--- testsuite/python-imageoutput/run.py
+++ testsuite/python-imageoutput/run.py
@@ -8,9 +8,11 @@ command += oiio_app("iconvert") + imagedir + "/grid.tif --tile 64 64 tiled.tif >
 # Run the script 
 command += pythonbin + " src/test_imageoutput.py > out.txt ;"
 
-# compare the outputs
+# compare the outputs -- these are custom because they compare to grid.tif
 files = [ "grid-image.tif", "grid-scanline.tif", "grid-scanlines.tif",
           "grid-timage.tif", "grid-tile.tif", "grid-tiles.tif" ]
 for f in files :
     command += oiio_app("idiff") + f + " " + imagedir + "/grid.tif >> out.txt;"
+
+outputs = [ "multipart.exr", "out.txt" ]
 
