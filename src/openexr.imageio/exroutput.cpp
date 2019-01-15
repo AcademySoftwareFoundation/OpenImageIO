9c6655fd41d848688b2d4319be600d78162d12c5
Ramon Montoya <ramon@solidangle.com>
2019-01-15 20:51:52 +1300

    OpenEXROutput: skip dwa compression for single channel images with small or non power of two tile dimensions

--- src/openexr.imageio/exroutput.cpp
+++ src/openexr.imageio/exroutput.cpp
@@ -679,9 +679,11 @@ OpenEXROutput::spec_to_header(ImageSpec& spec, int subimage,
     // on deep files (but allow "none" as well)
     if (spec.deep && comp != "none")
         comp = "zips";
-    // Currently dwaa/b compression does not work on single channel with tile < 16
-    if (spec.nchannels == 1 && spec.tile_width < 16 && spec.tile_height < 16 &&
-        Strutil::istarts_with(comp, "dwa")) {
+    // For single channel images, dwaa/b compression only seems to work reliably
+    // when size > 16 and size is a power of two
+    if (spec.nchannels == 1 && Strutil::istarts_with(comp, "dwa") &&
+        ((spec.tile_width < 16 && spec.tile_height < 16) ||
+         !ispow2(spec.tile_width) || !ispow2(spec.tile_height))) {
         comp = "zip";
     }
     spec.attribute("compression", comp);
