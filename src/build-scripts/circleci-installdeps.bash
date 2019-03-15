8f3e3b3ca7c1e08ab8c48eda1462d3bd06d3dbdc
Larry Gritz <lg@larrygritz.com>
2019-03-15 10:24:29 -0700

    Coalesce apt-get for better performance installing dependencies

--- src/build-scripts/circleci-installdeps.bash
+++ src/build-scripts/circleci-installdeps.bash
@@ -7,40 +7,34 @@ set -ex
 
 time apt-get update
 
-time apt-get install -y git
-time apt-get install -y cmake
-time apt-get install -y g++
+time apt-get install -y \
+    git \
+    cmake \
+    ninja-build \
+    g++ \
+    ccache \
+    libboost-dev libboost-thread-dev \
+    libboost-filesystem-dev libboost-regex-dev \
+    libtiff-dev \
+    libilmbase-dev libopenexr-dev \
+    python-dev python-numpy \
+    libgif-dev \
+    libpng-dev \
+    libraw-dev \
+    libwebp-dev \
+    libfreetype6-dev \
+    dcmtk \
+    libavcodec-dev libavformat-dev libswscale-dev libavutil-dev \
+    locales \
+    opencolorio-tools \
+    wget
 # time apt-get install -y g++-4.8
 #time apt-get install -y g++-7
 #time apt-get install -y g++-8
-time apt-get install -y ccache
 # time apt-get install -y clang
 # time apt-get install -y llvm
-time apt-get install -y libboost-dev
-time apt-get install -y libboost-thread-dev
-time apt-get install -y libboost-filesystem-dev
-time apt-get install -y libboost-regex-dev
-time apt-get install -y libtiff-dev
-time apt-get install -y libilmbase-dev
-time apt-get install -y libopenexr-dev
-time apt-get install -y python-dev
-time apt-get install -y libboost-python-dev
-time apt-get install -y python-numpy
-time apt-get install -y libgif-dev
-time apt-get install -y libpng-dev
 #time apt-get install -y libopenjpeg-dev
-time apt-get install -y libraw-dev
-time apt-get install -y libwebp-dev
-time apt-get install -y libfreetype6-dev
 #time apt-get install -y libjpeg-turbo8-dev
-time apt-get install -y dcmtk
-time apt-get install -y libavcodec-dev
-time apt-get install -y libavformat-dev
-time apt-get install -y libswscale-dev
-time apt-get install -y libavutil-dev
-time apt-get install -y locales
-time apt-get install -y opencolorio-tools
-time apt-get install -y ninja-build
 
 dpkg --list
 
