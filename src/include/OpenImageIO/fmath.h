871b2ecf84551a7b730132b44b6845eda9743ab1
Larry Gritz <lg@larrygritz.com>
2019-06-06 10:12:15 -0700

    Fix stof return type error and other windows warnings (#2254)
    Submitted on behalf of Patrick Hodoul, with some minor mods by LG.
    
    * Fixes error in return type of stof.
    
    * Add several casts that fix MSVS warnings.

--- src/include/OpenImageIO/fmath.h
+++ src/include/OpenImageIO/fmath.h
@@ -1260,7 +1260,7 @@ inline OIIO_HOSTDEVICE float fast_sin (float x) {
     // Results on: [-2pi,2pi]
     // Examined 2173837240 values of sin: 0.00662760244 avg ulp diff, 2 max ulp, 1.19209e-07 max error
     int q = fast_rint (x * float(M_1_PI));
-    float qf = q;
+    float qf = float(q);
     x = madd(qf, -0.78515625f*4, x);
     x = madd(qf, -0.00024187564849853515625f*4, x);
     x = madd(qf, -3.7747668102383613586e-08f*4, x);
@@ -1290,7 +1290,7 @@ inline OIIO_HOSTDEVICE float fast_cos (float x) {
 #ifndef __CUDA_ARCH__
     // same argument reduction as fast_sin
     int q = fast_rint (x * float(M_1_PI));
-    float qf = q;
+    float qf = float(q);
     x = madd(qf, -0.78515625f*4, x);
     x = madd(qf, -0.00024187564849853515625f*4, x);
     x = madd(qf, -3.7747668102383613586e-08f*4, x);
@@ -1318,7 +1318,7 @@ inline OIIO_HOSTDEVICE void fast_sincos (float x, float* sine, float* cosine) {
 #ifndef __CUDA_ARCH__
     // same argument reduction as fast_sin
     int q = fast_rint (x * float(M_1_PI));
-    float qf = q;
+    float qf = float(q);
     x = madd(qf, -0.78515625f*4, x);
     x = madd(qf, -0.00024187564849853515625f*4, x);
     x = madd(qf, -3.7747668102383613586e-08f*4, x);
@@ -1356,7 +1356,7 @@ inline OIIO_HOSTDEVICE float fast_tan (float x) {
     // note that we cannot apply the "denormal crush" trick everywhere because
     // we sometimes need to take the reciprocal of the polynomial
     int q = fast_rint (x * float(2 * M_1_PI));
-    float qf = q;
+    float qf = float(q);
     x = madd(qf, -0.78515625f*2, x);
     x = madd(qf, -0.00024187564849853515625f*2, x);
     x = madd(qf, -3.7747668102383613586e-08f*2, x);
