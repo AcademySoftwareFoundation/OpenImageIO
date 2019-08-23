90d61babf4adc0a6b023c019fffc6934809b3e35
Larry Gritz <lg@larrygritz.com>
2019-08-23 10:27:00 -0700

    Some cleanup of recent SPI-specific build scripts (#2321)
    Some synchronization with OSL build scripts, including making some
    places that said "OpenImageIO_blah" into "${PROJECT_NAME}_blah" and
    the like, for the sake of easy sharing between the projects.
    
    build.py turns out to no longer be needed, but a new CMakeLists.txt for
    the Rez distro makes downstream Rez packages happy. From Jon Ware.

--- site/spi/cmake/sitecustom.cmake
+++ site/spi/cmake/sitecustom.cmake
@@ -1,15 +1,15 @@
-option (OIIO_SPIREZ "Enable extra bits for building for SPI Rez install" OFF)
-set (OIIO_REZ_PACKAGE_NAME CACHE STRING "${PROJECT_NAME}")
+option (${PROJ_NAME}_SPIREZ "Enable extra bits for building for SPI Rez install" OFF)
+set (${PROJ_NAME}_REZ_PACKAGE_NAME CACHE STRING "${PROJECT_NAME}")
 
-if (OIIO_SPIREZ)
+if (${PROJ_NAME}_SPIREZ)
     message (STATUS "Creating package.py from package.py.in")
     configure_file ("${PROJECT_SOURCE_DIR}/site/spi/rez/package.py.in" "${CMAKE_BINARY_DIR}/package.py")
 
-    set (appcfg_filename "${CMAKE_BINARY_DIR}/OpenImageIO_${OIIO_VERSION_MAJOR}.${OIIO_VERSION_MINOR}.${OIIO_VERSION_PATCH}.${OIIO_VERSION_TWEAK}.xml")
+    set (appcfg_filename "${CMAKE_BINARY_DIR}/${PROJECT_NAME}_${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.${PROJECT_VERSION_TWEAK}.xml")
     configure_file ("${PROJECT_SOURCE_DIR}/site/spi/appcfg.xml.in" "${appcfg_filename}")
 
     install (FILES ${CMAKE_BINARY_DIR}/package.py
-                   "${PROJECT_SOURCE_DIR}/site/spi/rez/build.py"
+                   "${PROJECT_SOURCE_DIR}/site/spi/rez/CMakeLists.txt"
                    ${appcfg_filename}
              DESTINATION ${CMAKE_INSTALL_PREFIX}
              COMPONENT developer)
