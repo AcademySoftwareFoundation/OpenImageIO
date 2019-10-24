130e8648f3892f64d52bb5ce8ba7d411d65782a7
Larry Gritz <lg@larrygritz.com>
2019-10-23 22:08:41 -0700

    Move shrinking contents of install.cmake into compiler.cmake

--- src/cmake/compiler.cmake
+++ src/cmake/compiler.cmake
@@ -383,3 +383,27 @@ if (DEFINED ENV{TRAVIS} OR DEFINED ENV{APPVEYOR} OR DEFINED ENV{CI} OR DEFINED E
         set(CMAKE_FIND_FRAMEWORK LAST)
     endif ()
 endif ()
+
+
+###########################################################################
+# Rpath handling at the install step
+set (MACOSX_RPATH ON)
+if (CMAKE_SKIP_RPATH)
+    # We need to disallow the user from truly setting CMAKE_SKIP_RPATH, since
+    # we want to run the generated executables from the build tree in order to
+    # generate the manual page documentation.  However, we make sure the
+    # install rpath is unset so that the install tree is still free of rpaths
+    # for linux packaging purposes.
+    set (CMAKE_SKIP_RPATH FALSE)
+    unset (CMAKE_INSTALL_RPATH)
+else ()
+    if (NOT CMAKE_INSTALL_RPATH)
+        set (CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_FULL_LIBDIR}")
+    endif ()
+    # add the automatically determined parts of the RPATH that
+    # point to directories outside the build tree to the install RPATH
+    set (CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+    if (VERBOSE)
+        message (STATUS "CMAKE_INSTALL_RPATH = ${CMAKE_INSTALL_RPATH}")
+    endif ()
+endif ()
