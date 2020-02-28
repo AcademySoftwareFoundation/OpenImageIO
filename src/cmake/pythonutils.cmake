7b2aa815c5b4b20125f77ea9093f86b398e550a6
Larry Gritz <lg@larrygritz.com>
2020-02-28 10:44:55 -0800

    Build system: Revise pybind11 procedures
    The way we downloaded header-only library was a little janky inasmuch as it
    did not actually "build" pybind11, which although there is nothing to compile
    per se for this header-only project, nonetheless that's what would generate
    the cmake config files defining the targets to import. And the presence of our
    FindPybind11.cmake module inhibited cmake finding the config files even when
    present elsewhere.
    
    This patch:
    
    * No longer automatically downloads pybind11 as part of our cmake config step.
    
    * Eliminates FindPybind11.cmake module in favor of always expecting a
      pybind11Config.cmake as part of a true pybind11 install.
    
    * Correctly uses the imported pybind11::pybind11 target instead of
      futzing with ${PYBIND11_INCLUDE_DIR}.
    
    * Supplies a new src/build-scripts/build_pybind11.bash script that will do the
      download and "build" of pybind11 (into the ext/ area by default) for those
      users who need it and don't have it on their systems.

--- src/cmake/pythonutils.cmake
+++ src/cmake/pythonutils.cmake
@@ -55,59 +55,6 @@ endmacro()
 ###########################################################################
 # pybind11
 
-option (BUILD_PYBIND11_FORCE "Force local download/build of Pybind11 even if installed" OFF)
-option (BUILD_MISSING_PYBIND11 "Local download/build of Pybind11 if not installed" ON)
-set (BUILD_PYBIND11_VERSION "v2.4.3" CACHE STRING "Preferred pybind11 version, of downloading/building our own")
-set (BUILD_PYBIND11_MINIMUM_VERSION "2.4.2")
-
-macro (find_or_download_pybind11)
-    # If we weren't told to force our own download/build of pybind11, look
-    # for an installed version. Still prefer a copy that seems to be
-    # locally installed in this tree.
-    if (NOT BUILD_PYBIND11_FORCE)
-        find_package (Pybind11 ${BUILD_PYBIND11_MINIMUM_VERSION} QUIET)
-    endif ()
-    # If an external copy wasn't found (and of an adequate version) and we
-    # requested that missing packages be built, or we we are forcing a local
-    # copy to be built unconditionally, then download and build it.
-    if ((BUILD_MISSING_PYBIND11 AND NOT PYBIND11_FOUND) OR BUILD_PYBIND11_FORCE)
-        message (STATUS "Building local Pybind11")
-        set (PYBIND11_INSTALL_DIR "${PROJECT_SOURCE_DIR}/ext/pybind11")
-        set (PYBIND11_GIT_REPOSITORY "https://github.com/pybind/pybind11")
-        if (NOT IS_DIRECTORY ${PYBIND11_INSTALL_DIR}/include)
-            find_package (Git REQUIRED)
-            execute_process(COMMAND
-                            ${GIT_EXECUTABLE} clone ${PYBIND11_GIT_REPOSITORY}
-                            --branch ${BUILD_PYBIND11_VERSION}
-                            ${PYBIND11_INSTALL_DIR}
-                            )
-            if (IS_DIRECTORY ${PYBIND11_INSTALL_DIR}/include)
-                message (STATUS "DOWNLOADED pybind11 to ${PYBIND11_INSTALL_DIR}.\n"
-                         "Remove that dir to get rid of it.")
-            else ()
-                message (FATAL_ERROR "Could not download pybind11")
-            endif ()
-            set (PYBIND11_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/ext/pybind11/include")
-        endif ()
-    endif ()
-    checked_find_package (Pybind11 ${BUILD_PYBIND11_MINIMUM_VERSION})
-
-    if (NOT PYBIND11_FOUND)
-        if (PYBIND11_INCLUDE_DIR)
-            message (STATUS "Pybind11 was found in ${PYBIND11_INCLUDE_DIR}")
-            message (STATUS "... but was version ${PYBIND11_VERSION} (minimum is ${BUILD_PYBIND11_MINIMUM_VERSION})")
-            if (PYBIND11_INCLUDE_DIR MATCHES "/ext/pybind11")
-                message (STATUS "Try removing ext/pybind11 and let me download a newer version.")
-            endif ()
-        endif ()
-        message (FATAL_ERROR "pybind11 is missing! If it's not on your "
-                 "system, you need to install it, or build with either "
-                 "-DBUILD_MISSING_DEPS=ON or -DBUILD_PYBIND11_FORCE=ON. "
-                 "Or build with -DUSE_PYTHON=OFF.")
-    endif ()
-endmacro()
-
-
 macro (setup_python_module)
     cmake_parse_arguments (lib "" "TARGET;MODULE;LIBS" "SOURCES" ${ARGN})
     # Arguments: <prefix> <options> <one_value_keywords> <multi_value_keywords> args...
@@ -124,14 +71,14 @@ macro (setup_python_module)
 
     # Set up include dirs for python & pybind11
     target_include_directories (${target_name} SYSTEM PRIVATE ${PYTHON_INCLUDE_PATH})
-    target_include_directories (${target_name} PRIVATE ${PYBIND11_INCLUDE_DIR})
 
     # Declare the libraries it should link against
-    target_link_libraries (${target_name} ${lib_LIBS} ${SANITIZE_LIBRARIES})
+    target_link_libraries (${target_name} PRIVATE
+                           pybind11::pybind11 ${lib_LIBS} ${SANITIZE_LIBRARIES})
     if (APPLE)
         set_target_properties (${target_name} PROPERTIES LINK_FLAGS "-undefined dynamic_lookup")
     else ()
-        target_link_libraries (${target_name} ${PYTHON_LIBRARIES})
+        target_link_libraries (${target_name} PRIVATE ${PYTHON_LIBRARIES})
     endif ()
 
     # Exclude the 'lib' prefix from the name
