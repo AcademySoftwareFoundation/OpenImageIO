52f2217ad63c797bd9e1ec555be3eadcfa7aed29
Larry Gritz <lg@larrygritz.com>
2020-03-10 10:16:28 -0700

    Adjust testsuite after adding tga samples to oiio-images (#2512)
    The place where the TGAUTILS samples were stored seems to have disappeared
    from the web. So put them in oiio-images. Adjusted the targa-tgautils test
    for the new location.
    
    Also took the opportunity to clean up the pnm test and simplify how it's
    declared.

--- testsuite/pnm/run.py
+++ testsuite/pnm/run.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 
-
-#files = [ "oiio-logo-no-alpha.png",  "oiio-logo-with-alpha.png" ]
-#for f in files:
-#    command += rw_command (OIIO_TESTSUITE_IMAGEDIR,  f)
-
+imagedir = OIIO_TESTSUITE_IMAGEDIR + "/pnm"
 
 # We can't yet write PFM files, so just get the hashes and call it a day
 files = [ "test-1.pfm", "test-2.pfm", "test-3.pfm" ]
 for f in files:
-    command += info_command (OIIO_TESTSUITE_IMAGEDIR + "/" + f,
+    command += info_command (imagedir + "/" + f,
                              safematch=True, hash=True)
-    #command += rw_command (OIIO_TESTSUITE_IMAGEDIR,  f)
--- testsuite/targa-tgautils/run.py
+++ testsuite/targa-tgautils/run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+imagedir = OIIO_TESTSUITE_IMAGEDIR + "/targa"
+
 files = [ "CBW8.TGA", "CCM8.TGA", "CTC16.TGA", "CTC24.TGA", "CTC32.TGA",
           "UBW8.TGA", "UCM8.TGA", "UTC16.TGA", "UTC24.TGA", "UTC32.TGA" ]
 for f in files:
-    command += rw_command (OIIO_TESTSUITE_IMAGEDIR, f)
+    command += rw_command (imagedir, f)
