8853dd0b0149fc6431dbe9bc8d6e3e18412a985d
Larry Gritz <lg@larrygritz.com>
2020-03-10 10:14:56 -0700

    CI: allow homebrew field3d installation failure (#2514)
    Seems that Homebrew has removed the field3d package, as its scons
    build system needs discontinued python2.7 and other parts have fallen
    into disrepair and are having trouble on modern compilers and cmake.

--- src/build-scripts/install_homebrew_deps.bash
+++ src/build-scripts/install_homebrew_deps.bash
@@ -40,9 +40,11 @@ else
     brew install --display-times libtiff ilmbase openexr opencolorio
     brew install --display-times libpng giflib webp jpeg-turbo openjpeg
     brew install --display-times freetype libraw dcmtk pybind11 numpy
-    brew install --display-times field3d ffmpeg libheif libsquish
+    brew install --display-times ffmpeg libheif libsquish
     brew install --display-times openvdb tbb
     brew install --display-times opencv qt ptex
+    # N.B.: seems that Homebrew has removed field3d, allow failure
+    brew install --display-times field3d && true
 fi
 
 if [[ "$LINKSTATIC" == "1" ]] ; then
