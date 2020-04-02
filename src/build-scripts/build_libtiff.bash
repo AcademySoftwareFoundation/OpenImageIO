dda61e5e01c2f34e85677631f9625e4db7026dcd
Larry Gritz <lg@larrygritz.com>
2020-04-02 13:05:46 -0700

    Make the 'bleeding edge' CI test build libtiff and openexr from master
    The idea of this "bleeding edge" entry in the CI test matrix is to
    really use the very latest toolchain, in order to spot problems long
    before users will encounter them.
    
    To that end, let's make sure we are building and testing the current
    master of libtiff and OpenEXR.

---
+++ src/build-scripts/build_libtiff.bash
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Utility script to download and build libtiff
+
+# Exit the whole script if any command fails.
+set -ex
+
+LIBTIFF_REPO=${LIBTIFF_REPO:=https://gitlab.com/libtiff/libtiff.git}
+LOCAL_DEPS_DIR=${LOCAL_DEPS_DIR:=${PWD}/ext}
+LIBTIFF_BUILD_DIR=${LIBTIFF_BUILD_DIR:=${LOCAL_DEPS_DIR}/libtiff}
+LIBTIFF_INSTALL_DIR=${LIBTIFF_INSTALL_DIR:=${LOCAL_DEPS_DIR}/libtiff/dist}
+LIBTIFF_VERSION=${LIBTIFF_VERSION:=4.1.0}
+LIBTIFF_BRANCH=${LIBTIFF_BRANCH:=v${LIBTIFF_VERSION}}
+LIBTIFF_CXX_FLAGS=${LIBTIFF_CXX_FLAGS:="-Wno-unused-function -Wno-deprecated-declarations -Wno-cast-qual -Wno-write-strings"}
+LIBTIFF_BUILDOPTS="${LIBTIFF_BUILDOPTS}"
+BASEDIR=`pwd`
+pwd
+echo "libtiff install dir will be: ${LIBTIFF_INSTALL_DIR}"
+
+mkdir -p ${LOCAL_DEPS_DIR}
+pushd ${LOCAL_DEPS_DIR}
+
+# Clone libtiff project from GitHub and build
+if [[ ! -e libtiff ]] ; then
+    echo "git clone ${LIBTIFF_REPO} libtiff"
+    git clone ${LIBTIFF_REPO} libtiff
+fi
+cd libtiff
+
+echo "git checkout ${LIBTIFF_BRANCH} --force"
+git checkout ${LIBTIFF_BRANCH} --force
+mkdir -p build
+cd build
+time cmake --config Release -DCMAKE_INSTALL_PREFIX=${LIBTIFF_INSTALL_DIR} -DCMAKE_CXX_FLAGS="${LIBTIFF_CXX_FLAGS}" ${LIBTIFF_BUILDOPTS} ..
+time cmake --build . --config Release --target install
+popd
+
+ls -R ${LIBTIFF_INSTALL_DIR}
+
+#echo "listing .."
+#ls ..
+
+# Set up paths. These will only affect the caller if this script is
+# run with 'source' rather than in a separate shell.
+export TIFF_ROOT=$LIBTIFF_INSTALL_DIR
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LIBTIFF_INSTALL_DIR}/lib
+
