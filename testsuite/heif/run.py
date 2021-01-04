15ac232a9345c961abb581ce9fe0b39393c3d49c
Harry Mallon <harry.mallon@codex.online>
2021-01-04 19:27:22 +0000

    Add a test file and simple test for AVIF

--- testsuite/heif/run.py
+++ testsuite/heif/run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
 imagedir = "ref/"
-files = [ "IMG_7702_small.heic" ]
+files = [ "IMG_7702_small.heic", "Chimera-AV1-8bit-162.avif" ]
 for f in files:
     command = command + info_command (os.path.join(imagedir, f))
