e49043a35b5ba050d7e56a8ad6103e4857953920
Harry Mallon <harry.mallon@codex.online>
2021-01-06 11:06:10 +0000

    Fix tests with libheif built without AV1 support

--- testsuite/heif/run.py
+++ testsuite/heif/run.py
@@ -4,3 +4,6 @@ imagedir = "ref/"
 files = [ "IMG_7702_small.heic", "Chimera-AV1-8bit-162.avif" ]
 for f in files:
     command = command + info_command (os.path.join(imagedir, f))
+
+# avif conversion is expected to fail if libheif is built without AV1 support
+failureok = 1
