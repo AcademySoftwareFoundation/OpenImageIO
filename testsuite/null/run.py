411795c54ef40f490a87c3b9cb03558342f4336a
Larry Gritz <lg@larrygritz.com>
2021-01-14 15:49:08 -0800

    Windows CI fix: windows shell treats quotes differently

--- testsuite/null/run.py
+++ testsuite/null/run.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-command += oiiotool ("-v -info -stats 'foo.null?RES=640x480&CHANNELS=3&TYPE=uint8&PIXEL=0.25,0.5,1'")
+command += oiiotool ('-v -info -stats "foo.null?RES=640x480&CHANNELS=3&TYPE=uint8&PIXEL=0.25,0.5,1"')
