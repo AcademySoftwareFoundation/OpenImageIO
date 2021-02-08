02ffd75c7db5afbdb6b94f2adb3617c59fe0f5ab
Larry Gritz <lg@larrygritz.com>
2021-02-08 12:51:57 -0800

    More thoroughly constexpr string_view (#2841)
    This should not break backward compatibility.

--- src/include/OpenImageIO/string_view.h
+++ src/include/OpenImageIO/string_view.h
@@ -16,6 +16,7 @@
 
 #include <OpenImageIO/export.h>
 #include <OpenImageIO/oiioversion.h>
+#include <OpenImageIO/platform.h>
 
 
 OIIO_NAMESPACE_BEGIN
@@ -62,38 +63,51 @@ OIIO_NAMESPACE_BEGIN
 
 class OIIO_API string_view {
 public:
-    typedef char charT;
-    typedef charT value_type;
-    typedef const charT* pointer;
-    typedef const charT* const_pointer;
-    typedef const charT& reference;
-    typedef const charT& const_reference;
-    typedef const_pointer const_iterator;
-    typedef const_iterator iterator;
-    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-    typedef const_reverse_iterator reverse_iterator;
-    typedef size_t size_type;
-    typedef ptrdiff_t difference_type;
-    typedef std::char_traits<charT> traits;
+    using charT = char;
+    using Traits = std::char_traits<charT>;  // standard name
+    using traits_type = Traits;
+    using value_type = charT;
+    using pointer = const charT*;
+    using const_pointer = const charT*;
+    using reference = const charT&;
+    using const_reference = const charT&;
+    using const_iterator = const_pointer;
+    using iterator = const_iterator;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    using reverse_iterator = const_reverse_iterator;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using traits = std::char_traits<charT>;  // obsolete custom name
     static const size_type npos = ~size_type(0);
 
     /// Default ctr
     constexpr string_view() noexcept : m_chars(nullptr), m_len(0) { }
+
     /// Copy ctr
-    string_view(const string_view& copy) noexcept { init(copy.m_chars, copy.m_len); }
+    constexpr string_view(const string_view& copy)
+        : m_chars(copy.data()), m_len(copy.size()) { }
+
     /// Construct from char* and length.
-    string_view(const charT* chars, size_t len) { init(chars, len); }
+    constexpr string_view(const charT* chars, size_t len)
+        : m_chars(chars), m_len(len) { }
+
     /// Construct from char*, use strlen to determine length.
-    string_view(const charT* chars) { init(chars, chars ? strlen(chars) : 0); }
+    OIIO_CONSTEXPR17 string_view(const charT* chars)
+        : m_chars(chars), m_len(chars ? Traits::length(chars) : 0) { }
+    // N.B. char_traits::length() is constexpr starting with C++17.
+
     /// Construct from std::string. Remember that a string_view doesn't have
     /// its own copy of the characters, so don't use the `string_view` after
     /// the original string has been destroyed or altered.
-    string_view(const std::string& str) noexcept { init(str.data(), str.size()); }
+    OIIO_CONSTEXPR20 string_view(const std::string& str) noexcept
+        : m_chars(str.data()), m_len(str.size()) { }
+    // N.B. std::string::size() is constexpr starting with C++20.
 
     /// Convert a string_view to a `std::string`.
-    std::string str() const
+    OIIO_CONSTEXPR20 std::string str() const
     {
         return (m_chars ? std::string(m_chars, m_len) : std::string());
+        // N.B. std::string ctr from chars+len is constexpr in C++20.
     }
 
     /// Explicitly request a 0-terminated string. USUALLY, this turns out to
@@ -117,24 +131,20 @@ public:
     const char* c_str() const;
 
     // Assignment
-    string_view& operator=(const string_view& copy) noexcept
-    {
-        init(copy.data(), copy.length());
-        return *this;
-    }
+    OIIO_CONSTEXPR14 string_view& operator=(const string_view& copy) noexcept = default;
 
     /// Convert a string_view to a `std::string`.
-    operator std::string() const { return str(); }
+    OIIO_CONSTEXPR20 operator std::string() const { return str(); }
 
     // iterators
-    const_iterator begin() const noexcept { return m_chars; }
-    const_iterator end() const noexcept { return m_chars + m_len; }
-    const_iterator cbegin() const noexcept { return m_chars; }
-    const_iterator cend() const noexcept { return m_chars + m_len; }
-    const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator (end()); }
-    const_reverse_iterator rend() const noexcept { return const_reverse_iterator (begin()); }
-    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator (end()); }
-    const_reverse_iterator crend() const noexcept { return const_reverse_iterator (begin()); }
+    constexpr iterator begin() const noexcept { return m_chars; }
+    constexpr iterator end() const noexcept { return m_chars + m_len; }
+    constexpr const_iterator cbegin() const noexcept { return m_chars; }
+    constexpr const_iterator cend() const noexcept { return m_chars + m_len; }
+    OIIO_CONSTEXPR17 reverse_iterator rbegin() const noexcept { return reverse_iterator (end()); }
+    OIIO_CONSTEXPR17 reverse_iterator rend() const noexcept { return reverse_iterator (begin()); }
+    OIIO_CONSTEXPR17 const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator (cend()); }
+    OIIO_CONSTEXPR17 const_reverse_iterator crend() const noexcept { return const_reverse_iterator (cbegin()); }
 
     // capacity
     constexpr size_type size() const noexcept { return m_len; }
@@ -147,35 +157,35 @@ public:
 
     /// Element access of an individual character (beware: no bounds
     /// checking!).
-    const charT& operator[](size_type pos) const { return m_chars[pos]; }
+    constexpr const_reference operator[](size_type pos) const { return m_chars[pos]; }
     /// Element access with bounds checking and exception if out of bounds.
-    const charT& at(size_t pos) const
+    OIIO_CONSTEXPR17 const_reference at(size_t pos) const
     {
         if (pos >= m_len)
             throw(std::out_of_range("OpenImageIO::string_view::at"));
         return m_chars[pos];
     }
-    const charT& front() const { return m_chars[0]; }
-    const charT& back() const { return m_chars[m_len - 1]; }
-    const charT* data() const noexcept { return m_chars; }
+    constexpr const_reference front() const { return m_chars[0]; }
+    constexpr const_reference back() const { return m_chars[m_len - 1]; }
+    constexpr const_pointer data() const noexcept { return m_chars; }
 
     // modifiers
-    void clear() noexcept { init(nullptr, 0); }
-    void remove_prefix(size_type n) noexcept
+    OIIO_CONSTEXPR14 void clear() noexcept { init(nullptr, 0); }
+    OIIO_CONSTEXPR14 void remove_prefix(size_type n) noexcept
     {
         if (n > m_len)
             n = m_len;
         m_chars += n;
         m_len -= n;
     }
-    void remove_suffix(size_type n) noexcept
+    OIIO_CONSTEXPR14 void remove_suffix(size_type n) noexcept
     {
         if (n > m_len)
             n = m_len;
         m_len -= n;
     }
 
-    string_view substr(size_type pos, size_type n = npos) const noexcept
+    OIIO_CONSTEXPR14 string_view substr(size_type pos, size_type n = npos) const noexcept
     {
         if (pos >= size())
             return string_view();  // start past end -> return empty
@@ -184,8 +194,9 @@ public:
         return string_view(data() + pos, n);
     }
 
-    int compare (string_view x) const noexcept {
-        const int cmp = std::char_traits<char>::compare (m_chars, x.m_chars, (std::min)(m_len, x.m_len));
+    OIIO_CONSTEXPR17 int compare (string_view x) const noexcept {
+        // N.B. char_traits<char>::compare is constexpr for C++17
+        const int cmp = traits_type::compare (m_chars, x.m_chars, (std::min)(m_len, x.m_len));
         return cmp != 0 ? cmp : int(m_len - x.m_len);
         // Equivalent to:
         //  cmp != 0 ? cmp : (m_len == x.m_len ? 0 : (m_len < x.m_len ? -1 : 1));
@@ -297,10 +308,10 @@ public:
     }
 
 private:
-    const charT* m_chars;
-    size_t m_len;
+    const charT* m_chars = nullptr;
+    size_t m_len = 0;
 
-    void init(const charT* chars, size_t len) noexcept
+    OIIO_CONSTEXPR14 void init(const charT* chars, size_t len) noexcept
     {
         m_chars = chars;
         m_len   = len;
@@ -323,45 +334,45 @@ private:
 
     class traits_eq {
     public:
-        traits_eq (charT ch) noexcept : ch(ch) {}
-        bool operator () (charT val) const noexcept { return traits::eq (ch, val); }
+        constexpr traits_eq (charT ch) noexcept : ch(ch) {}
+        constexpr bool operator () (charT val) const noexcept { return traits::eq (ch, val); }
         charT ch;
     };
 };
 
 
 
-inline bool
+inline OIIO_CONSTEXPR17 bool
 operator==(string_view x, string_view y) noexcept
 {
     return x.size() == y.size() ? (x.compare(y) == 0) : false;
 }
 
-inline bool
+inline OIIO_CONSTEXPR17 bool
 operator!=(string_view x, string_view y) noexcept
 {
     return x.size() == y.size() ? (x.compare(y) != 0) : true;
 }
 
-inline bool
+inline OIIO_CONSTEXPR17 bool
 operator<(string_view x, string_view y) noexcept
 {
     return x.compare(y) < 0;
 }
 
-inline bool
+inline OIIO_CONSTEXPR17 bool
 operator>(string_view x, string_view y) noexcept
 {
     return x.compare(y) > 0;
 }
 
-inline bool
+inline OIIO_CONSTEXPR17 bool
 operator<=(string_view x, string_view y) noexcept
 {
     return x.compare(y) <= 0;
 }
 
-inline bool
+inline OIIO_CONSTEXPR17 bool
 operator>=(string_view x, string_view y) noexcept
 {
     return x.compare(y) >= 0;
