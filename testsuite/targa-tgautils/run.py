7f4f9a7adce199f9c890926fdb3c457e5b165fa4
Larry Gritz <lg@larrygritz.com>
2021-03-20 21:21:37 -0700

    Targa read fix for tga files with 0-sized thumbnails (#2903)
    All of our prior tga tests had valid thumbnails. The code skipped the
    thumbnail if the "offset to the thumbnail" was 0. But if the offset was
    nonzero but the thumbnail record at that spot had a 0x0 size, we just
    ran merrily along, and on Windows in debug mode even it an assert.
    
    This patch makes it understand that a 0x0 thumbnail size means no
    thumbnail.
    
    I added an image from Doug Rogers, the reporter of the problem, to the
    oiio-images repo in the targa directory, and updated the OIIO testsuite
    to also check that image. (So testsuite will fail for people who don't
    update their oiio-images repo.)
    
    Also changed some stray refs to &vector[0] to vector.data().
    
    Fixes #2900

--- testsuite/targa-tgautils/run.py
+++ testsuite/targa-tgautils/run.py
@@ -3,6 +3,7 @@
 imagedir = OIIO_TESTSUITE_IMAGEDIR + "/targa"
 
 files = [ "CBW8.TGA", "CCM8.TGA", "CTC16.TGA", "CTC24.TGA", "CTC32.TGA",
-          "UBW8.TGA", "UCM8.TGA", "UTC16.TGA", "UTC24.TGA", "UTC32.TGA" ]
+          "UBW8.TGA", "UCM8.TGA", "UTC16.TGA", "UTC24.TGA", "UTC32.TGA",
+          "round_grill.tga" ]
 for f in files:
     command += rw_command (imagedir, f)
