3ae756b64f689cb9ba21c08bea0c2dbf51942b10
Larry Gritz <lg@larrygritz.com>
2021-06-06 23:47:20 -0700

    Less restrictive warning pragma, amend last PR

--- src/oiiotool/oiiotool.cpp
+++ src/oiiotool/oiiotool.cpp
@@ -5153,7 +5153,7 @@ crash_me()
     size_t a   = 37;
     char* addr = (char*)a;
     OIIO_PRAGMA_WARNING_PUSH
-#if OIIO_GNUC_VERSION >= 110000 && OIIO_CPLUSPLUS_VERSION >= 20
+#if OIIO_GNUC_VERSION >= 110000
     OIIO_GCC_ONLY_PRAGMA(GCC diagnostic ignored "-Wstringop-overflow")
 #endif
     *addr = 0;  // This should crash
