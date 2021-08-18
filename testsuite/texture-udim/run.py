a518027033f0f36b9c36b5b84694277cf6f96501
Larry Gritz <lg@larrygritz.com>
2021-08-18 12:59:09 -0700

    Fix typo in test generation

--- testsuite/texture-udim/run.py
+++ testsuite/texture-udim/run.py
@@ -6,7 +6,7 @@ command += oiiotool ("-pattern constant:color=.1,.5,.1 256x256 3 -text:size=50:x
 command += "sleep 1 ; "
 command += oiiotool ("-pattern constant:color=.1,.1,.5 256x256 3 -text:size=50:x=75:y=140 1011 --attrib Make pet --attrib Model cat -d uint8 -otex file.1011.tx")
 command += oiiotool ("-pattern constant:color=.1,.5,.5 256x256 3 -text:size=50:x=75:y=140 1012 --attrib Make pet --attrib Model dog -d uint8 -otex file.1012.tx")
-command += oiiotool ("-pattern constant:color=.1,.5,.5 256x256 3 -text:size=50:x=75:y=140 1012 --attrib Make pet --attrib Model dog -d uint8 -otex file.1032.tx")
+command += oiiotool ("-pattern constant:color=.1,.5,.5 256x256 3 -text:size=50:x=75:y=140 1032 --attrib Make pet --attrib Model dog -d uint8 -otex file.1032.tx")
 
 command += testtex_command ("\"file.<UDIM>.tx\"",
                             "-nowarp -scalest 2 2 -res 128 128 -d uint8 -o out.tif")
