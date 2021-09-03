7a90bf9261e4658fe9710f7f394b222d2a6247be
Larry Gritz <lg@larrygritz.com>
2021-09-02 22:13:26 -0700

    OCIO: better catching of exceptions.
    When OIIO that was built against OCIO 1.x, but encounters an OCIO 2.x
    config file, OCIO could throw an exception that was not caught by
    OIIO.  The problem was the call to OCIO::GetCurrentConfig(), it should
    have been inside the try/catch block.

--- testsuite/oiiotool-color/run.py
+++ testsuite/oiiotool-color/run.py
@@ -62,6 +62,11 @@ command += oiiotool ("--autocc " + oiiotoolsrcdir+"/tahoe-tiny.tif"+
                      + "--ccmatrix 0.805,0.506,-0.311,0,-0.311,0.805,0.506,0,0.506,-0.311,0.805,0,0,0,0,1 "
                      + "-d uint8 -o tahoe-ccmatrix.tif")
 
+# test what happens when we read an OCIOv2 config. In particular, when
+# building against OCIOv1, we should at worst have an error message, not crash
+# with an uncaught exception.
+command += oiiotool ("--colorconfig ../common/OpenColorIO/ocio-v2_demo.ocio -echo 'OCIOv2 config Ok'")
+
 
 # To add more tests, just append more lines like the above and also add
 # the new 'feature.tif' (or whatever you call it) to the outputs list,
