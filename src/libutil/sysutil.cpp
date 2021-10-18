ec2fe43d75eed22c0736157f40a1123f4cb217e9
Brad Smith <brad@comstyle.com>
2021-10-18 03:45:32 -0400

    Sysutil: improvements to NetBSD and OpenBSD support (#3137)

--- src/libutil/sysutil.cpp
+++ src/libutil/sysutil.cpp
@@ -22,7 +22,8 @@
 #    include <unistd.h>
 #endif
 
-#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+#if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__NetBSD__) \
+    || defined(__OpenBSD__)
 #    include <sys/ioctl.h>
 #    include <sys/resource.h>
 #    include <sys/sysctl.h>
@@ -130,7 +131,9 @@ Sysutil::memory_used(bool resident)
     else
         return 0;
 
-#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__) \
+    || defined(__NetBSD__) || defined(__OpenBSD__)
+
     // FIXME -- does somebody know a good method for figuring this out for
     // FreeBSD?
     return 0;  // Punt
@@ -199,6 +202,13 @@ Sysutil::physical_memory()
     sysctl(mib, 2, &physical_memory, &length, NULL, 0);
     return physical_memory;
 
+#elif defined(__NetBSD__) || defined(__OpenBSD__)
+    int mib[2] = { CTL_HW, HW_PHYSMEM64 };
+    uint64_t physical_memory;
+    size_t length = sizeof(physical_memory);
+    sysctl(mib, 2, &physical_memory, &length, NULL, 0);
+    return physical_memory;
+
 #else
     // No idea what platform this is
     OIIO_ASSERT(
@@ -256,6 +266,15 @@ Sysutil::this_program_path()
     size_t cb = sizeof(filename);
     int r     = 1;
     sysctl(mib, 4, filename, &cb, NULL, 0);
+#elif defined(__NetBSD__)
+    int mib[4];
+    mib[0]    = CTL_KERN;
+    mib[1]    = KERN_PROC_ARGS;
+    mib[2]    = -1;
+    mib[3]    = KERN_PROC_PATHNAME;
+    size_t cb = sizeof(filename);
+    int r     = 1;
+    sysctl(mib, 4, filename, &cb, NULL, 0);
 #elif defined(__GNU__) || defined(__OpenBSD__) || defined(_WIN32)
     int r = 0;
 #else
@@ -309,7 +328,8 @@ Sysutil::terminal_columns()
     int columns = 80;  // a decent guess, if we have nothing more to go on
 
 #if defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) \
-    || defined(__FreeBSD_kernel__) || defined(__GNU__)
+    || defined(__FreeBSD_kernel__) || defined(__NetBSD__)            \
+    || defined(__OpenBSD__) || defined(__GNU__)
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
     columns = w.ws_col;
@@ -333,7 +353,8 @@ Sysutil::terminal_rows()
     int rows = 24;  // a decent guess, if we have nothing more to go on
 
 #if defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__) \
-    || defined(__FreeBSD_kernel__) || defined(__GNU__)
+    || defined(__FreeBSD_kernel__) || defined(__NetBSD__)            \
+    || defined(__OpenBSD__) || defined(__GNU__)
     struct winsize w;
     ioctl(0, TIOCGWINSZ, &w);
     rows = w.ws_row;
