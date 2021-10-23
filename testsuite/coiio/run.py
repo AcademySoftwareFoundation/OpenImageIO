b0d5c0c34c2c615973c8e1c0d0d35209c622ab68
Larry Gritz <lg@larrygritz.com>
2021-10-23 12:00:52 -0700

    Remove the last vestiges of the experimental C API
    We will have a C API eventually, but it will be implemented
    differently, and this cruft will probably get in the way. If the new
    code needs any of this (particularly the test cases), it's easy enough
    to revive it from the project history).

--- testsuite/coiio/run.py
+++
@@ -1,6 +0,0 @@
-command += run_app("cmake " + test_source_dir + " -DCMAKE_BUILD_TYPE=Release >> build.txt 2>&1", silent=True)
-command += run_app("cmake --build . --config Release >> build.txt 2>&1", silent=True)
-if platform.system() == 'Windows' :
-    command += run_app("Release\\coiiotest")
-else :
-    command += run_app("./coiiotest")
