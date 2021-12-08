c4d9b6e66db59ab5e6a09892e1d2faec1fbc2d6d
Larry Gritz <lg@larrygritz.com>
2021-12-07 16:15:10 -0800

    hdr overhaul (#3218)
    * Add full IOProxy support to both input and output.
    
    * This necessitated a major refactor, including sucking the I/O specific
      code from rgbe.{cpp,h} into hdr{input,output}.cpp and getting rid of
      the old files.
    
    * Add a testsuite entry for hdr (shockingly, there was none before)

--- src/hdr.imageio/hdrinput.cpp
+++ src/hdr.imageio/hdrinput.cpp
@@ -10,8 +10,6 @@
 #include <OpenImageIO/filesystem.h>
 #include <OpenImageIO/imageio.h>
 
-#include "rgbe.h"
-
 
 OIIO_PLUGIN_NAMESPACE_BEGIN
 
@@ -20,11 +18,16 @@ OIIO_PLUGIN_NAMESPACE_BEGIN
 //
 // General info on the hdr/rgbe format can be found at:
 //     http://local.wasp.uwa.edu.au/~pbourke/dataformats/pic/
-// The source code in rgbe.{h,cpp} originally came from:
-//     http://www.graphics.cornell.edu/~bjw/rgbe.html
-// But it's been modified in several minor ways by LG.
+//
 // Also see Greg Ward's "Real Pixels" chapter in Graphics Gems II for an
 // explanation of the encoding that's used in Radiance rgba files.
+//
+// Based on source code that originally came from:
+//     http://www.graphics.cornell.edu/~bjw/rgbe.html
+// But it's been modified very heavily, and little of the original remains.
+// Nonetheless, it bore this original notice:
+//     written by Bruce Walter  (bjw@graphics.cornell.edu)  5/26/95
+//     based on code written by Greg Ward
 /////////////////////////////////////////////////////////////////////////////
 
 
@@ -34,7 +37,13 @@ public:
     HdrInput() { init(); }
     virtual ~HdrInput() { close(); }
     virtual const char* format_name(void) const override { return "hdr"; }
+    virtual int supports(string_view feature) const override
+    {
+        return feature == "ioproxy";
+    }
     virtual bool open(const std::string& name, ImageSpec& spec) override;
+    virtual bool open(const std::string& name, ImageSpec& newspec,
+                      const ImageSpec& config) override;
     virtual bool read_native_scanline(int subimage, int miplevel, int y, int z,
                                       void* data) override;
     virtual bool close() override;
@@ -42,21 +51,43 @@ public:
     virtual bool seek_subimage(int subimage, int miplevel) override;
 
 private:
-    std::string m_filename;  ///< File name
-    FILE* m_fd;              ///< The open file handle
-    int m_subimage;          ///< What subimage are we looking at?
-    int m_next_scanline;     ///< Next scanline to read
-    std::vector<int64_t>
-        m_scanline_offsets;  ///< Cached scanline offsets for random access
-    std::string rgbe_error;  ///< Buffer for RGBE library error msgs
+    std::string m_filename;  // File name
+    int m_subimage;          // What subimage are we looking at?
+    int m_next_scanline;     // Next scanline to read
+    std::vector<int64_t> m_scanline_offsets;  // Cached scanline offsets
+    int64_t m_io_pos = 0;                     // current position
 
     void init()
     {
-        m_fd            = NULL;
         m_subimage      = -1;
         m_next_scanline = 0;
         m_scanline_offsets.clear();
-        rgbe_error.clear();
+        ioproxy_clear();
+    }
+
+    bool RGBE_ReadHeader();
+    bool RGBE_ReadPixels(float* data, int y, uint64_t numpixels);
+    bool RGBE_ReadPixels_RLE(float* data, int y, int scanline_width,
+                             int num_scanlines);
+
+    // helper: fgets reads a "line" from the proxy, akin to std fgets. The
+    // bytes go in the buffer, and part up to and including the new line is
+    // returned as a string_view, and the file pointer is updated to the byte
+    // right after the newline.
+    string_view fgets(span<char> buf)
+    {
+        Filesystem::IOProxy* m_io = ioproxy();
+        auto rdsize = m_io->pread(buf.data(), buf.size(), m_io_pos);
+        string_view sv(buf.data(), rdsize);
+        if (sv.size() == 0) {
+            errorfmt(
+                "RGBE read error -- early end of file at position {}, asked for {}, got {} bytes, file size {}",
+                m_io_pos, buf.size(), rdsize, m_io->size());
+            return sv;
+        }
+        sv = Strutil::parse_line(sv);
+        m_io_pos += sv.size();
+        return sv;
     }
 };
 
@@ -85,14 +116,66 @@ OIIO_PLUGIN_EXPORTS_END
 
 
 
+/* standard conversion from rgbe to float pixels */
+/* note: Ward uses ldexp(col+0.5,exp-(128+8)).  However we wanted pixels */
+/*       in the range [0,1] to map back into the range [0,1].            */
+inline void
+rgbe2float(float& red, float& green, float& blue, unsigned char rgbe[4])
+{
+    if (rgbe[3]) { /*nonzero pixel*/
+        float f = ldexpf(1.0f, rgbe[3] - (int)(128 + 8));
+        red     = rgbe[0] * f;
+        green   = rgbe[1] * f;
+        blue    = rgbe[2] * f;
+    } else {
+        red = green = blue = 0.0f;
+    }
+}
+
+
+
+bool
+HdrInput::open(const std::string& name, ImageSpec& newspec,
+               const ImageSpec& config)
+{
+    // Check 'config' for any special requests
+    ioproxy_retrieve_from_config(config);
+    return open(name, newspec);
+}
+
+
+
 bool
 HdrInput::open(const std::string& name, ImageSpec& newspec)
 {
     m_filename = name;
 
-    bool ok = seek_subimage(0, 0);
-    newspec = spec();
-    return ok;
+    if (!ioproxy_use_or_open(name))
+        return false;
+
+    m_io_pos = iotell();
+
+    // hdr always makes a 3 channel float image.
+    // RGBE_ReadHeader will set the width and height.
+    m_spec = ImageSpec(1, 1, 3, TypeDesc::FLOAT);
+
+    if (!RGBE_ReadHeader()) {
+        close();
+        return false;
+    }
+    m_spec.full_width  = m_spec.width;
+    m_spec.full_height = m_spec.height;
+
+    // FIXME -- should we do anything about exposure, software,
+    // pixaspect, primaries?  (N.B. rgbe.c doesn't even handle most of them)
+
+    m_next_scanline = 0;
+    m_scanline_offsets.clear();
+    m_scanline_offsets.push_back(m_io_pos);
+
+    m_subimage = 0;
+    newspec    = spec();
+    return true;
 }
 
 
@@ -109,53 +192,212 @@ HdrInput::seek_subimage(int subimage, int miplevel)
         return true;
     }
 
-    close();
+    return true;
+}
 
-    // Check that file exists and can be opened
-    m_fd = Filesystem::fopen(m_filename, "rb");
-    if (m_fd == NULL) {
-        errorf("Could not open file \"%s\"", m_filename);
+
+
+bool
+HdrInput::RGBE_ReadHeader()
+{
+    char buffer[128];
+    string_view line = fgets(buffer);
+    if (!line.size())
+        return false;
+    if (!Strutil::parse_prefix(line, "#?")) {
+        // if you want to require the magic token then uncomment the next lines
+        // errorfmt("RGBE header read error (line = '{}')", line);
+        // return false;
+    }
+    /* string_view programtype = */ Strutil::parse_until(line);
+
+    line = fgets(buffer);
+    if (!line.size())
+        return false;
+
+    m_spec.attribute("oiio:ColorSpace", "linear");  // presume linear
+    bool found_FORMAT_line = false;
+    for (int nlines = 0; nlines < 100 /* safety */; ++nlines) {
+        if (line.size() == 0 || line[0] == '\n')  // stop at blank line
+            break;
+        float tempf;
+        if (line == "FORMAT=32-bit_rle_rgbe\n") {
+            found_FORMAT_line = true;
+            /* LG says no:    break;       // format found so break out of loop */
+        } else if (Strutil::parse_values(line, "GAMMA=", span<float>(tempf))) {
+            // Round gamma to the nearest hundredth to prevent stupid
+            // precision choices and make it easier for apps to make
+            // decisions based on known gamma values. For example, you want
+            // 2.2, not 2.19998.
+            float g = float(1.0 / tempf);
+            g       = roundf(100.0 * g) / 100.0f;
+            m_spec.attribute("oiio:Gamma", g);
+            if (g == 1.0f)
+                m_spec.attribute("oiio:ColorSpace", "linear");
+            else
+                m_spec.attribute("oiio:ColorSpace",
+                                 Strutil::sprintf("Gamma%.2g", g));
+
+        } else if (Strutil::parse_values(line,
+                                         "EXPOSURE=", span<float>(tempf))) {
+            m_spec.attribute("hdr:exposure", tempf);
+        }
+
+        line = fgets(buffer);
+        if (!line.size())
+            return false;
+    }
+    if (!found_FORMAT_line) {
+        errorfmt("no FORMAT specifier found");
         return false;
     }
-
-    rgbe_header_info h;
-    int width, height;
-    int r = RGBE_ReadHeader(m_fd, &width, &height, &h, rgbe_error);
-    if (r != RGBE_RETURN_SUCCESS) {
-        errorf("%s", rgbe_error);
-        close();
+    if (line != "\n") {
+        errorfmt("missing blank line after FORMAT specifier");
         return false;
     }
+    line = fgets(buffer);
+    if (!line.size())
+        return false;
 
-    m_spec = ImageSpec(width, height, 3, TypeDesc::FLOAT);
-
-    if (h.valid & RGBE_VALID_GAMMA) {
-        // Round gamma to the nearest hundredth to prevent stupid
-        // precision choices and make it easier for apps to make
-        // decisions based on known gamma values. For example, you want
-        // 2.2, not 2.19998.
-        float g = float(1.0 / h.gamma);
-        g       = roundf(100.0 * g) / 100.0f;
-        m_spec.attribute("oiio:Gamma", g);
-        if (g == 1.0f)
-            m_spec.attribute("oiio:ColorSpace", "linear");
-        else
-            m_spec.attribute("oiio:ColorSpace",
-                             Strutil::sprintf("Gamma%.2g", g));
+    int hw[2];
+    int orientation = 1;
+    if (Strutil::parse_values(line, "-Y", hw, "+X")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 1;
+    } else if (Strutil::parse_values(line, "-Y", hw, "-X")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 2;
+    } else if (Strutil::parse_values(line, "+Y", hw, "-X")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 3;
+    } else if (Strutil::parse_values(line, "+Y", hw, "+X")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 4;
+    } else if (Strutil::parse_values(line, "+X", hw, "-Y")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 5;
+    } else if (Strutil::parse_values(line, "+X", hw, "+Y")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 6;
+    } else if (Strutil::parse_values(line, "-X", hw, "+Y")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 7;
+    } else if (Strutil::parse_values(line, "-X", hw, "-Y")) {
+        m_spec.height = hw[0], m_spec.width = hw[1];
+        orientation = 8;
     } else {
-        // Presume linear color space
-        m_spec.attribute("oiio:ColorSpace", "linear");
+        errorfmt("missing image size specifier");
+        return false;
     }
-    if (h.valid & RGBE_VALID_ORIENTATION)
-        m_spec.attribute("Orientation", h.orientation);
+    m_spec.attribute("Orientation", orientation);
 
-    // FIXME -- should we do anything about exposure, software,
-    // pixaspect, primaries?  (N.B. rgbe.c doesn't even handle most of them)
+    return true;
+}
 
-    m_subimage      = subimage;
-    m_next_scanline = 0;
-    m_scanline_offsets.clear();
-    m_scanline_offsets.push_back(Filesystem::ftell(m_fd));
+
+
+/* simple read routine.  will not correctly handle run length encoding */
+bool
+HdrInput::RGBE_ReadPixels(float* data, int y, uint64_t numpixels)
+{
+    size_t size = 4 * numpixels;
+    unsigned char* rgbe;
+    OIIO_ALLOCATE_STACK_OR_HEAP(rgbe, unsigned char, size);
+    if (ioproxy()->pread(rgbe, size, m_io_pos) != size) {
+        errorfmt("Read error reading pixels on scanline {}", y);
+        return false;
+    }
+    m_io_pos += size;
+    for (uint64_t i = 0; i < numpixels; ++i)
+        rgbe2float(data[3 * i], data[3 * i + 1], data[3 * i + 2], &rgbe[4 * i]);
+    return true;
+}
+
+
+
+bool
+HdrInput::RGBE_ReadPixels_RLE(float* data, int y, int scanline_width,
+                              int num_scanlines)
+{
+    if ((scanline_width < 8) || (scanline_width > 0x7fff))
+        /* run length encoding is not allowed so read flat*/
+        return RGBE_ReadPixels(data, y, scanline_width * num_scanlines);
+
+    unsigned char rgbe[4], *ptr, *ptr_end;
+    int i, count;
+    unsigned char buf[2];
+    std::vector<unsigned char> scanline_buffer;
+    Filesystem::IOProxy* m_io = ioproxy();
+
+    /* read in each successive scanline */
+    while (num_scanlines > 0) {
+        if (m_io->pread(rgbe, sizeof(rgbe), m_io_pos) < sizeof(rgbe)) {
+            errorfmt("Read error on scanline {}", y);
+            return false;
+        }
+        m_io_pos += sizeof(rgbe);
+        if ((rgbe[0] != 2) || (rgbe[1] != 2) || (rgbe[2] & 0x80)) {
+            /* this file is not run length encoded */
+            rgbe2float(data[0], data[1], data[2], rgbe);
+            data += 3;
+            return RGBE_ReadPixels(data, y, scanline_width * num_scanlines - 1);
+        }
+        if ((((int)rgbe[2]) << 8 | rgbe[3]) != scanline_width) {
+            errorfmt("wrong scanline width for scanline {}", y);
+            return false;
+        }
+        scanline_buffer.resize(4 * scanline_width);
+
+        ptr = &scanline_buffer[0];
+        /* read each of the four channels for the scanline into the buffer */
+        for (i = 0; i < 4; i++) {
+            ptr_end = &scanline_buffer[(i + 1) * scanline_width];
+            while (ptr < ptr_end) {
+                if (m_io->pread(buf, 2, m_io_pos) < 2) {
+                    errorfmt("Read error on scanline {}", y);
+                    return false;
+                }
+                m_io_pos += 2;
+                if (buf[0] > 128) {
+                    /* a run of the same value */
+                    count = buf[0] - 128;
+                    if ((count == 0) || (count > ptr_end - ptr)) {
+                        errorfmt("bad scanline {} data", y);
+                        return false;
+                    }
+                    while (count-- > 0)
+                        *ptr++ = buf[1];
+                } else {
+                    /* a non-run */
+                    count = buf[0];
+                    if ((count == 0) || (count > ptr_end - ptr)) {
+                        errorfmt("bad scanline {} data", y);
+                        return false;
+                    }
+                    *ptr++ = buf[1];
+                    if (--count > 0) {
+                        if (m_io->pread(ptr, count, m_io_pos)
+                            < uint64_t(count)) {
+                            errorfmt("Read error on scanline {}", y);
+                            return false;
+                        }
+                        m_io_pos += count;
+                        ptr += count;
+                    }
+                }
+            }
+        }
+        /* now convert data from buffer into floats */
+        for (i = 0; i < scanline_width; i++) {
+            rgbe[0] = scanline_buffer[i];
+            rgbe[1] = scanline_buffer[i + scanline_width];
+            rgbe[2] = scanline_buffer[i + 2 * scanline_width];
+            rgbe[3] = scanline_buffer[i + 3 * scanline_width];
+            rgbe2float(data[0], data[1], data[2], rgbe);
+            data += 3;
+        }
+        num_scanlines--;
+    }
     return true;
 }
 
@@ -173,21 +415,18 @@ HdrInput::read_native_scanline(int subimage, int miplevel, int y, int /*z*/,
         // For random access, use cached file offsets of scanlines. This avoids
         // re-reading the same pixels many times over.
         m_next_scanline = std::min((size_t)y, m_scanline_offsets.size() - 1);
-        Filesystem::fseek(m_fd, m_scanline_offsets[m_next_scanline], SEEK_SET);
+        m_io_pos        = m_scanline_offsets[m_next_scanline];
     }
 
     while (m_next_scanline <= y) {
         // Keep reading until we've read the scanline we really need
-        int r = RGBE_ReadPixels_RLE(m_fd, (float*)data, m_spec.width, 1,
-                                    rgbe_error);
+        bool ok = RGBE_ReadPixels_RLE((float*)data, y, m_spec.width, 1);
         ++m_next_scanline;
         if ((size_t)m_next_scanline == m_scanline_offsets.size()) {
-            m_scanline_offsets.push_back(Filesystem::ftell(m_fd));
+            m_scanline_offsets.push_back(m_io_pos);
         }
-        if (r != RGBE_RETURN_SUCCESS) {
-            errorf("%s", rgbe_error);
+        if (!ok)
             return false;
-        }
     }
     return true;
 }
@@ -197,8 +436,11 @@ HdrInput::read_native_scanline(int subimage, int miplevel, int y, int /*z*/,
 bool
 HdrInput::close()
 {
-    if (m_fd)
-        fclose(m_fd);
+    // if (m_io_local) {
+    //     // If we allocated our own ioproxy, close it.
+    //     ioproxy_clear();
+    // }
+
     init();  // Reset to initial state
     return true;
 }
--- src/hdr.imageio/hdroutput.cpp
+++ src/hdr.imageio/hdroutput.cpp
@@ -7,7 +7,6 @@
 #include <cstdio>
 #include <iostream>
 
-#include "rgbe.h"
 #include <OpenImageIO/filesystem.h>
 #include <OpenImageIO/imageio.h>
 #include <OpenImageIO/strutil.h>
@@ -19,6 +18,10 @@ public:
     HdrOutput() { init(); }
     virtual ~HdrOutput() { close(); }
     virtual const char* format_name(void) const override { return "hdr"; }
+    virtual int supports(string_view feature) const override
+    {
+        return feature == "ioproxy";
+    }
     virtual bool open(const std::string& name, const ImageSpec& spec,
                       OpenMode mode) override;
     virtual bool write_scanline(int y, int z, TypeDesc format, const void* data,
@@ -29,16 +32,15 @@ public:
     virtual bool close() override;
 
 private:
-    FILE* m_fd;
     std::vector<unsigned char> scratch;
-    std::string rgbe_error;  // Buffer for RGBE library error msgs
     std::vector<unsigned char> m_tilebuffer;
 
-    void init(void)
-    {
-        m_fd = NULL;
-        rgbe_error.clear();
-    }
+    void init(void) { ioproxy_clear(); }
+
+    bool RGBE_WritePixels(float* data, int64_t numpixels);
+    bool RGBE_WriteBytes_RLE(unsigned char* data, int numbytes);
+    bool RGBE_WritePixels_RLE(float* data, int scanline_width,
+                              int num_scanlines);
 };
 
 
@@ -53,6 +55,149 @@ hdr_output_imageio_create()
 OIIO_EXPORT const char* hdr_output_extensions[] = { "hdr", "rgbe", nullptr };
 
 OIIO_PLUGIN_EXPORTS_END
+
+
+// convert float[3] to rgbe-encoded pixels
+inline void
+float2rgbe(unsigned char* rgbe, float red, float green, float blue)
+{
+    float v = red;
+    if (green > v)
+        v = green;
+    if (blue > v)
+        v = blue;
+    if (v < 1e-32) {
+        rgbe[0] = rgbe[1] = rgbe[2] = rgbe[3] = 0;
+    } else {
+        int e;
+        v       = frexpf(v, &e) * 256.0f / v;
+        rgbe[0] = (unsigned char)(red * v);
+        rgbe[1] = (unsigned char)(green * v);
+        rgbe[2] = (unsigned char)(blue * v);
+        rgbe[3] = (unsigned char)(e + 128);
+    }
+}
+
+
+inline void
+float2rgbe(unsigned char* rgbe, const float* rgb)
+{
+    return float2rgbe(rgbe, rgb[0], rgb[1], rgb[2]);
+}
+
+
+
+// The code below is only needed for the run-length encoded files.
+// Run length encoding adds considerable complexity but does
+// save some space.  For each scanline, each channel (r,g,b,e) is
+// encoded separately for better compression.
+bool
+HdrOutput::RGBE_WriteBytes_RLE(unsigned char* data, int numbytes)
+{
+    static const int MINRUNLENGTH = 4;
+    int cur, beg_run, run_count, old_run_count, nonrun_count;
+    unsigned char buf[2];
+
+    cur = 0;
+    while (cur < numbytes) {
+        beg_run = cur;
+        /* find next run of length at least 4 if one exists */
+        run_count = old_run_count = 0;
+        while ((run_count < MINRUNLENGTH) && (beg_run < numbytes)) {
+            beg_run += run_count;
+            old_run_count = run_count;
+            run_count     = 1;
+            while ((beg_run + run_count < numbytes) && (run_count < 127)
+                   && (data[beg_run] == data[beg_run + run_count]))
+                run_count++;
+        }
+        /* if data before next big run is a short run then write it as such */
+        if ((old_run_count > 1) && (old_run_count == beg_run - cur)) {
+            buf[0] = 128 + old_run_count; /*write short run*/
+            buf[1] = data[cur];
+            if (!iowrite(buf, 2))
+                return false;
+            cur = beg_run;
+        }
+        /* write out bytes until we reach the start of the next run */
+        while (cur < beg_run) {
+            nonrun_count = beg_run - cur;
+            if (nonrun_count > 128)
+                nonrun_count = 128;
+            buf[0] = nonrun_count;
+            if (!iowrite(buf, 1))
+                return false;
+            if (!iowrite(&data[cur], nonrun_count))
+                return false;
+            cur += nonrun_count;
+        }
+        /* write out next run if one was found */
+        if (run_count >= MINRUNLENGTH) {
+            buf[0] = 128 + run_count;
+            buf[1] = data[beg_run];
+            if (!iowrite(buf, 2))
+                return false;
+            cur += run_count;
+        }
+    }
+    return true;
+}
+
+
+
+bool
+HdrOutput::RGBE_WritePixels_RLE(float* data, int scanline_width,
+                                int num_scanlines)
+{
+    if (scanline_width < 8 || scanline_width > 0x7fff)
+        // run length encoding is not allowed so write flat
+        return RGBE_WritePixels(data, scanline_width * num_scanlines);
+    unsigned char* buffer;
+    OIIO_ALLOCATE_STACK_OR_HEAP(buffer, unsigned char, scanline_width * 4);
+    while (num_scanlines-- > 0) {
+        unsigned char rgbe[4];
+        rgbe[0] = 2;
+        rgbe[1] = 2;
+        rgbe[2] = scanline_width >> 8;
+        rgbe[3] = scanline_width & 0xFF;
+        if (!iowrite(rgbe, 4))
+            return false;
+        for (int i = 0; i < scanline_width; i++) {
+            float2rgbe(rgbe, data);
+            buffer[i]                      = rgbe[0];
+            buffer[i + scanline_width]     = rgbe[1];
+            buffer[i + 2 * scanline_width] = rgbe[2];
+            buffer[i + 3 * scanline_width] = rgbe[3];
+            data += 3;
+        }
+        // write out each of the four channels separately run length encoded
+        // first red, then green, then blue, then exponent
+        for (int i = 0; i < 4; i++) {
+            if (!RGBE_WriteBytes_RLE(&buffer[i * scanline_width],
+                                     scanline_width))
+                return false;
+        }
+    }
+    return true;
+}
+
+
+
+// Simple write routine that does not use run length encoding.
+// These routines can be made faster by allocating a larger buffer and
+// fread-ing and fwrite-ing the data in larger chunks.
+bool
+HdrOutput::RGBE_WritePixels(float* data, int64_t numpixels)
+{
+    unsigned char* rgbe;
+    OIIO_ALLOCATE_STACK_OR_HEAP(rgbe, unsigned char, 4 * numpixels);
+    for (int64_t i = 0; i < numpixels; ++i)
+        float2rgbe(&rgbe[4 * i], data + 3 * i);
+    if (!iowrite(rgbe, 4 * numpixels))
+        return false;
+    return true;
+}
+
 
 
 bool
@@ -88,33 +233,20 @@ HdrOutput::open(const std::string& name, const ImageSpec& newspec,
 
     m_spec.set_format(TypeDesc::FLOAT);  // Native rgbe is float32 only
 
-    m_fd = Filesystem::fopen(name, "wb");
-    if (m_fd == NULL) {
-        errorf("Could not open \"%s\"", name);
+    ioproxy_retrieve_from_config(m_spec);
+    if (!ioproxy_use_or_open(name))
         return false;
-    }
 
-    rgbe_header_info h;
-    h.valid = 0;
+    if (!iowritefmt("#?RADIANCE\n"))
+        return false;
 
-    // Most readers seem to think that rgbe files are valid only if they
-    // identify themselves as from "RADIANCE".
-    h.valid |= RGBE_VALID_PROGRAMTYPE;
-    Strutil::safe_strcpy(h.programtype, "RADIANCE", sizeof(h.programtype));
+    // FIXME -- should we do anything about orientation, gamma, exposure,
+    // software, pixaspect, primaries?
 
-    ParamValue* p;
-    p = m_spec.find_attribute("Orientation", TypeDesc::INT);
-    if (p) {
-        h.valid |= RGBE_VALID_ORIENTATION;
-        h.orientation = *(int*)p->data();
-    }
-
-    // FIXME -- should we do anything about gamma, exposure, software,
-    // pixaspect, primaries?  (N.B. rgbe.c doesn't even handle most of them)
-
-    int r = RGBE_WriteHeader(m_fd, m_spec.width, m_spec.height, &h, rgbe_error);
-    if (r != RGBE_RETURN_SUCCESS)
-        errorf("%s", rgbe_error);
+    if (!iowritefmt("FORMAT=32-bit_rle_rgbe\n\n"))
+        return false;
+    if (!iowritefmt("-Y {} +X {}\n", m_spec.height, m_spec.width))
+        return false;
 
     // If user asked for tiles -- which this format doesn't support, emulate
     // it by buffering the whole image.
@@ -130,12 +262,8 @@ bool
 HdrOutput::write_scanline(int /*y*/, int /*z*/, TypeDesc format,
                           const void* data, stride_t xstride)
 {
-    data  = to_native_scanline(format, data, xstride, scratch);
-    int r = RGBE_WritePixels_RLE(m_fd, (float*)data, m_spec.width, 1,
-                                 rgbe_error);
-    if (r != RGBE_RETURN_SUCCESS)
-        errorf("%s", rgbe_error);
-    return (r == RGBE_RETURN_SUCCESS);
+    data = to_native_scanline(format, data, xstride, scratch);
+    return RGBE_WritePixels_RLE((float*)data, m_spec.width, 1);
 }
 
 
@@ -154,7 +282,7 @@ HdrOutput::write_tile(int x, int y, int z, TypeDesc format, const void* data,
 bool
 HdrOutput::close()
 {
-    if (!m_fd) {  // already closed
+    if (!ioproxy_opened()) {  // already closed
         init();
         return true;
     }
@@ -168,8 +296,6 @@ HdrOutput::close()
         std::vector<unsigned char>().swap(m_tilebuffer);
     }
 
-    fclose(m_fd);
-    m_fd = NULL;
     init();
 
     return ok;
