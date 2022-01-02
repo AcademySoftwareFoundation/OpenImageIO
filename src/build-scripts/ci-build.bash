c66a5323523de70439129dc1ed4cf82666a77850
Larry Gritz <lg@larrygritz.com>
2022-01-02 14:50:25 -0800

    CI: Bump OCIO and fmt versions (#3258)
    New OpenColorIO and fmt versions are out.
    
    * Bump "latest releases" test to fmt 8.1.0, and OCIO 2.1.1.
    * Re-adjust CI test fmt versions across many tests to give a better
      spread over the supported releases.
    * Bump the default version to download and use if none is found to 8.0.0.

--- src/build-scripts/ci-build.bash
+++ src/build-scripts/ci-build.bash
@@ -8,6 +8,10 @@ if [[ "$USE_SIMD" != "" ]] ; then
     MY_CMAKE_FLAGS="$MY_CMAKE_FLAGS -DUSE_SIMD=$USE_SIMD"
 fi
 
+if [[ -n "$FMT_VERSION" ]] ; then
+    MY_CMAKE_FLAGS="$MY_CMAKE_FLAGS -DBUILD_FMT_VERSION=$FMT_VERSION"
+fi
+
 pushd build
 cmake .. -G "$CMAKE_GENERATOR" \
         -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
