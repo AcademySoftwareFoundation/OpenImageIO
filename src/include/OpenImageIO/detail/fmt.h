87f6f16a330a80b471c62d0012c98b834e3cf6bd
Larry Gritz <lg@larrygritz.com>
2022-03-14 21:44:36 -0700

    Custom fmt formatters for vector types
    I'm a total convert to fmt (and/or std::format, std::print) and slowly
    converting all prior use of sprintf and iostream to it.
    
    If I print an Imath::V3f or a simd::vfloat3,
    
        fmt::print ("v = {} {} {}\n", myvec[0], myvec[1], myvec[2]);
    
    That's ok, I guess, but it's clunky and a big pain to put the format
    specification for every element separately if it were a vfloat16,
    say. I would like to be able to do this:
    
        fmt::print ("v = {}\n", myvec);
    
    and in fact, it does print all elements (thanks to some nice behavior
    by fmt)!  But this does not work as expected:
    
        fmt::print ("v = {:.3f}\n", myvec);
    
    fmt's tricks will print all elements, but only using the default format
    spec, and the spec above applied so the "whole thing", not each element,
    if you see the distinction.
    
    So this patch sets up the custom formatter infrastructure to be able
    to print array-like things (and in particular, the Imath and
    OIIO::simd vector and matrix classes) by specifying the format spec
    used for the elements.
    
    The important elements to note:
    
    * detail/fmt.h adds custom formatter helpers for (a) any class with
      operator[] and size() ("index_formatter"); (b) any class that lacks
      these but nonetheless has an array-like memory layout and a known
      length ("array_formatter"). I also added a nonstandard extension to
      the format spec for these, which optionally lets you use a comma
      separator rather than just the space between elements.
    
    * These building blocks are used to specify custom formatters for
      the commonly-used Imath types (in our Imath.h) and our internal
      SIMD vector types (in simd.h).
    
    * simd.h also adds a few other goodies, including an `is_simd<>`
      template, and `size()` constexpr methods to each vector type. These
      make certain template metaprogramming tasks easier for these types.
    
    * A new "type_traits.h" header for collecting a bunch of little utility
      templates, such as `has_subscript<>` and `has_size_method<>`.

--- src/include/OpenImageIO/detail/fmt.h
+++ src/include/OpenImageIO/detail/fmt.h
@@ -6,6 +6,7 @@
 #define OIIO_FMT_H
 
 #include <OpenImageIO/platform.h>
+#include <OpenImageIO/type_traits.h>
 
 // We want the header-only implemention of fmt
 #ifndef FMT_HEADER_ONLY
@@ -42,3 +43,123 @@ OIIO_PRAGMA_WARNING_PUSH
 #include <OpenImageIO/detail/fmt/printf.h>
 
 OIIO_PRAGMA_WARNING_POP
+
+
+OIIO_NAMESPACE_BEGIN
+namespace pvt {
+
+
+// Custom inheritable parse() method used by fmt formatters. In addition to
+// saving the formatting spec, it also checks for a (nonstandard) optional
+// leading ',' and records it as a separator flag.
+struct format_parser_with_separator {
+    FMT_CONSTEXPR auto parse(fmt::format_parse_context& ctx)
+    {
+        auto beg = ctx.begin(), end = ctx.end();
+        if (beg != end && *beg == ',')
+            sep = *beg++;
+        auto it = beg;  // where's the close brace?
+        for (; it != end && *it != '}'; ++it)
+            ;
+        elem_fmt = fmt::string_view(beg, it - beg);
+        return it;
+    }
+
+protected:
+    fmt::string_view elem_fmt;
+    char sep = 0;
+};
+
+
+
+// fmtlib custom formatter that formats a type `T` that has array-like
+// semantics (must have a valid operator[] and size() method) by printing each
+// element according to the format spec. For example, if the object has 3
+// float elements and the spec is "{.3f}", then the output might be "1.234
+// 2.345 3.456".
+//
+// In addition to the usual formatting spec, we also recognize the following
+// special extension: If the first character of the format spec is ','
+// (comma), then the array elements will be separated by ", " rather than the
+// default " ".
+//
+// For example, `format("[{:,.3f}]")` will format the array as `[1.234, 2.345,
+// 3.456]`.
+//
+// Now then, the way this class is helpful is to inherit from it for a custom
+// formatter. For example, this will make this formatter be used for
+// Imath::V3f:
+//
+//     // in global namespace:
+//     template<> struct fmt::formatter<Imath::V3f>
+//         : OIIO::array_formatter<Imath::V3f, float, 3> {};
+//
+template<typename T,
+         OIIO_ENABLE_IF(has_subscript<T>::value&& has_size_method<T>::value)>
+struct index_formatter : format_parser_with_separator {
+    // inherits parse() from format_parser_with_separator
+    template<typename FormatContext> auto format(const T& v, FormatContext& ctx)
+    {
+        std::string vspec = elem_fmt.size() ? fmt::format("{{:{}}}", elem_fmt)
+                                            : std::string("{}");
+        for (size_t i = 0; i < v.size(); ++i) {
+            if (i)
+                fmt::format_to(ctx.out(), "{}", sep == ',' ? ", " : " ");
+#if FMT_VERSION >= 80000
+            fmt::format_to(ctx.out(), fmt::runtime(vspec), v[i]);
+#else
+            fmt::format_to(ctx.out(), vspec, v[i]);
+#endif
+        }
+        return ctx.out();
+    }
+};
+
+
+
+// fmtlib custom formatter that formats a type `T` as if it were an array
+// `Elem[Size]` (and it must be laid out that way in memory). The formatting
+// spec will apply to each element. For example, if the object has 3 float
+// elements and the spec is "{.3f}", then the output might be "1.234 2.345
+// 3.456".
+//
+// In addition to the usual formatting spec, we also recognize the following
+// special extension: If the first character of the format spec is ','
+// (comma), then the array elements will be separated by ", " rather than the
+// default " ".
+//
+// For example, `format("[{:,.3f}]")` will format the array as `[1.234, 2.345,
+// 3.456]`.
+//
+// Now then, the way this class is helpful is to inherit from it for a custom
+// formatter. For example, this will make this formatter be used for
+// Imath::V3f:
+//
+//     // in global namespace:
+//     template<> struct fmt::formatter<Imath::V3f>
+//         : OIIO::array_formatter<Imath::V3f, float, 3> {};
+//
+template<typename T, typename Elem, int Size>
+struct array_formatter : format_parser_with_separator {
+    // inherits parse() from format_parser_with_separator
+    template<typename FormatContext> auto format(const T& v, FormatContext& ctx)
+    {
+        std::string vspec = elem_fmt.size() ? fmt::format("{{:{}}}", elem_fmt)
+                                            : std::string("{}");
+        for (int i = 0; i < Size; ++i) {
+            if (i)
+                fmt::format_to(ctx.out(), "{}", sep == ',' ? ", " : " ");
+#if FMT_VERSION >= 80000
+            fmt::format_to(ctx.out(), fmt::runtime(vspec),
+                           ((const Elem*)&v)[i]);
+#else
+            fmt::format_to(ctx.out(), vspec, ((const Elem*)&v)[i]);
+#endif
+        }
+        return ctx.out();
+    }
+};
+
+
+}  // namespace pvt
+OIIO_NAMESPACE_END
