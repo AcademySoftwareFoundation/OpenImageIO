41eceb64acb85ca802529642ad050b5b934a590d
Anders Langlands <anderslanglands@gmail.com>
2022-07-11 12:54:43 +1200

    enable dll loading from PATH by default

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -1,10 +1,10 @@
 import os, sys, platform
 
-# This works around python 3.8 change to stop loading DLLs from PATH on Windows.
+# This works around the python 3.8 change to stop loading DLLs from PATH on Windows.
 # We reproduce the old behaviour by manually tokenizing PATH, checking that the directories exist and are not ".",
 # then add them to the DLL load path.
-# This behviour is only enabled if the environment variable "OIIO_LOAD_DLLS_FROM_PATH" is set to "1" so it is opt-in.
-if sys.version_info >= (3, 8) and platform.system() == "Windows" and os.getenv("OIIO_LOAD_DLLS_FROM_PATH", "0") == "1":
+# This behviour can be disablef by setting the environment variable "OIIO_LOAD_DLLS_FROM_PATH" to "0" 
+if sys.version_info >= (3, 8) and platform.system() == "Windows" and os.getenv("OIIO_LOAD_DLLS_FROM_PATH", "1") == "1":
     for path in os.getenv("PATH", "").split(os.pathsep):
         if os.path.exists(path) and path != ".":
             os.add_dll_directory(path)
