2e88801fa52b0c48d493d2e83eac171d3d1dd264
Johannes Unterguggenberger <112864721+johannesunterguggenberger-huawei@users.noreply.github.com>
2022-09-05 14:38:34 +0200

    Fixes a warning from an unsuitable format specifier
    `DASSERT_MSG` is was used with the format specifier `%zd` which is unsuitable for either of the options that `oiio_span_size_type` could evaluate to. This PR should fix it for both cases (`OIIO_SPAN_SIZE_IS_UNSIGNED` is defined and not defined) by using the appropriate format specifier in each case.

--- src/include/OpenImageIO/fmath.h
+++ src/include/OpenImageIO/fmath.h
@@ -2419,7 +2419,13 @@ inline OIIO_HOSTDEVICE float
 interpolate_linear (float x, span_strided<const float> y)
 {
 #ifndef __CUDA_ARCH__
-    DASSERT_MSG (y.size() >= 2, "interpolate_linear needs at least 2 knot values (%zd)", y.size());
+#ifdef OIIO_SPAN_SIZE_IS_UNSIGNED
+    // using oiio_span_size_type = size_t; => %zu is suitable
+    DASSERT_MSG (y.size() >= 2, "interpolate_linear needs at least 2 knot values (%zu)", y.size());
+#else
+    // using oiio_span_size_type = ptrdiff_t; => %td is suitable
+    DASSERT_MSG (y.size() >= 2, "interpolate_linear needs at least 2 knot values (%td)", y.size());
+#endif
 #endif
     x = clamp (x, float(0.0), float(1.0));
     int nsegs = int(y.size()) - 1;
