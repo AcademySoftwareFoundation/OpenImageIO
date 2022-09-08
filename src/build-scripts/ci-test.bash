a028a11a0b08c9d1efcd8b72379bfe50889141c3
Larry Gritz <lg@larrygritz.com>
2022-09-07 21:31:43 -0700

    CI: Re-enable sanitizer tests (#3545)
    Oops, at some point, something about the way we set up the CI test
    that runs the sanitizers, and ever since then (embarrassingly, perhaps
    as much as a couple years ago), the "sanitizer" test has not in fact
    been running the sanitizers. Here we fix that up, get sanitizers
    running again.

--- src/build-scripts/ci-test.bash
+++ src/build-scripts/ci-test.bash
@@ -4,10 +4,17 @@
 # any command in it fails. This is crucial for CI tests.
 set -ex
 
+: ${CTEST_EXCLUSIONS:="broken"}
+: ${CTEST_TEST_TIMEOUT:=180}
+
 $OpenImageIO_ROOT/bin/oiiotool --help
 
-echo "Parallel test " ${CTEST_PARALLEL_LEVEL}
+echo "Parallel test ${CTEST_PARALLEL_LEVEL}"
+echo "Default timeout ${CTEST_TEST_TIMEOUT}"
+echo "Test exclusions '${CTEST_EXCLUSIONS}'"
+echo "CTEST_ARGS '${CTEST_ARGS}'"
+
 pushd build
-time ctest -C ${CMAKE_BUILD_TYPE} -E broken --force-new-ctest-process \
-    --output-on-failure --timeout ${CTEST_TEST_TIMEOUT:=180} ${CTEST_ARGS}
+time ctest -C ${CMAKE_BUILD_TYPE} --force-new-ctest-process --output-on-failure \
+    -E "${CTEST_EXCLUSIONS}" --timeout ${CTEST_TEST_TIMEOUT} ${CTEST_ARGS}
 popd
