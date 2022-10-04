0d15247b56d67366b7829ce5537615708a98fe57
Aras Pranckevicius <aras@nesnausk.org>
2022-10-04 13:21:14 +0300

    DDS: extend test coverage for more formats & channel size cases
    With more test images recently added (https://github.com/OpenImageIO/oiio/pull/3459),
    start using them for DDS tests. This covers now:
    - Alpha, Luminance and Alpha+Luminance formats,
    - RGB formats with rarer channel sizes (rgb10 a2, r3g3b2),
    - Several "broken" DDS file cases
    - Removed usage of sample-DXT1 since it is well covered by others.

--- testsuite/dds/run.py
+++ testsuite/dds/run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
+failureok = 1
+redirect = ' >> out.txt 2>&1 '
 files = [
-    "sample-DXT1.dds",
     "dds_bc1.dds",
     "dds_bc1_mips.dds",
     "dds_bc2.dds",
@@ -20,6 +21,25 @@ files = [
     "dds_rgb8.dds",
     "dds_rgba4.dds",
     "dds_rgba8.dds",
-    "dds_rgba8_mips.dds" ]
+    "dds_rgba8_mips.dds",
+    "dds_a8.dds",
+    "dds_abgr8.dds",
+    "dds_bc3nm.dds",
+    "dds_bc3rxgb.dds",
+    "dds_bc3ycocg.dds",
+    "dds_l8.dds",
+    "dds_l8a8.dds",
+    "dds_rgb10a2.dds",
+    "dds_rgb332.dds",
+    "dds_rgb5a1.dds",
+    # In some CI matrix cases a PtexReader
+    # also attempts to read this, and outputs
+    # a "PtexReader error: read failed (EOF)"
+    # failure error.
+    #"broken/dds_8bytes.dds",
+    "broken/dds_bc3_just_header.dds",
+    "broken/dds_bc3_no_full_header.dds",
+    "broken/dds_bc7_just_header.dds",
+    "broken/dds_bc7_not_enough_data.dds" ]
 for f in files:
     command += info_command (OIIO_TESTSUITE_IMAGEDIR + "/" + f)
