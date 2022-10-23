acfb6a9e287eaaa8f0883d25551f3ad081344f8d
Larry Gritz <lg@larrygritz.com>
2022-10-23 13:08:12 -0700

    TIFF: guard against corrupt files with buffer overflows (#3628)
    Along the way, improve the runtest.py script by giving the
    info_command() fuction the same silent, concat, and failureok
    parameters that the oiiotool() function has. (This lets individual
    tests assemble iinfo command lines with a little more flexibility.)

--- testsuite/tiff-misc/run.py
+++ testsuite/tiff-misc/run.py
@@ -2,14 +2,19 @@
 
 # Miscellaneous TIFF-related tests
 
-outputs = [ ]
+# save the error output
+redirect = " >> out.txt 2>&1 "
 
 # Regression test -- we once had a bug where 'separate' planarconfig
 # tiled float files would have data corrupted by a buffer overwrite.
 command += oiiotool("--pattern checker 128x128 4 --tile 64 64 --planarconfig separate -d float -o check1.tif")
-outputs += [ "check1.tif" ]
 
 # Test bug we had until OIIO 2.3 when reading planarconfig=separate files
 # (fixed by #2757) that was not detected by the uncompressed file. So copy
 # to force compression in order to properly test:
 command += rw_command ("src", "separate.tif")
+
+# Test bug we had until OIIO 2.4 for this corrupt file
+command += oiiotool ("--oiioattrib try_all_readers 0 --info -v src/corrupt1.tif", failureok = True)
+
+outputs = [ "check1.tif", "out.txt" ]
