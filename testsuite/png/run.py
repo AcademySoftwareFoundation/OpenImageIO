8cfa5f90b57980e87aa7f3775120df121b36467c
Joris Nijs <jorisnijs@gmail.com>
2023-01-08 20:46:12 +0100

    Add EXIF write support to PNG output. (#3736)
    * Add Exif write support to PNG output.
    
    Stores any Exif data from the given ImageSpec into the PNG eXIf chunk.
    
    * add test for writing Exif data in png
    
    * add test reference output for libpng versions not supporting the eXIf chunk.
    
    * Update testsuite/png/ref/out-libpng15.txt
    
    Co-authored-by: Joris Nijs <joris.nijs@esko.com>

--- testsuite/png/run.py
+++ testsuite/png/run.py
@@ -6,3 +6,10 @@ redirect = " >> out.txt 2>&1 "
 files = [ "oiio-logo-no-alpha.png",  "oiio-logo-with-alpha.png" ]
 for f in files:
         command += rw_command (OIIO_TESTSUITE_IMAGEDIR,  f)
+
+# test writing of exif data
+command += oiiotool ("--create 64x64 3 " +
+                     "--attrib Exif:WhiteBalance 0 " +
+                     "--attrib Exif:FocalLength 45.7 " +
+                     "-o exif.png")
+command += info_command ("exif.png", safematch=True)
