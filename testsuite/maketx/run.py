4f180d13f8aef3344f3bea7de7cb0ec763c64093
Larry Gritz <lg@larrygritz.com>
2023-01-21 15:22:18 -0800

    Improve OCIO (especially 2.2) support and better testing (#3755)
    Tweak the heuristics for trying to determine which arbitrary-named
    color spaces are equivalent to certain known common names.
    
    For OCIO 2.2+, some of this is easier and more reliable by using the
    new built-in color spaces -- for example, we can transform a set of
    sentinel colors from an arbitrary cs into the known built-in acescg,
    or lin_srgb, etc., and if the values don't change, we know that cs
    *is* the canonical one, even if named something weird.
    
    But one problem is that for older OCIO, the built-in config is not
    present, so if you have a color space named, I dunno, "linear", it's
    hard to know what that is. Is it what we now call lin_srgb? Is it
    ACEScg?  Something else? So the hack is: first, we try to identify a
    space that we think is sRGB (almost all configs have something called
    "srgb", because everybody needs it, and we take it at face value that
    if they call a cs "srgb", it's the real "srgb", fools who use the name
    for something else get what they deserve). We transform the sentinel
    colors from the srgb into the cs we are interrogating, and if they
    match what we expect to see from an srgb->lin_srgb transformation,
    then guess what -- the unknown color space must be lin_srgb!
    
    We may, in the future, expand this to deduce other spaces. But this is
    a start.
    
    There are other changes I made here to make this whole process much
    less expensive than it had been getting, there was some redundant
    work, and some work that only was useful for 2.2 or only useful for <=
    2.1, so I only do those in the right circumstances now.
    
    Also along for the ride: improve test coverage for maketx features of
    colorconvert, unpremult, attrib, attrib.

--- testsuite/maketx/run.py
+++ testsuite/maketx/run.py
@@ -140,18 +140,23 @@ command += maketx_command ("noise.exr", "cdf.exr",
 
 # Test --sattrib, --attrib
 command += maketx_command ("checker.tif", "checker-attribs.tx",
-                           "--sattrib sattr sname sval " +
-                           "--attrib iattr iname 42 " +
-                           "--attrib fattr iname 3.14159 " +
-                           "--attrib sattr2 sval2 ",
+                           "--sattrib sname sval " +
+                           "--attrib iname 42 " +
+                           "--attrib fname 3.14159 " +
+                           "--attrib sname2 sval2 ",
                            showinfo = True)
 
+# Test --colorconvert and --unpremult
+command += oiiotool (" --pattern constant:color=.25,.25,.25,.5 64x64 4 "
+                    + " -d uint8 -o " + make_relpath("gray64srgb.tif"))
+command += maketx_command ("gray64srgb.tif", "gray64linsrgb.tx",
+                           "--colorconvert srgb lin_srgb --unpremult")
 
 outputs += [ "out.txt" ]
 
 
 
 # To do:  --fullpixels
-#         --prman-metadata --ignore-unassoc
+#         --ignore-unassoc
 #         --mipimage 
-#         --colorconvert --unpremult -u --fovcot
+#         --fovcot
