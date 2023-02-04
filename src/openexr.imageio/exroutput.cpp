bd319821e8015f39f7852b12efc1eafa91917bba
jreichel-nvidia <43646584+jreichel-nvidia@users.noreply.github.com>
2023-02-04 23:51:17 +0100

    Initialize OpenEXROutput::m_levelmode() in init(). (#3764)
    Avoids conditional jump on uninitialised values in close().
    
    Co-authored-by: Joachim Reichel <joachim.reichel@posteo.de>

--- src/openexr.imageio/exroutput.cpp
+++ src/openexr.imageio/exroutput.cpp
@@ -190,8 +190,9 @@ private:
         m_tiled_output_part.reset();
         m_deep_scanline_output_part.reset();
         m_deep_tiled_output_part.reset();
-        m_subimage = -1;
-        m_miplevel = -1;
+        m_levelmode = Imf::ONE_LEVEL;
+        m_subimage  = -1;
+        m_miplevel  = -1;
         m_subimagespecs.clear();
         m_subimagespecs.shrink_to_fit();
         m_headers.clear();
