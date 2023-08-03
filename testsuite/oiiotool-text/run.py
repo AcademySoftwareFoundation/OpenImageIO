1bb1ca591a145a120db43af2c10e549cd1c6be82
Nicolas <nicolas@vivien.fr>
2023-08-03 19:46:16 +0200

    fix: utf-8 text rendering (#3935)
    Text rendering doesn't work with utf-8... 
    
    I can't render chinese, arabic... or special chars as éèûîà...
    
    It seems that the string function to convert utf-8 to unicode is quiet
    simple.
    
    Fixes #3933 
    
    [Note]: OpenImageIO is used in the [gpx2video
    project](https://github.com/progweb/gpx2video).
    
    ---------
    
    Signed-off-by: Nicolas VIVIEN <nicolas@vivien.fr>
    Signed-off-by: Larry Gritz <lg@larrygritz.com>
    Co-authored-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/oiiotool-text/run.py
+++ testsuite/oiiotool-text/run.py
@@ -13,7 +13,7 @@ import os
 command += oiiotool ("--create 320x240 3 "
             "--text:x=25:y=50:font=DroidSerif \"Hello, world\" "
             "--text:x=50:y=120:color=1,0,0:size=42 \"Go Big Red!\" "
-            "-d uint8 -o text.tif >> out.txt")
+            "-d uint8 -o text.tif")
 
 # test alignment
 command += oiiotool ("--create 320x320 3 "
@@ -25,22 +25,33 @@ command += oiiotool ("--create 320x320 3 "
             "--text:x=200:y=100:xalign=center:yalign=baseline:size=20 Center "
             "--text:x=100:y=200:xalign=right:yalign=bottom:size=20 Rightbot "
             "--text:x=200:y=200:xalign=left:yalign=baseline:size=20 Default "
-            "-d uint8 -o aligned.tif >> out.txt")
+            "-d uint8 -o aligned.tif")
 
 # test shadow
 command += oiiotool ("../common/tahoe-tiny.tif "
             "--text:x=64:y=20:xalign=center:size=20:shadow=0 \"shadow = 0\" "
             "--text:x=64:y=40:xalign=center:size=20:shadow=1 \"shadow = 1\" "
             "--text:x=64:y=60:xalign=center:size=20:shadow=2 \"shadow = 2\" "
-            "-o textshadowed.tif >> out.txt")
+            "-o textshadowed.tif")
 
 # test whether alpha works properly
 command += oiiotool ("--pattern checker:color1=.2,0,0:color2=0,.2,0 320x240 3 "
             "--text:x=20:y=120:color=.5,.5,.5,.5:size=50 \"Hello, world\" "
-            "-d uint8 -o textalpha.tif >> out.txt")
-
+            "-d uint8 -o textalpha.tif")
 
 # Outputs to check against references
 outputs = [ "text.tif", "aligned.tif", "textshadowed.tif", "textalpha.tif" ]
+
+# Test Unicode characters. But only when not on Windows, because I just
+# can't figure out how to get Unicode chars on the command line properly
+# on Windows.
+if os.name != "nt" :
+    command += oiiotool ("--create 320x240 3 "
+                "--text:x=25:y=50:font=DroidSerif 'test: éèúçàïö€' "
+                "-d uint8 -o unicode.tif")
+    outputs += [ "unicode.tif" ]
+
+# Don't test out.txt because we have multiple reference outputs
+# outputs += [ "out.txt" ]
 
 #print "Running this command:\n" + command + "\n"
