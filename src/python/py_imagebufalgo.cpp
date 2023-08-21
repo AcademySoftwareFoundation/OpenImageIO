66d178be31346a91b6bd73595d6a34dce04e9eb5
ssh4net <libalias@gmail.com>
2023-08-21 22:30:29 +0900

    Update py_imagebufalgo.cpp
    python bindings
    
    Signed-off-by: ssh4net <libalias@gmail.com>

--- src/python/py_imagebufalgo.cpp
+++ src/python/py_imagebufalgo.cpp
@@ -1700,6 +1700,28 @@ IBA_complex_to_polar_ret(const ImageBuf& src, ROI roi, int nthreads)
 
 
 bool
+IBA_normalize(ImageBuf& dst, const ImageBuf& src, float inCenter,
+              float outCenter, float scale, ROI roi, int nthreads)
+{
+	py::gil_scoped_release gil;
+	return ImageBufAlgo::normalize(dst, src, inCenter, outCenter, 
+                                   scale, roi, nthreads);
+}
+
+
+
+ImageBuf
+IBA_normalize_ret(const ImageBuf& src, float inCenter, float outCenter,
+                  float scale, ROI roi, int nthreads)
+{
+	py::gil_scoped_release gil;
+	return ImageBufAlgo::normalize(src, inCenter, outCenter, scale, roi,
+        								   nthreads);
+}
+
+
+
+bool
 IBA_fillholes_pushpull(ImageBuf& dst, const ImageBuf& src, ROI roi,
                        int nthreads)
 {
@@ -2958,6 +2980,13 @@ declare_imagebufalgo(py::module& m)
         .def_static("fixNonFinite", &IBA_fixNonFinite_ret, "src"_a,
                     "mode"_a = ImageBufAlgo::NONFINITE_BOX3,
                     "roi"_a = ROI::All(), "nthreads"_a = 0)
+
+        .def_static("normalize", &IBA_normalize, "dst"_a, "src"_a, 
+                    "incenter"_a = 0.0f, "outcenter"_a = 0.0f, "scale"_a = 1.0f,
+                    "roi"_a = ROI::All(), "nthreads"_a = 0)
+        .def_static("normalize", &IBA_normalize_ret, "src"_a,
+                    "incenter"_a = 0.0f, "outcenter"_a = 0.0f, "scale"_a = 1.0f,
+					"roi"_a = ROI::All(), "nthreads"_a = 0)
 
         .def_static("fillholes_pushpull", &IBA_fillholes_pushpull, "dst"_a,
                     "src"_a, "roi"_a = ROI::All(), "nthreads"_a = 0)
