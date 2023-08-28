83c885309c34166a012b8d9bbdea03bd23ec31b1
Larry Gritz <lg@larrygritz.com>
2023-08-27 20:31:32 -0700

    build(win): Fix windows.h pre-definitions (#3965)
    * Avoid possible double define of NOMINMAX in platform.h
    * Move other Windows symbol definitions into the cmake file instead of
      repeating them in certain .cpp files.
    
    Fixes #3964
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/libutil/strutil.cpp
+++ src/libutil/strutil.cpp
@@ -32,12 +32,8 @@
 #include <OpenImageIO/ustring.h>
 
 #ifdef _WIN32
-#    define WIN32_LEAN_AND_MEAN
-#    define VC_EXTRALEAN
-#    define NOMINMAX
-#    include <windows.h>
-
 #    include <shellapi.h>
+#    include <windows.h>
 #endif
 
 
