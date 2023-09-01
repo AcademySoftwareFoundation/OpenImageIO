AcademySoftwareFoundation/OpenImageIO

commit 150af5a36ec82cb2711ad927a8e7ccd08c192cb2

ci: Fix broken heif dependency and test (#3894)
The strukturag repo for libheif recently upgraded the version it was
serving from 1.14 to 1.16, and that caused our tests to fail because (a)
some of the codec support has been moved to plugins that are loaded at
runtime and those are now distributed in packages we need to install
separately; (b) you must call heif_init() or it won't know about the
optional plugins.

Signed-off-by: Larry Gritz <lg@larrygritz.com>

src/heif.imageio/heifinput.cpp
@@ -59,6 +59,17 @@ private:
 
 
 
+void
+oiio_heif_init()
+{
+#if LIBHEIF_HAVE_VERSION(1, 6, 0)
+    static std::once_flag flag;
+    std::call_once(flag, []() { heif_init(nullptr); });
+#endif
+}
+
+
+
 // Export version number and create function symbols
 OIIO_PLUGIN_EXPORTS_BEGIN
 
@@ -73,6 +84,7 @@ heif_imageio_library_version()
 OIIO_EXPORT ImageInput*
 heif_input_imageio_create()
 {
+    oiio_heif_init();
     return new HeifInput;
 }
 

src/build-scripts/gh-installdeps.bash
@@ -97,7 +97,9 @@ else
     if [[ "$USE_LIBHEIF" != "0" ]] ; then
        sudo add-apt-repository ppa:strukturag/libde265 || true
        sudo add-apt-repository ppa:strukturag/libheif || true
-       time sudo apt-get -q install -y libheif-dev || true
+       time sudo apt-get -q install -y libheif-plugin-aomdec \
+            libheif-plugin-aomenc libheif-plugin-libde265 \
+            libheif-plugin-x265 libheif-dev || true
     fi
 
     export CMAKE_PREFIX_PATH=/usr/lib/x86_64-linux-gnu:$CMAKE_PREFIX_PATH

src/heif.imageio/heifoutput.cpp
@@ -70,11 +70,14 @@ private:
 }  // namespace
 
 
+
 OIIO_PLUGIN_EXPORTS_BEGIN
 
 OIIO_EXPORT ImageOutput*
 heif_output_imageio_create()
 {
+    extern void oiio_heif_init();
+    oiio_heif_init();
     return new HeifOutput;
 }
 

commit 0246a1bc8b13f57a7ffaf5c6f66fdef1baa3a5a9

build: Fix broken libheif < 1.13 (#3970)
Typo introduced in #3894 broke our ability to build against libheif <
1.13. A conditional compilation that should have kicked in for >=1.16
was mistakenly written as 1.6.

Signed-off-by: Larry Gritz <lg@larrygritz.com>

src/heif.imageio/heifinput.cpp
@@ -62,7 +62,7 @@ private:
 void
 oiio_heif_init()
 {
-#if LIBHEIF_HAVE_VERSION(1, 6, 0)
+#if LIBHEIF_HAVE_VERSION(1, 16, 0)
     static std::once_flag flag;
     std::call_once(flag, []() { heif_init(nullptr); });
 #endif