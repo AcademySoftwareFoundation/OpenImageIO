445bef5e9a6c2a06215c924349b7d5a045bb734c
Larry Gritz <lg@larrygritz.com>
2023-09-07 23:17:00 -0700

    Fixes for sanitize test and windows test
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/docs-examples-cpp/run.py
+++ testsuite/docs-examples-cpp/run.py
@@ -6,12 +6,12 @@
 
 
 # command += "echo test_source_dir=" + test_source_dir + " >> build.txt ;"
-command += run_app("cmake -S data -B build -DCMAKE_BUILD_TYPE=Release >> build.txt 2>&1", silent=True)
-command += run_app("cmake --build build --config Release >> build.txt 2>&1", silent=True)
+command += run_app("cmake " + test_source_dir + " -DCMAKE_BUILD_TYPE=Release >> build.txt 2>&1", silent=True)
+command += run_app("cmake --build . --config Release >> build.txt 2>&1", silent=True)
 if platform.system() == 'Windows' :
-    command += run_app("build\\Release\\docs-examples-imageoutput")
+    command += run_app("Release\\docs-examples-imageoutput")
 else :
-    command += run_app("./build/docs-examples-imageoutput")
+    command += run_app("./docs-examples-imageoutput")
 
 outputs = [ "simple.tif", "scanlines.tif",
             "out.txt" ]
