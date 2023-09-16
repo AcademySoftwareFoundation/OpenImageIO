8a595bc583df8f9975d19a0b06bf355422d4e863
Larry Gritz <lg@larrygritz.com>
2023-09-16 14:26:39 -0700

    build: Use exported targets for libjpeg-turbo and bump min to 2.1
    libjpeg-turbo has exported cmake configs since its version 2.1.
    Relying on this simplifies the build and allows us to completely
    remove the troublesome Findlibjpeg-turbo.cmake. This implies raising
    the floor for libjpeg-turbo support to 2.1 (the current release is
    3.0) which was released in April 2021 (beta in Nov 2020).
    
    That's cutting it a little close (we usually support dependencies
    going back at least 3 years at a minimum, and often significantly
    longer).  But since this is an optional dependency anyway and
    simplifies the build, I'm tempted to do so.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/build-scripts/build_libjpeg-turbo.bash
+++ src/build-scripts/build_libjpeg-turbo.bash
@@ -11,14 +11,15 @@ set -ex
 
 # Repo and branch/tag/commit of libjpeg-turbo to download if we don't have it yet
 LIBJPEGTURBO_REPO=${LIBJPEGTURBO_REPO:=https://github.com/libjpeg-turbo/libjpeg-turbo.git}
-LIBJPEGTURBO_VERSION=${LIBJPEGTURBO_VERSION:=2.0.5}
+LIBJPEGTURBO_VERSION=${LIBJPEGTURBO_VERSION:=3.0.0}
 
 # Where to put libjpeg-turbo repo source (default to the ext area)
-LIBJPEGTURBO_SRC_DIR=${LIBJPEGTURBO_SRC_DIR:=${PWD}/ext/libjpeg-turbo}
+LOCAL_DEPS_DIR=${LOCAL_DEPS_DIR:=${PWD}/ext}
+LIBJPEGTURBO_SRC_DIR=${LIBJPEGTURBO_SRC_DIR:=${LOCAL_DEPS_DIR}/libjpeg-turbo}
 # Temp build area (default to a build/ subdir under source)
 LIBJPEGTURBO_BUILD_DIR=${LIBJPEGTURBO_BUILD_DIR:=${LIBJPEGTURBO_SRC_DIR}/build}
 # Install area for libjpeg-turbo (default to ext/dist)
-LIBJPEGTURBO_INSTALL_DIR=${LIBJPEGTURBO_INSTALL_DIR:=${PWD}/ext/dist}
+LIBJPEGTURBO_INSTALL_DIR=${LIBJPEGTURBO_INSTALL_DIR:=${LOCAL_DEPS_DIR}/dist}
 #LIBJPEGTURBO_CONFIG_OPTS=${LIBJPEGTURBO_CONFIG_OPTS:=}
 
 pwd
@@ -50,4 +51,5 @@ popd
 # Set up paths. These will only affect the caller if this script is
 # run with 'source' rather than in a separate shell.
 export JPEGTurbo_ROOT=$LIBJPEGTURBO_INSTALL_DIR
+export libjpegturbo_ROOT=$LIBJPEGTURBO_INSTALL_DIR
 
