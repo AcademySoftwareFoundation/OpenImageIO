69c412e6f49e56e3b5325db6a1826124e854de98
Larry Gritz <lg@larrygritz.com>
2023-09-21 16:22:18 -0700

    testing: Set up placeholders for docs-examples-are-tests (#3994)
    I had previously prototyped an instance of this for the ImageOutput
    chapter.
    
    This patch makes empty placeholders for all of the other API chapters.
    They don't contain any actual tests or generate any output now, but they
    do build and run, so people can drop new examples into them without
    having to worry about how to set up new files for each chapter.
    
    ---------
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/docs-examples-cpp/src/docs-examples-imageoutput.cpp
+++ testsuite/docs-examples-cpp/src/docs-examples-imageoutput.cpp
@@ -1,3 +1,37 @@
+// Copyright Contributors to the OpenImageIO project.
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/OpenImageIO/oiio
+
+
+///////////////////////////////////////////////////////////////////////////
+// This file contains code examples from the ImageOutput chapter of the
+// main OpenImageIO documentation.
+//
+// To add an additional test, replicate the section below. Change
+// "example1" to a helpful short name that identifies the example.
+
+// BEGIN-imageoutput-example1
+#include <OpenImageIO/imageio.h>
+using namespace OIIO;
+
+void example1()
+{
+    //
+    // Example code fragment from the docs goes here.
+    //
+    // It probably should generate either some text output (which will show up
+    // in "out.txt" that captures each test's output), or it should produce a
+    // (small) image file that can be compared against a reference image that
+    // goes in the ref/ subdirectory of this test.
+    //
+}
+// END-imageoutput-example1
+
+//
+///////////////////////////////////////////////////////////////////////////
+
+
+
 // BEGIN-imageoutput-simple
 #include <OpenImageIO/imageio.h>
 using namespace OIIO;
