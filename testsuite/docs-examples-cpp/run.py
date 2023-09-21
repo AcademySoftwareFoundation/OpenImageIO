3948280c6a8d66e91953b142f1fbfef2807cdf78
Larry Gritz <lg@larrygritz.com>
2023-09-21 12:58:37 -0700

    testing: Set up placeholders for docs-examples-are-tests
    I had previously prototyped an instance of this for the ImageOutput
    chapter.
    
    This patch makes empty placeholders for all of the other API chapters.
    They don't contain any actual tests or generate any output now, but
    they do build and run, so people can drop new examples into them
    without having to worry about how to set up new files for each
    chapter.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/docs-examples-cpp/run.py
+++ testsuite/docs-examples-cpp/run.py
@@ -5,13 +5,28 @@
 # https://github.com/OpenImageIO/oiio
 
 
+if platform.system() == 'Windows' :
+    prefix = "Release\\"
+else :
+    prefix = "./"
+
 # command += "echo test_source_dir=" + test_source_dir + " >> build.txt ;"
 command += run_app("cmake " + test_source_dir + " -DCMAKE_BUILD_TYPE=Release >> build.txt 2>&1", silent=True)
 command += run_app("cmake --build . --config Release >> build.txt 2>&1", silent=True)
-if platform.system() == 'Windows' :
-    command += run_app("Release\\docs-examples-imageoutput")
-else :
-    command += run_app("./docs-examples-imageoutput")
 
-outputs = [ "simple.tif", "scanlines.tif",
-            "out.txt" ]
+# Run the examples for each chapter
+for chapter in [ "imageioapi", "imageoutput", "imageinput", "writingplugins",
+                 "imagecache", "texturesys", "imagebuf", "imagebufalgo" ] :
+    command += run_app(prefix + "docs-examples-" + chapter)
+
+outputs = [
+    # Outputs from the ImageOutput chapter:
+    "simple.tif", "scanlines.tif",
+    # Outputs from the ImageInput chapter:
+
+    # ... etc ... other chapters ...
+
+    # Last, we have the out.txt that captures console output of the test
+    # programs.
+    "out.txt"
+    ]
