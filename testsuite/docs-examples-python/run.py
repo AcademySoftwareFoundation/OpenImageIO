3031f9153f5549940663d7c6dd15b1f61ca523e8
Larry Gritz <lg@larrygritz.com>
2023-09-21 16:22:18 -0700

    testing: Set up placeholders for docs-examples-are-tests (#3994)
    I had previously prototyped an instance of this for the ImageOutput
    chapter.
    
    This patch makes empty placeholders for all of the other API chapters.
    They don't contain any actual tests or generate any output now, but they
    do build and run, so people can drop new examples into them without
    having to worry about how to set up new files for each chapter.
    
    ---------
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/docs-examples-python/run.py
+++ testsuite/docs-examples-python/run.py
@@ -5,8 +5,20 @@
 # https://github.com/OpenImageIO/oiio
 
 
-# Run the script 
-command += pythonbin + " src/docs-examples-imageoutput.py >> out.txt ;"
+# Run the examples for each chapter
+for chapter in [ "imageioapi", "imageoutput", "imageinput", "writingplugins",
+                 "imagecache", "texturesys", "imagebuf", "imagebufalgo" ] :
+    command += pythonbin + " src/docs-examples-" + chapter + ".py >> out.txt ;"
 
-outputs = [ "simple.tif", "scanlines.tif", "out.txt" ]
+outputs = [
+    # Outputs from the ImageOutput chapter:
+    "simple.tif", "scanlines.tif",
+    # Outputs from the ImageInput chapter:
+
+    # ... etc ... other chapters ...
+
+    # Last, we have the out.txt that captures console output of the test
+    # programs.
+    "out.txt"
+    ]
 
