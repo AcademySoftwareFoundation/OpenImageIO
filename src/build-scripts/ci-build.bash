a6585e54bf4030c140f1af41de5b2946cddde4f3
Larry Gritz <lg@larrygritz.com>
2023-12-30 11:53:29 -0800

    ci: Repair Sonar scanner analysis (#4097)
    This broke some time back, probably partly related to our change in
    repository home. This seems to get it mostly working? The reports are
    still not showing up, but I think the solution might be to make sure
    this is checked into master, so we need to merge this before assessing
    whether there is more work to be done in this area.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>
    Signed-off-by: Peter Kovář <peter.kovar@reflexion.tv>

--- src/build-scripts/ci-build.bash
+++ src/build-scripts/ci-build.bash
@@ -9,6 +9,9 @@
 set -ex
 
 OIIO_CMAKE_FLAGS="$MY_CMAKE_FLAGS $OIIO_CMAKE_FLAGS"
+export OIIO_SRC_DIR=${OIIO_SRC_DIR:=$PWD}
+export OIIO_BUILD_DIR=${OIIO_BUILD_DIR:=${OIIO_SRC_DIR}/build}
+export OIIO_INSTALL_DIR=${OIIO_INSTALL_DIR:=${OIIO_SRC_DIR}/dist}
 
 if [[ "$USE_SIMD" != "" ]] ; then
     OIIO_CMAKE_FLAGS="$OIIO_CMAKE_FLAGS -DUSE_SIMD=$USE_SIMD"
@@ -27,7 +30,8 @@ if [[ ${GITHUB_ACTIONS} == true ]] ; then
     OIIO_CMAKE_FLAGS+=" -DCMAKE_UNITY_BUILD=${CMAKE_UNITY_BUILD:=ON} -DCMAKE_UNITY_BUILD_MODE=${CMAKE_UNITY_BUILD_MODE:=BATCH}"
 fi
 
-cmake -S . -B build -G "$CMAKE_GENERATOR" \
+# pushd $OIIO_BUILD_DIR
+cmake -S $OIIO_SRC_DIR -B $OIIO_BUILD_DIR -G "$CMAKE_GENERATOR" \
         -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE}" \
         -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" \
         -DCMAKE_INSTALL_PREFIX="$OpenImageIO_ROOT" \
@@ -39,14 +43,18 @@ cmake -S . -B build -G "$CMAKE_GENERATOR" \
         $OIIO_CMAKE_FLAGS -DVERBOSE=1
 
 # Save a copy of the generated files for debugging broken CI builds.
-mkdir build/cmake-save || /bin/true
-cp -r build/CMake* build/*.cmake build/cmake-save
+mkdir ${OIIO_BUILD_DIR}/cmake-save || /bin/true
+cp -r ${OIIO_BUILD_DIR}/CMake* ${OIIO_BUILD_DIR}/*.cmake ${OIIO_BUILD_DIR}/cmake-save
 
 : ${BUILDTARGET:=install}
 if [[ "$BUILDTARGET" != "none" ]] ; then
     echo "Parallel build ${CMAKE_BUILD_PARALLEL_LEVEL} of target ${BUILDTARGET}"
-    time ${OIIO_CMAKE_BUILD_WRAPPER} cmake --build build --target ${BUILDTARGET} --config ${CMAKE_BUILD_TYPE}
+    if [[ ${OIIO_CMAKE_BUILD_WRAPPER} != "" ]] ; then
+        echo "Using build wrapper '${OIIO_CMAKE_BUILD_WRAPPER}'"
+    fi
+    time ${OIIO_CMAKE_BUILD_WRAPPER} cmake --build ${OIIO_BUILD_DIR} --target ${BUILDTARGET} --config ${CMAKE_BUILD_TYPE}
 fi
+# popd
 
 if [[ "${DEBUG_CI:=0}" != "0" ]] ; then
     echo "PATH=$PATH"
