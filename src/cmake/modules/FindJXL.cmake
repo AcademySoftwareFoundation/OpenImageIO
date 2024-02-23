b9a51b2902879f730bfd284cec226520e4b56da5
Peter Kovář <peter.kovar@reflexion.tv>
2024-02-23 13:20:38 +0100

    Added license and updated the includes variable
    Signed-off-by: Peter Kovář <peter.kovar@reflexion.tv>

--- src/cmake/modules/FindJXL.cmake
+++ src/cmake/modules/FindJXL.cmake
@@ -1,7 +1,12 @@
-# Find libjxl
+# Module to find libjxl
+#
+# Copyright Contributors to the OpenImageIO project.
+# SPDX-License-Identifier: Apache-2.0
+# https://github.com/AcademySoftwareFoundation/OpenImageIO
+
 # Will define:
 # - JXL_FOUND
-# - JXL_INCLUDE_DIRS directory to include for libjxl headers
+# - JXL_INCLUDES directory to include for libjxl headers
 # - JXL_LIBRARIES libraries to link to
 
 include (FindPackageHandleStandardArgs)
@@ -23,5 +28,5 @@ find_package_handle_standard_args(JXL
 
 if(JXL_FOUND)
   set(JXL_LIBRARIES ${JXL_LIBRARY} ${JXL_THREADS_LIBRARY})
-  set(JXL_INCLUDE_DIRS ${JXL_INCLUDE_DIR})
+  set(JXL_INCLUDES ${JXL_INCLUDE_DIR})
 endif(JXL_FOUND)
