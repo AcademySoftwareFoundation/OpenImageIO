8573f21eb886e23104df0b585e5196f68e3a7848
Larry Gritz <lg@larrygritz.com>
2024-03-16 23:00:05 -0700

    build: switch to target-based definitions
    We had lots of stray usage of add_defintions, add_compile_options,
    etc.  In modern cmake, this is considered bad form, since it makes
    subprojects pollute the global option lists. A better way is to add
    all these to specific targets, so now that's what we do in this PR.
    This should make all the definitions and options we want to set for
    compmilng OIIO's code not have any interference with subprojects, or
    with higher-level surrounding projecs.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/cmake/fancy_add_executable.cmake
+++ src/cmake/fancy_add_executable.cmake
@@ -21,15 +21,20 @@
 #                         [ SRC source1 ... ]
 #                         [ INCLUDE_DIRS include_dir1 ... ]
 #                         [ DEFINITIONS -DFOO=bar ... ])
+#                         [ COMPILE_OPTIONS -Wno-foo ... ]
 #                         [ LINK_LIBRARIES external_lib1 ... ]
+#                         [ FOLDER foldername ]
 #
 macro (fancy_add_executable)
-    cmake_parse_arguments (_target "" "NAME" "SRC;INCLUDE_DIRS;SYSTEM_INCLUDE_DIRS;LINK_LIBRARIES;DEFINITIONS" ${ARGN})
+    cmake_parse_arguments (_target "NO_INSTALL" "NAME;FOLDER" "SRC;INCLUDE_DIRS;SYSTEM_INCLUDE_DIRS;LINK_LIBRARIES;DEFINITIONS;COMPILE_OPTIONS" ${ARGN})
        # Arguments: <prefix> <options> <one_value_keywords> <multi_value_keywords> args...
     if (NOT _target_NAME)
         # If NAME is not supplied, infer target name (and therefore the
         # executable name) from the directory name.
         get_filename_component (_target_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+    endif ()
+    if (NOT _target_FOLDER)
+        set (_target_FOLDER "Tools")
     endif ()
     if (NOT _target_SRC)
         # If SRC is not supplied, assume local cpp files are its source.
@@ -44,20 +49,25 @@ macro (fancy_add_executable)
         if (_target_SYSTEM_INCLUDE_DIRS)
             target_include_directories (${_target_NAME} SYSTEM PRIVATE ${_target_SYSTEM_INCLUDE_DIRS})
         endif ()
-        if (_target_DEFINITIONS)
-            target_compile_definitions (${_target_NAME} PRIVATE ${_target_DEFINITIONS})
-        endif ()
+        target_compile_definitions (${_target_NAME} PRIVATE
+                                        ${${PROJECT_NAME}_compile_definitions}
+                                        ${_target_DEFINITIONS})
+        target_compile_options (${_target_NAME} PRIVATE
+                                    ${${PROJECT_NAME}_compile_options}
+                                    ${_target_COMPILE_OPTIONS})
+        target_link_options (${_target_NAME} PRIVATE
+                                 ${${PROJECT_NAME}_link_options})
         if (_target_LINK_LIBRARIES)
             target_link_libraries (${_target_NAME} PRIVATE ${_target_LINK_LIBRARIES})
         endif ()
-        set_target_properties (${_target_NAME} PROPERTIES FOLDER "Tools")
+        set_target_properties (${_target_NAME} PROPERTIES FOLDER ${_target_FOLDER})
         check_is_enabled (INSTALL_${_target_NAME} _target_NAME_INSTALL_enabled)
         if (CMAKE_UNITY_BUILD AND UNITY_BUILD_MODE STREQUAL GROUP)
             set_source_files_properties(${_target_SRC} PROPERTIES
                                         UNITY_GROUP ${_target_NAME})
             message (VERBOSE "Unity group ${_target_NAME} = ${_target_SRC}")
         endif ()
-        if (_target_NAME_INSTALL_enabled)
+        if (_target_NAME_INSTALL_enabled AND NOT _target_NO_INSTALL)
             install_targets (${_target_NAME})
         endif ()
     else ()
