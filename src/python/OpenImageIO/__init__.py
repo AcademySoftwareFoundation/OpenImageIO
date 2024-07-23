df83fc880d9da3ee1344c5058266859d00e214e1
Zach Lewis <zachcanbereached@gmail.com>
2024-07-23 15:54:56 -0400

    Add pyproject.toml for building wheels with scikit-build-core
    Scikit-build-core is used for collecting CMake and Ninja as needed, and for invoking the build. When invoked via cibuildwheels, `repairwheel` is used after each build to re-bundle and relink the shared library dependencies into properly redistributable whl archives. The command-line tools are exposed under the [project.scripts] section.
    This commit incorporates or is otherwise inspired by similar efforts by @aclark4life and @JeanChristopheMorinPerso, as well as @remia's work on the OpenColorIO wheels.
    
    Signed-off-by: Zach Lewis <zachcanbereached@gmail.com>

---
+++ src/python/OpenImageIO/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenImageIO Project.
+
+from .OpenImageIO import *
+
+__version__ = VERSION_STRING
---
+++ src/python/OpenImageIO/command_line.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright Contributors to the OpenImageIO Project.
+
+import os
+import subprocess
+import sys
+
+BIN_DIR = os.path.join(os.path.dirname(__file__), 'bin')
+
+def call_program(name, args):
+    return subprocess.call([os.path.join(BIN_DIR, name)] + args)
+
+
+def main():
+    name = os.path.basename(sys.argv[0])
+    raise SystemExit(call_program(name, sys.argv[1:]))
