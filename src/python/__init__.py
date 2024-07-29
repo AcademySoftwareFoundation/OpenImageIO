78fed979e29a28ad80f1fe443c43614cbb9c8c99
Zach Lewis <zachcanbereached@gmail.com>
2024-07-29 16:32:34 -0400

    refactor: Simplify structure of Python module
    Instead of creating a separate OpenImageIO.OpenImageIO.command_line module for the CLI shims, move the CLI shim logic up to a "_command_line()" method in OpenImageIO.__init__.py.
    (Maybe this method should still be called "main()" though?)
    
    This also means the module is technically importable from OpenImageIO.OpenImageIO, but that's an improvement over OpenImageIO.OpenImageIO.OpenImageIO...!
    
    Signed-off-by: Zach Lewis <zachcanbereached@gmail.com>

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 
-import os, sys, platform
+import os, sys, platform, subprocess
 
 # This works around the python 3.8 change to stop loading DLLs from PATH on Windows.
 # We reproduce the old behaviour by manually tokenizing PATH, checking that the directories exist and are not ".",
@@ -13,5 +13,32 @@ if sys.version_info >= (3, 8) and platform.system() == "Windows" and os.getenv("
         if os.path.exists(path) and path != ".":
             os.add_dll_directory(path)
 
+
 from .OpenImageIO import *
 
+__version__ = VERSION_STRING
+
+__status__ = "dev"
+
+__author__ = "OpenImageIO Contributors"
+
+__email__ = "oiio-dev@lists.aswf.io"
+
+__license__ = "SPDX-License-Identifier: Apache-2.0"
+
+__copyright__ = "Copyright Contributors to the OpenImageIO Project"
+
+__doc__ = """OpenImageIO is a toolset for reading, writing, and manipulating image files of 
+any image file format relevant to VFX / animation via a format-agnostic API 
+with a feature set, scalability, and robustness needed for feature film 
+production.
+"""
+
+
+def _call_program(name, args):
+    BIN_DIR = os.path.join(os.path.dirname(__file__), 'bin')
+    return subprocess.call([os.path.join(BIN_DIR, name)] + args)
+
+def _command_line():
+    name = os.path.basename(sys.argv[0])
+    raise SystemExit(_call_program(name, sys.argv[1:]))
