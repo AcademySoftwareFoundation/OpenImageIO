13f60cead769e2ae6bfa122687d575d7328fc20c
Zach Lewis <zachcanbereached@gmail.com>
2024-08-30 10:02:39 -0400

    build(deps): add yaml-cpp build recipe
    It's an OCIO dependency.
    
    Signed-off-by: Zach Lewis <zachcanbereached@gmail.com>

--- src/cmake/externalpackages.cmake
+++ src/cmake/externalpackages.cmake
@@ -129,8 +129,12 @@ checked_find_package (Freetype
                       VERSION_MIN 2.10.0
                       DEFINITIONS USE_FREETYPE=1 )
 
-checked_find_package (OpenColorIO
-                      VERSION_MIN 1.1
+
+checked_find_package (yaml-cpp
+                      VERSION_MIN 0.6.0 )  # Used by OpenColorIO
+
+checked_find_package (OpenColorIO REQUIRED
+                      VERSION_MIN 2.2
                       VERSION_MAX 2.9
                       NO_FP_RANGE_CHECK
                       DEFINITIONS  USE_OCIO=1 USE_OPENCOLORIO=1
