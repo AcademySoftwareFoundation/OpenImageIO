AcademySoftwareFoundation/OpenImageIO

commit 5fe444151891b513c0464605d50631fc4945d75c

feat(iv): OCIO color managed display (#4031)
This change adds OCIO functionality to iv.

To enable OCIO, first set the $OCIO environment variable to point with a
path to the OCIO config. "ocio://default" should work as well.

If a valid config provided, a new menu gets added under "View/OCIO" with
an on/off toggle and submenus to select the image color space and
display/view and optional look.

If an image color space, display and view are provided as command line
parameters ("image-color-space", "display", "view"), then the OCIO mode
is switched on automatically.

If no image color space is provided as command line parameters, the top
most color space of the config get pre-selected in the menus.

If no display or view is provided as command line parameters, the
default values get pre-selected in the menus.

In OCIO mode the exposure and gamma adjustments are done by OCIO. The
exposure is done in scene linear, gamma is still in display space. The
exposure adjustments don't match between the OCIO and non-OCIO modes.

When OCIO fails to initialise the pipeline for any reason, an error
message is shown in the status bar, and the non-OCIO path gets used.

TODO:
- All channel shuffling is done by the existing shader and happens
before the OCIO step. We may want to discuss/change that?
- The initial image color space gets selected from the command line
parameter, or the first color space in the config. There is no attempt
being made to use the image metadata.
- The exposure/gamma values get baked into the shader, which gets
recompiled on every change. We may want to use uniforms instead. - DONE


---------

Signed-off-by: Anton Dukhovnikov <antond@wetafx.co.nz>

src/iv/ivgl.cpp
@@ -43,14 +43,6 @@ gl_err_to_string(GLenum err)
     }
 }
 
-
-#define GLERRPRINT(msg)                                                     \
-    for (GLenum err = glGetError(); err != GL_NO_ERROR; err = glGetError()) \
-        std::cerr << "GL error " << msg << " " << (int)err << " - "         \
-                  << gl_err_to_string(err) << "\n";
-
-
-
 IvGL::IvGL(QWidget* parent, ImageViewer& viewer)
     : QOpenGLWidget(parent)
     , m_viewer(viewer)
@@ -151,19 +143,19 @@ IvGL::create_textures(void)
     for (unsigned int texture : textures) {
         m_texbufs.emplace_back();
         glBindTexture(GL_TEXTURE_2D, texture);
-        GLERRPRINT("bind tex");
+        print_error("bind tex");
         glTexImage2D(GL_TEXTURE_2D, 0 /*mip level*/,
                      4 /*internal format - color components */, 1 /*width*/,
                      1 /*height*/, 0 /*border width*/,
                      GL_RGBA /*type - GL_RGB, GL_RGBA, GL_LUMINANCE */,
                      GL_FLOAT /*format - GL_FLOAT */, NULL /*data*/);
-        GLERRPRINT("tex image 2d");
+        print_error("tex image 2d");
         // Initialize tex parameters.
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-        GLERRPRINT("After tex parameters");
+        print_error("After tex parameters");
         m_texbufs.back().tex_object = texture;
         m_texbufs.back().x          = 0;
         m_texbufs.back().y          = 0;
@@ -189,25 +181,80 @@ IvGL::create_textures(void)
     m_tex_created = true;
 }
 
-
+const char*
+IvGL::color_func_shader_text()
+{
+    // clang-format off
+    return
+        "uniform float gain;\n"
+        "uniform float gamma;\n"
+        "\n"
+        "vec4 ColorFunc(vec4 C)\n"
+        "{\n"
+        "    C.xyz *= gain;\n"
+        "    float invgamma = 1.0/gamma;\n"
+        "    C.xyz = pow (C.xyz, vec3 (invgamma, invgamma, invgamma));\n"
+        "    return C;\n"
+        "}\n";
+    // clang-format on
+}
 
 void
 IvGL::create_shaders(void)
 {
-    // clang-format off
-    static const GLchar *vertex_source =
-        "varying vec2 vTexCoord;\n"
-        "void main ()\n"
-        "{\n"
-        "    vTexCoord = gl_MultiTexCoord0.xy;\n"
-        "    gl_Position = ftransform();\n"
-        "}\n";
+    if (!m_use_shaders) {
+        std::cerr << "Not using shaders!\n";
+        return;
+    }
 
-    static const GLchar *fragment_source =
+    const char* color_shader = color_func_shader_text();
+    if (m_color_shader_text != color_shader) {
+        if (m_shader_program) {
+            if (m_vertex_shader) {
+                glDetachShader(m_shader_program, m_vertex_shader);
+            }
+            glUseProgram(0);
+            glDeleteProgram(m_shader_program);
+            m_shader_program  = 0;
+            m_shaders_created = false;
+        }
+    }
+
+    if (m_shaders_created) {
+        return;
+    }
+
+    // This holds the compilation status
+    GLint status;
+
+    if (!m_vertex_shader) {
+        // clang-format off
+        static const GLchar* vertex_source =
+            "varying vec2 vTexCoord;\n"
+            "void main ()\n"
+            "{\n"
+            "    vTexCoord = gl_MultiTexCoord0.xy;\n"
+            "    gl_Position = ftransform();\n"
+            "}\n";
+        // clang-format on
+
+        m_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(m_vertex_shader, 1, &vertex_source, NULL);
+        glCompileShader(m_vertex_shader);
+        glGetShaderiv(m_vertex_shader, GL_COMPILE_STATUS, &status);
+
+        if (!status) {
+            std::cerr << "vertex shader compile status: " << status << "\n";
+            print_shader_log(std::cerr, m_vertex_shader);
+            create_shaders_abort();
+            return;
+        }
+    }
+
+    // clang-format off
+    static const GLchar* fragment_source =
         "uniform sampler2D imgtex;\n"
         "varying vec2 vTexCoord;\n"
-        "uniform float gain;\n"
-        "uniform float gamma;\n"
         "uniform int startchannel;\n"
         "uniform int colormode;\n"
         // Remember, if imgchannels == 2, second channel would be channel 4 (a).
@@ -305,76 +352,60 @@ IvGL::create_shaders(void)
         "        C = heatmap_mode (C);\n"
         "    if (pixelview != 0)\n"
         "        C.a = 1.0;\n"
-        "    C.xyz *= gain;\n"
-        "    float invgamma = 1.0/gamma;\n"
-        "    C.xyz = pow (C.xyz, vec3 (invgamma, invgamma, invgamma));\n"
+        "    C = ColorFunc(C);\n"
         "    gl_FragColor = C;\n"
         "}\n";
     // clang-format on
 
-    if (!m_use_shaders) {
-        std::cerr << "Not using shaders!\n";
-        return;
-    }
-    if (m_shaders_created)
-        return;
+    const char* fragment_sources[] = { "#version 120\n", color_shader,
+                                       fragment_source };
+    m_color_shader_text            = color_shader;
 
-    //initialize shader object handles for abort function
-    m_shader_program  = 0;
-    m_vertex_shader   = 0;
-    m_fragment_shader = 0;
-
-    // When using extensions to support shaders, we need to load the function
-    // entry points (which is actually done by GLEW) and then call them. So
-    // we have to get the functions through the right symbols otherwise
-    // extension-based shaders won't work.
-    m_shader_program = glCreateProgram();
-
-    GLERRPRINT("create program");
-
-    // This holds the compilation status
-    GLint status;
-
-    m_vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(m_vertex_shader, 1, &vertex_source, NULL);
-    glCompileShader(m_vertex_shader);
-    glGetShaderiv(m_vertex_shader, GL_COMPILE_STATUS, &status);
-
-    if (!status) {
-        std::cerr << "vertex shader compile status: " << status << "\n";
-        print_shader_log(std::cerr, m_vertex_shader);
-        create_shaders_abort();
-        return;
-    }
-    glAttachShader(m_shader_program, m_vertex_shader);
-    GLERRPRINT("After attach vertex shader.");
-
-    m_fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(m_fragment_shader, 1, &fragment_source, NULL);
-    glCompileShader(m_fragment_shader);
-    glGetShaderiv(m_fragment_shader, GL_COMPILE_STATUS, &status);
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 3, fragment_sources, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
     if (!status) {
         std::cerr << "fragment shader compile status: " << status << "\n";
-        print_shader_log(std::cerr, m_fragment_shader);
+        print_shader_log(std::cerr, fragment_shader);
         create_shaders_abort();
         return;
     }
-    glAttachShader(m_shader_program, m_fragment_shader);
-    GLERRPRINT("After attach fragment shader");
 
-    glLinkProgram(m_shader_program);
-    GLERRPRINT("link");
-    GLint linked;
-    glGetProgramiv(m_shader_program, GL_LINK_STATUS, &linked);
-    if (!linked) {
-        std::cerr << "NOT LINKED\n";
-        char buf[10000];
-        buf[0] = 0;
-        GLsizei len;
-        glGetProgramInfoLog(m_shader_program, sizeof(buf), &len, buf);
-        std::cerr << "link log:\n" << buf << "---\n";
-        create_shaders_abort();
-        return;
+    if (!m_shader_program) {
+        // When using extensions to support shaders, we need to load the
+        // function entry points (which is actually done by GLEW) and then call
+        // them. So we have to get the functions through the right symbols
+        // otherwise extension-based shaders won't work.
+        m_shader_program = glCreateProgram();
+        print_error("create program");
+
+        glAttachShader(m_shader_program, m_vertex_shader);
+        print_error("After attach vertex shader.");
+
+        glAttachShader(m_shader_program, fragment_shader);
+        print_error("After attach fragment shader");
+
+        glLinkProgram(m_shader_program);
+        print_error("link");
+        GLint linked;
+        glGetProgramiv(m_shader_program, GL_LINK_STATUS, &linked);
+        if (!linked) {
+            std::cerr << "NOT LINKED\n";
+            char buf[10000];
+            buf[0] = 0;
+            GLsizei len;
+            glGetProgramInfoLog(m_shader_program, sizeof(buf), &len, buf);
+            std::cerr << "link log:\n" << buf << "---\n";
+            create_shaders_abort();
+            return;
+        }
+
+        glDetachShader(m_shader_program, fragment_shader);
+        print_error("After detach fragment shader");
+
+        glDeleteShader(fragment_shader);
+        print_error("After delete fragment shader");
     }
 
     m_shaders_created = true;
@@ -390,10 +421,8 @@ IvGL::create_shaders_abort(void)
         glDeleteProgram(m_shader_program);
     if (m_vertex_shader)
         glDeleteShader(m_vertex_shader);
-    if (m_fragment_shader)
-        glDeleteShader(m_fragment_shader);
 
-    GLERRPRINT("After delete shaders");
+    print_error("After delete shaders");
     m_use_shaders = false;
 }
 
@@ -402,7 +431,7 @@ IvGL::create_shaders_abort(void)
 void
 IvGL::resizeGL(int w, int h)
 {
-    GLERRPRINT("resizeGL entry");
+    print_error("resizeGL entry");
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -413,7 +442,7 @@ IvGL::resizeGL(int w, int h)
     glMatrixMode(GL_MODELVIEW);
 
     clamp_view_to_window();
-    GLERRPRINT("resizeGL exit");
+    print_error("resizeGL exit");
 }
 
 
@@ -549,6 +578,8 @@ IvGL::paintGL()
     glTranslatef(-real_centerx, -real_centery, 0.0f);
     // Recentered so that the pixel space (m_centerx,m_centery) position is
     // at the center of the visible window.
+
+    update_state();
 
     useshader(m_texture_width, m_texture_height);
 
@@ -775,7 +806,7 @@ IvGL::paint_pixelview()
         glBindTexture(GL_TEXTURE_2D, m_pixelview_tex);
         glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, xend - xbegin, yend - ybegin,
                         glformat, gltype, zoombuffer);
-        GLERRPRINT("After tsi2d");
+        print_error("After tsi2d");
     } else {
         smin = -1;
         smax = -1;
@@ -874,10 +905,6 @@ IvGL::paint_pixelview()
 void
 IvGL::useshader(int tex_width, int tex_height, bool pixelview)
 {
-    IvImage* img = m_viewer.cur();
-    if (!img)
-        return;
-
     if (!m_use_shaders) {
         glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
         for (auto&& tb : m_texbufs) {
@@ -897,10 +924,25 @@ IvGL::useshader(int tex_width, int tex_height, bool pixelview)
         return;
     }
 
-    const ImageSpec& spec(img->spec());
+    use_program();
+    update_uniforms(tex_width, tex_height, pixelview);
+}
 
+void
+IvGL::use_program(void)
+{
     glUseProgram(m_shader_program);
-    GLERRPRINT("After use program");
+    print_error("After use program");
+}
+
+void
+IvGL::update_uniforms(int tex_width, int tex_height, bool pixelview)
+{
+    IvImage* img = m_viewer.cur();
+    if (!img)
+        return;
+
+    const ImageSpec& spec(img->spec());
 
     GLint loc;
 
@@ -915,8 +957,7 @@ IvGL::useshader(int tex_width, int tex_height, bool pixelview)
     // This is the texture unit, not the texture object
     glUniform1i(loc, 0);
 
-    loc = glGetUniformLocation(m_shader_program, "gain");
-
+    loc        = glGetUniformLocation(m_shader_program, "gain");
     float gain = powf(2.0, img->exposure());
     glUniform1f(loc, gain);
 
@@ -940,7 +981,8 @@ IvGL::useshader(int tex_width, int tex_height, bool pixelview)
 
     loc = glGetUniformLocation(m_shader_program, "height");
     glUniform1i(loc, tex_height);
-    GLERRPRINT("After setting uniforms");
+
+    print_error("After setting uniforms");
 }
 
 
@@ -986,14 +1028,14 @@ IvGL::update()
         glTexImage2D(GL_TEXTURE_2D, 0 /*mip level*/, glinternalformat,
                      m_texture_width, m_texture_height, 0 /*border width*/,
                      glformat, gltype, NULL /*data*/);
-        GLERRPRINT("Setting up texture");
+        print_error("Setting up texture");
     }
 
     // Set the right type for the texture used for pixelview.
     glBindTexture(GL_TEXTURE_2D, m_pixelview_tex);
     glTexImage2D(GL_TEXTURE_2D, 0, glinternalformat, closeuptexsize,
                  closeuptexsize, 0, glformat, gltype, NULL);
-    GLERRPRINT("Setting up pixelview texture");
+    print_error("Setting up pixelview texture");
 
     // Resize the buffer at once, rather than create one each drawing.
     m_tex_buffer.resize(m_texture_width * m_texture_height * nchannels
@@ -1496,17 +1538,17 @@ IvGL::load_texture(int x, int y, int width, int height)
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, m_pbo_objects[m_last_pbo_used]);
     glBufferData(GL_PIXEL_UNPACK_BUFFER, width * height * spec.pixel_bytes(),
                  &m_tex_buffer[0], GL_STREAM_DRAW);
-    GLERRPRINT("After buffer data");
+    print_error("After buffer data");
     m_last_pbo_used = (m_last_pbo_used + 1) & 1;
 
     // When using PBO this is the offset within the buffer.
     void* data = 0;
 
     glBindTexture(GL_TEXTURE_2D, tb.tex_object);
-    GLERRPRINT("After bind texture");
+    print_error("After bind texture");
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, glformat, gltype,
                     data);
-    GLERRPRINT("After loading sub image");
+    print_error("After loading sub image");
     m_last_texbuf_used = (m_last_texbuf_used + 1) % m_texbufs.size();
     glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
 }
@@ -1521,5 +1563,18 @@ IvGL::is_too_big(float width, float height)
     return tiles > m_texbufs.size();
 }
 
+void
+IvGL::update_state(void)
+{
+    create_shaders();
+}
+
+void
+IvGL::print_error(const char* msg)
+{
+    for (GLenum err = glGetError(); err != GL_NO_ERROR; err = glGetError())
+        std::cerr << "GL error " << msg << " " << (int)err << " - "
+                  << gl_err_to_string(err) << "\n";
+}
 
 OIIO_PRAGMA_WARNING_POP

src/iv/CMakeLists.txt
@@ -23,6 +23,8 @@ if (iv_enabled AND (Qt5_FOUND OR Qt6_FOUND) AND OPENGL_FOUND)
             $<TARGET_NAME_IF_EXISTS:Qt6::Widgets>
             $<TARGET_NAME_IF_EXISTS:Qt6::OpenGLWidgets>
             ${OPENGL_LIBRARIES}
+            $<TARGET_NAME_IF_EXISTS:OpenColorIO::OpenColorIO>
+            $<TARGET_NAME_IF_EXISTS:OpenColorIO::OpenColorIOHeaders>
         )
     if (iv_enabled AND FORCE_OPENGL_1)
         target_compile_definitions(iv PRIVATE FORCE_OPENGL_1)
@@ -32,6 +34,9 @@ if (iv_enabled AND (Qt5_FOUND OR Qt6_FOUND) AND OPENGL_FOUND)
     endif ()
     if (Qt6_FOUND)
         target_compile_definitions(iv PRIVATE OIIO_QT_MAJOR=6)
+    endif ()
+    if (APPLE)
+        target_compile_definitions(iv PRIVATE GL_SILENCE_DEPRECATION)
     endif ()
 else ()
     message (STATUS "\n\n   WARNING: Qt or OpenGL not found -- 'iv' will not be built!\n")

src/iv/imageviewer.cpp
@@ -15,6 +15,7 @@
 
 #include "imageviewer.h"
 #include "ivgl.h"
+#include "ivgl_ocio.h"
 
 #include <QApplication>
 #include <QComboBox>
@@ -35,12 +36,14 @@
 #    include <QDesktopWidget>
 #endif
 
+#include <OpenImageIO/color.h>
 #include <OpenImageIO/dassert.h>
 #include <OpenImageIO/filesystem.h>
 #include <OpenImageIO/imagecache.h>
 #include <OpenImageIO/strutil.h>
 #include <OpenImageIO/sysutil.h>
 #include <OpenImageIO/timer.h>
+
 
 #include "ivutils.h"
 
@@ -93,7 +96,8 @@ static const char *s_file_filters = ""
 
 
 
-ImageViewer::ImageViewer()
+ImageViewer::ImageViewer(bool use_ocio, const std::string& image_color_space,
+                         const std::string& display, const std::string& view)
     : infoWindow(NULL)
     , preferenceWindow(NULL)
     , darkPaletteBox(NULL)
@@ -105,6 +109,12 @@ ImageViewer::ImageViewer()
     , m_fullscreen(false)
     , m_default_gamma(1)
     , m_darkPalette(false)
+#ifdef HAS_OCIO_2
+    , m_useOCIO(use_ocio)
+    , m_ocioColourSpace(image_color_space)
+    , m_ocioDisplay(display)
+    , m_ocioView(view)
+#endif  // HAS_OCIO_2
 {
     readSettings(false);
 
@@ -126,7 +136,13 @@ ImageViewer::ImageViewer()
     slideTimer       = new QTimer();
     slideDuration_ms = 5000;
     slide_loop       = true;
-    glwin            = new IvGL(this, *this);
+
+#ifdef HAS_OCIO_2
+    glwin = new IvGL_OCIO(this, *this);
+#else
+    glwin = new IvGL(this, *this);
+#endif
+
     glwin->setPalette(m_palette);
     glwin->resize(m_default_width, m_default_height);
     setCentralWidget(glwin);
@@ -444,7 +460,151 @@ ImageViewer::createActions()
             SLOT(setSlideShowDuration(int)));
 }
 
+#ifdef HAS_OCIO_2
 
+void
+ImageViewer::createOCIOMenus(QMenu* parent)
+{
+    ocioColorSpacesMenu = new QMenu(tr("Image color space"));
+    ocioDisplaysMenu    = new QMenu(tr("Display/View"));
+
+    try {
+        ColorConfig config;
+
+        std::map<std::string, QMenu*> colourSpaceFamilies;
+
+        ocioColorSpacesGroup = new QActionGroup(ocioColorSpacesMenu);
+        ocioColorSpacesGroup->setExclusive(true);
+
+        for (int i = 0; i < config.getNumColorSpaces(); i++) {
+            const char* colorSpaceName = config.getColorSpaceNameByIndex(i);
+
+            if (colorSpaceName && *colorSpaceName) {
+                // If no color space provided via command line parameters, select the top color space in the list.
+                if (m_ocioColourSpace == "" && i == 0) {
+                    m_ocioColourSpace = colorSpaceName;
+                }
+
+                const char* family = config.getColorSpaceFamilyByName(
+                    colorSpaceName);
+
+                QMenu* targetMenu;
+                if (family && *family) {
+                    auto iter = colourSpaceFamilies.find(family);
+                    if (iter == colourSpaceFamilies.end()) {
+                        targetMenu = new QMenu(family);
+                        ocioColorSpacesMenu->addMenu(targetMenu);
+                        colourSpaceFamilies[family] = targetMenu;
+                    } else {
+                        targetMenu = iter->second;
+                    }
+                } else {
+                    targetMenu = ocioColorSpacesMenu;
+                }
+
+                QAction* action = new QAction(colorSpaceName, this);
+                action->setCheckable(true);
+                action->setChecked(m_ocioColourSpace == colorSpaceName);
+
+                connect(action, SIGNAL(triggered()), this,
+                        SLOT(ocioColorSpaceAction()));
+
+                ocioColorSpacesGroup->addAction(action);
+                targetMenu->addAction(action);
+            }
+        }
+
+        ocioDisplayViewsGroup = new QActionGroup(ocioDisplaysMenu);
+        ocioDisplayViewsGroup->setExclusive(true);
+
+        if (m_ocioDisplay == "" || m_ocioDisplay == "default") {
+            m_ocioDisplay = config.getDefaultDisplayName();
+        }
+
+        if (m_ocioView == "" || m_ocioView == "default") {
+            m_ocioView = config.getDefaultViewName();
+        }
+
+        for (int i = 0; i < config.getNumDisplays(); i++) {
+            const char* display = config.getDisplayNameByIndex(i);
+
+            if (display && *display) {
+                QMenu* menu = new QMenu(display);
+
+                for (int j = 0; j < config.getNumViews(display); j++) {
+                    const char* view = config.getViewNameByIndex(display, j);
+
+                    if (view && *view) {
+                        QAction* action = new QAction(view, menu);
+                        action->setCheckable(true);
+                        action->setChecked(m_ocioDisplay == display
+                                           && m_ocioView == view);
+
+                        connect(action, SIGNAL(triggered()), this,
+                                SLOT(ocioDisplayViewAction()));
+
+                        menu->addAction(action);
+                        ocioDisplayViewsGroup->addAction(action);
+                    }
+                }
+
+                ocioDisplaysMenu->addMenu(menu);
+            }
+        }
+    } catch (...) {
+        std::cerr << "Error loading OCIO config file" << std::endl;
+        m_useOCIO = false;
+        return;
+    }
+
+    QMenu* ocioMenu = new QMenu(tr("OCIO"));
+
+    QAction* action = new QAction(tr("Use OCIO"));
+    action->setCheckable(true);
+    action->setChecked(m_useOCIO);
+    connect(action, SIGNAL(toggled(bool)), this, SLOT(useOCIOAction(bool)));
+
+    ocioMenu->addAction(action);
+    ocioMenu->addMenu(ocioColorSpacesMenu);
+    ocioMenu->addMenu(ocioDisplaysMenu);
+
+    parent->addMenu(ocioMenu);
+}
+
+void
+ImageViewer::useOCIOAction(bool checked)
+{
+    m_useOCIO = checked;
+
+    ocioColorSpacesMenu->setEnabled(m_useOCIO);
+    ocioDisplaysMenu->setEnabled(m_useOCIO);
+
+    displayCurrentImage();
+}
+
+void
+ImageViewer::ocioColorSpaceAction()
+{
+    QAction* action = ocioColorSpacesGroup->checkedAction();
+    if (action) {
+        m_ocioColourSpace = action->text().toStdString();
+        displayCurrentImage();
+    }
+}
+
+void
+ImageViewer::ocioDisplayViewAction()
+{
+    QAction* action = ocioDisplayViewsGroup->checkedAction();
+    if (action) {
+        QMenu* menu   = qobject_cast<QMenu*>(action->parent());
+        m_ocioDisplay = menu->title().toStdString();
+        m_ocioView    = action->text().toStdString();
+        displayCurrentImage();
+    }
+}
+
+#endif  // HAS_OCIO_2
 
 void
 ImageViewer::createMenus()
@@ -531,6 +691,11 @@ ImageViewer::createMenus()
     viewMenu->addAction(viewSubimageNextAct);
     viewMenu->addMenu(channelMenu);
     viewMenu->addMenu(colormodeMenu);
+
+#ifdef HAS_OCIO_2
+    createOCIOMenus(viewMenu);
+#endif
+
     viewMenu->addMenu(expgamMenu);
     menuBar()->addMenu(viewMenu);
     // Full screen mode
@@ -877,7 +1042,13 @@ void
 ImageViewer::moveToNewWindow()
 {
     if (m_images.size()) {
-        ImageViewer* imageViewer = new ImageViewer();
+#ifdef HAS_OCIO_2
+        ImageViewer* imageViewer = new ImageViewer(m_useOCIO, m_ocioColourSpace,
+                                                   m_ocioDisplay, m_ocioView);
+#else
+        std::string dummy;
+        ImageViewer* imageViewer = new ImageViewer(false, dummy, dummy, dummy);
+#endif
 
         imageViewer->show();
         imageViewer->rawcolor(rawcolor());

src/iv/imageviewer.h
@@ -21,6 +21,7 @@
 #include <vector>
 
 #include <QAction>
+#include <QActionGroup>
 #include <QCheckBox>
 #include <QDialog>
 #include <QMainWindow>
@@ -37,6 +38,8 @@
 
 #include <OpenImageIO/imagebuf.h>
 #include <OpenImageIO/imageio.h>
+
+#include "ivgl_ocio.h"
 
 using namespace OIIO;
 
@@ -143,7 +146,8 @@ class ImageViewer final : public QMainWindow {
     Q_OBJECT
 
 public:
-    ImageViewer();
+    ImageViewer(bool use_ocio, const std::string& image_color_space,
+                const std::string& display, const std::string& view);
     ~ImageViewer();
 
     enum COLOR_MODE {
@@ -240,6 +244,13 @@ public:
     void rawcolor(bool val) { m_rawcolor = val; }
     bool rawcolor() const { return m_rawcolor; }
 
+#ifdef HAS_OCIO_2
+    bool useOCIO() { return m_useOCIO; }
+    const std::string& ocioColorSpace() { return m_ocioColourSpace; }
+    const std::string& ocioDisplay() { return m_ocioDisplay; }
+    const std::string& ocioView() { return m_ocioView; }
+#endif  // HAS_OCIO_2
+
 private slots:
     void open();                ///< Dialog to open new image from file
     void reload();              ///< Reread current image from disk
@@ -298,6 +309,13 @@ private slots:
     void showInfoWindow();       ///< View extended info on image
     void showPixelviewWindow();  ///< View closeup pixel view
     void editPreferences();      ///< Edit viewer preferences
+
+#ifdef HAS_OCIO_2
+    void useOCIOAction(bool checked);
+    void ocioColorSpaceAction();
+    void ocioDisplayViewAction();
+#endif  // HAS_OCIO_2
+
 private:
     void createActions();
     void createMenus();
@@ -328,6 +346,7 @@ private:
 #ifndef QT_NO_PRINTER
     // QPrinter printer;
 #endif
+
 
     QAction *openAct, *reloadAct, *closeImgAct;
     static const unsigned int MaxRecentFiles = 10;
@@ -407,6 +426,25 @@ private:
     friend class IvInfoWindow;
     friend class IvPreferenceWindow;
     friend bool image_progress_callback(void* opaque, float done);
+
+#ifdef HAS_OCIO_2
+    friend class IvGL_OCIO;
+
+    void createOCIOMenus(QMenu* parent);
+
+    QMenu* ocioColorSpacesMenu;
+    QMenu* ocioDisplaysMenu;
+    QMenu* ocioOptimizationMenu;
+
+    QActionGroup* ocioColorSpacesGroup;
+    QActionGroup* ocioDisplayViewsGroup;
+    QActionGroup* ocioOptimizationGroup;
+
+    bool m_useOCIO;
+    std::string m_ocioColourSpace;
+    std::string m_ocioDisplay;
+    std::string m_ocioView;
+#endif  // HAS_OCIO_2
 };
 
 

commit b40c30cb4a1a31985a51f03d52a214afeb006efd

fix (iv): iv shows constant brown and GL error messages on start-up.
This fixes a problem seen on RHEL 9.2 Linux when launching an image
in iv, eg:
  iv testsuite/texture-overscan/ref/out-exact.exr

The image displayed as a constant brown and the terminal showed:
  GL error create program 1281 - Invalid value
  GL error After attach vertex shader. 1281 - Invalid value

The fix is to initialize m_shader_program and m_vertex_shader
in the class constructor.


ERROR ANALYSIS:

The bug was introduced in 5fe44415 "feat(iv): OCIO color managed
display (#4031)".

Before that commit, create_shaders() initialized those members to 0
before they were used.

After that commit:

* uninitialized m_shader_program is passed to glDeleteProgram()
(near the top of create_shaders) ), causing the
"create program" warning (there isn't an print_error call after
glDeleteProgram so the error is reported as "create program").

* ununitialized m_vertex_shader is non-0 when checked by
if (!m_vertex_shader), causing the vertex shader not to be created
and causing the later call to glAttachShader() with uninitialized
m_vertex_shader to fail.  That causes the second error message, and lack
of a vertex shader apparently causes the image to display as constant
brown.

Signed-off-by: David Adler <david.adler@disneyanimation.com>

src/iv/ivgl.cpp
@@ -47,6 +47,8 @@ IvGL::IvGL(QWidget* parent, ImageViewer& viewer)
     : QOpenGLWidget(parent)
     , m_viewer(viewer)
     , m_shaders_created(false)
+    , m_vertex_shader(0)
+    , m_shader_program(0)
     , m_tex_created(false)
     , m_zoom(1.0)
     , m_centerx(0)