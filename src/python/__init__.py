221e4d8fd0bb15b6fc3192d36debf70a4be5591e
Zach Lewis <zachcanbereached@gmail.com>
2024-10-02 16:19:24 -0400

    cleanup: python __init__.py, warn about loading DLLs from PATH on Win
    OCIO recently made the decision to disable the "Load DLLs from $PATH" hack for Python > 3.7 that they had copied from OIIO.
    We should follow suit -- this behavior was removed from Python for a reason (it was a security vulnerability).
    
    This PR warns users that this behavior is deprecated.
    
    Signed-off-by: Zach Lewis <zachcanbereached@gmail.com>

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -4,36 +4,43 @@
 
 import os
 import sys
+import logging
 import platform
 import subprocess
 
+logger = logging.getLogger(__name__)
 
 if platform.system() == "Windows":
     # Python wheel module is dynamically linked to the OIIO DLL present in the bin folder.
-    here = os.path.abspath(os.path.dirname(__file__))
-    bin_dir = os.path.join(here, "bin")
-    if os.path.exists(bin_dir):
+    _here = os.path.abspath(os.path.dirname(__file__))
+    _bin_dir = os.path.join(_here, "bin")
+    if os.path.exists(_bin_dir):
         if sys.version_info >= (3, 8):
-            os.add_dll_directory(bin_dir)  
+            os.add_dll_directory(_bin_dir)  
         else:
-            os.environ['PATH'] = '{0};{1}'.format(bin_dir, os.getenv('PATH', ''))
-            
+            os.environ['PATH'] = '{0};{1}'.format(_bin_dir, os.getenv('PATH', ''))
+    
+    #TODO: Remove. Python 3.8 stopped loading DLLs from PATH on Windows for security reasons.
     if sys.version_info >= (3, 8):
         # This works around the python 3.8 change to stop loading DLLs from PATH on Windows.
         # We reproduce the old behavior by manually tokenizing PATH, checking that the 
         # directories exist and are not ".", then add them to the DLL load path.
         # Set the environment variable "OIIO_LOAD_DLLS_FROM_PATH" to "0" to disable this behavior.
         if os.getenv("OIIO_LOAD_DLLS_FROM_PATH", "1") == "1":
+            logger.warning(
+                "Adding DLL directories to PATH. This behavior will be removed in "
+                "a future version. Set the environment variable OIIO_LOAD_DLLS_FROM_PATH "
+                "to 0 to disable this behavior."
+            )
             for path in os.getenv("PATH", "").split(os.pathsep):
                 if os.path.exists(path) and path != ".":
                     os.add_dll_directory(path)
 
+from .OpenImageIO import * # type: ignore # noqa: F401, F403, E402
 
-from .OpenImageIO import * # noqa: F401, F403
+__version__ = VERSION_STRING # type: ignore # noqa: F405
 
-__version__ = VERSION_STRING # noqa: F405
-
-__status__ = "dev"
+__status__ = "production"
 
 __author__ = "OpenImageIO Contributors"
 
