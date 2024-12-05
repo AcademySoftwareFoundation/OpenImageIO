d2077eb22a821d0ef7614d06cc1540b952d37ddf
Larry Gritz <lg@larrygritz.com>
2024-12-05 14:37:10 -0800

    fix: Detect invalid ICC profile tags (#4557)
    Affects jpeg, tiff, and other formats with embedded ICC profiles.
    Corrupted/invalid tag offset and/or length values were causing us to
    touch memory outside the memory allocated for the profile.
    
    Fixes #4551
    
    Not sure if this is wise or not, but for JPEGs, I also enabled some
    previously commented-out code that has the effect of treating a file
    with a verifiably corrupted/nonsensical ICC profile block as a failed
    file. If people ever complain about this in the future, maybe we'll come
    back and add some kind of global option that lets the app express how
    tolerant OIIO readers should be about corruptions in files -- do you
    assume that the whole file is bogus or malicious upon discovering the
    first invalidity, or do you press on and hope you can get something else
    useful from the pixels? Let's be cautious for now.
    
    ---------
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/libOpenImageIO/icc.cpp
+++ src/libOpenImageIO/icc.cpp
@@ -304,11 +304,13 @@ decode_icc_profile(cspan<uint8_t> iccdata, ImageSpec& spec, std::string& error)
         if (!extract(iccdata, offset, tag, error))
             return false;
         string_view signature(tag.signature, 4);
-        if (tag.offset + tag.size > iccdata.size()) {
-            error = format("ICC profile tag {} extends past end", signature);
-            return false;
+        if (!check_span(iccdata, iccdata.data() + tag.offset,
+                        std::max(4U, tag.size))) {
+            error = format(
+                "ICC profile tag {} appears to contain corrupted/invalid data",
+                signature);
+            return false;  // Non-sensical: tag extends beyond icc data block
         }
-
         string_view typesignature((const char*)iccdata.data() + tag.offset, 4);
         // Strutil::print("   tag {} type {} offset {} size {}\n", signature,
         //                typesignature, tag.offset, tag.size);
@@ -360,6 +362,14 @@ decode_icc_profile(cspan<uint8_t> iccdata, ImageSpec& spec, std::string& error)
                     // Strutil::print(
                     //     "eng len={} stfoffset={} ({:x}) wcharsize={}\n", len,
                     //     stroffset, tag.offset + stroffset, sizeof(wchar_t));
+                    if (!check_span(iccdata,
+                                    iccdata.data() + tag.offset + stroffset,
+                                    len)) {
+                        error = format(
+                            "ICC profile tag {} appears to contain corrupted/invalid data",
+                            signature);
+                        return false;  // Non-sensical: tag extends beyond icc data block
+                    }
                     const char* start = (const char*)iccdata.data() + tag.offset
                                         + stroffset;
                     // The actual data is UTF-16
