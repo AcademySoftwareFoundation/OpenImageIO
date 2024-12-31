fd411a6e3634890089ba2126da0962003fb36c92
Larry Gritz <lg@larrygritz.com>
2024-12-31 12:31:37 -0800

    fix: various internal fixes to address Sonar and other warnings (#4577)
    Chipping away at minor warnings and areas identified by static analysis
    as potentially problematic or hard to analyze.
    
    * benchmark.h: suppress warnings about uninitialized Benchmarker
    members.
    * dpxoutput.cpp: change some raw pointers to spans
    * dead code removal (color_ocio.cpp, filesystem.cpp, sysutil.cpp)
    * exclude build-scripts from static analysis
    * some misc warning suppression
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/libutil/filesystem.cpp
+++ src/libutil/filesystem.cpp
@@ -1003,9 +1003,8 @@ Filesystem::scan_for_matching_filenames(const std::string& pattern,
         // case 3: pattern has format, but no view
         return scan_for_matching_filenames(pattern, frame_numbers, filenames);
     }
-
-    return true;
 }
+
 
 bool
 Filesystem::scan_for_matching_filenames(const std::string& pattern_,
@@ -1285,10 +1284,6 @@ Filesystem::IOFile::pwrite(const void* buf, size_t size, int64_t offset)
     // FIXME: the system pwrite returns ssize_t and is -1 on error.
     return r < 0 ? size_t(0) : size_t(r);
 #endif
-    offset += r;
-    if (m_pos > int64_t(m_size))
-        m_size = offset;
-    return r;
 }
 
 size_t
