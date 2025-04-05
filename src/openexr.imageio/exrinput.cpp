63771e0f930f2cf9d423c3d436386d4469dcfaaf
Basile Fraboni <basile.fraboni@gmail.com>
2025-04-04 17:41:58 -0700

    simplify and fix compilation issue
    Signed-off-by: Basile Fraboni <basile.fraboni@gmail.com>

--- src/openexr.imageio/exrinput.cpp
+++ src/openexr.imageio/exrinput.cpp
@@ -671,9 +671,6 @@ OpenEXRInput::PartInfo::parse_header(OpenEXRInput* in,
 
     spec.attribute("oiio:subimages", in->m_nsubimages);
 
-    if (miplevel == 0)
-        spec.attribute("oiio:miplevels", nmiplevels);
-
     // Squash some problematic texture metadata if we suspect it's wrong
     pvt::check_texture_metadata_sanity(spec);
 
@@ -1076,6 +1073,9 @@ OpenEXRInput::seek_subimage(int subimage, int miplevel)
 
     if (miplevel < 0 || miplevel >= part.nmiplevels)  // out of range
         return false;
+
+    if (miplevel == 0 && part.nmiplevels > 1)
+        part.spec.attribute("oiio:miplevels", part.nmiplevels);
 
     m_miplevel = miplevel;
     m_spec     = part.spec;
