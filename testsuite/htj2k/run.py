81d32da32e08595fd571a6e09b6c63396d68a7ea
Sam.Richards@taurich.org <Sam.Richards@taurich.org>
2025-04-12 17:57:33 +0100

    Refining the test.
    Signed-off-by: Sam.Richards@taurich.org <Sam.Richards@taurich.org>

--- testsuite/htj2k/run.py
+++ testsuite/htj2k/run.py
@@ -4,20 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 
-# Allow some LSB slop -- necessary because of the alpha disassociation
-# combined with implied sRGB conversion.
-#failthresh = 0.02
-#failpercent = 0.02
-#redirect = ' >> out.txt 2>&1 '
-#failureok = True
-
-#imagedir = OIIO_TESTSUITE_IMAGEDIR + "/jpeg2000/broken"
-#files = [ "issue_3427.jp2" ]
-#for f in files:
-#    command += rw_command (imagedir, f, printinfo=False)
-
-
-files = [ "dpx_nuke_10bits_rgb.dpx", "tahoe-gps.jpg" ]
 
 command += oiiotool(OIIO_TESTSUITE_IMAGEDIR+"/tahoe-gps.jpg"
                     " -o test.j2c")
@@ -31,6 +17,6 @@ command += diff_command(OIIO_TESTSUITE_IMAGEDIR+"/dpx_nuke_10bits_rgb.dpx", "tes
 
 
 command += oiiotool(OIIO_TESTSUITE_IMAGEDIR+"/tahoe-gps.jpg"
-                    " --compression qstep:0.001 -o testcompress.j2c")
+                    " --compression qstep:0.03 -o testcompress.j2c")
 
-command += diff_command(OIIO_TESTSUITE_IMAGEDIR+"/tahoe-gps.jpg", "testcompress.j2c")
+command += diff_command(OIIO_TESTSUITE_IMAGEDIR+"/tahoe-gps.jpg", "testcompress.j2c", extraargs="-fail 0.11")
