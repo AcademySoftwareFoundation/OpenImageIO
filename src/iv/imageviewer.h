4e56aa7b6f979a5f24718747004bfea1b1391f0c
Danielle Imogu <dannieim689@gmail.com>
2025-06-05 15:38:35 -0400

    feat(iv): area probe (#4767)
    Fixes #4714 
    
    This PR adds support for selecting a rectangular region within an image
    to compute and display per-channel minimum, maximum, and average values.
    
    The feature can be activated through the Tools menu or by using a
    keyboard shortcut (Ctrl-A / Cmd-A) . Once active, the user can click and
    drag with the left mouse button to define a rectangular area, which is
    visually indicated by a blue outline. Upon releasing the mouse button,
    the tool calculates statistics for all pixels within the selected area
    and displays the results in the status bar.
    
    ---------
    
    Signed-off-by: Danielle Imogu <dannieim689@gmail.com>

--- src/iv/imageviewer.h
+++ src/iv/imageviewer.h
@@ -224,6 +224,11 @@ public:
         return showPixelviewWindowAct && showPixelviewWindowAct->isChecked();
     }
 
+    bool probeviewOn(void) const
+    {
+        return toggleAreaSampleAct && toggleAreaSampleAct->isChecked();
+    }
+
     bool windowguidesOn(void) const
     {
         return toggleWindowGuidesAct && toggleWindowGuidesAct->isChecked();
@@ -249,6 +254,7 @@ public:
 
     void rawcolor(bool val) { m_rawcolor = val; }
     bool rawcolor() const { return m_rawcolor; }
+    bool areaSampleMode() const;
 
     bool useOCIO() { return m_useOCIO; }
     const std::string& ocioColorSpace() { return m_ocioColourSpace; }
@@ -316,6 +322,7 @@ private slots:
     void showInfoWindow();       ///< View extended info on image
     void showPixelviewWindow();  ///< View closeup pixel view
     void editPreferences();      ///< Edit viewer preferences
+    void toggleAreaSample();     ///< Use area probe
 
     void useOCIOAction(bool checked);
     void ocioColorSpaceAction();
@@ -385,6 +392,7 @@ private:
     QAction* showInfoWindowAct;
     QAction* editPreferencesAct;
     QAction* showPixelviewWindowAct;
+    QAction* toggleAreaSampleAct;
     QAction* toggleWindowGuidesAct;
     QMenu *fileMenu, *editMenu, /**imageMenu,*/ *viewMenu, *toolsMenu,
         *helpMenu;
@@ -420,7 +428,8 @@ private:
     float m_default_gamma;                    // Default gamma of the display
     QPalette m_palette;                       // Custom palette
     bool m_darkPalette;                       // Use dark palette?
-    bool m_rawcolor = false;                  // Use raw color mode
+    bool m_rawcolor       = false;            // Use raw color mode
+    bool m_areaSampleMode = false;            // Use area sample mode
 
     // The default width and height of the window:
     static const int m_default_width  = 640;
