30890f6fc3927fe3a623999df2b772da3c05b90f
Larry Gritz <lg@larrygritz.com>
2025-07-22 17:22:11 -0700

    build: PNG auto-build improvements (#4835)
    The main thing is no longer using a custom namespace prefix for
    auto-built libpng. We're already building it statically by default and
    hiding the symbols, so the custom prefix only serves to make occasional
    build problems by having different symbols than typically found in the
    library. We still keep it as an option that can be enabled.
    
    Also, be a little more careful in clearing unneeded variable that may
    have been set by an earier attempt to find the package but that failed
    to turn up a version we were happy with.
    
    ---------
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/cmake/build_PNG.cmake
+++ src/cmake/build_PNG.cmake
@@ -7,26 +7,34 @@
 ######################################################################
 
 set_cache (PNG_BUILD_VERSION 1.6.47 "PNG version for local builds")
-set (PNG_GIT_REPOSITORY "https://github.com/pnggroup/libpng")
-set (PNG_GIT_TAG "v${PNG_BUILD_VERSION}")
-
+super_set (PNG_BUILD_GIT_REPOSITORY "https://github.com/pnggroup/libpng")
+super_set (PNG_BUILD_GIT_TAG "v${PNG_BUILD_VERSION}")
+super_set (PNG_BUILD_EXTRA_CMAKE_ARGS "")
 set_cache (PNG_BUILD_SHARED_LIBS OFF
            DOC "Should execute a local PNG build, if necessary, build shared libraries" ADVANCED)
+set_option (PNG_BUILD_USE_CUSTOM_PREFIX "Use custom namespace prefix for libpng" OFF)
 
+if (PNG_BUILD_USE_CUSTOM_PREFIX)
+    list (APPEND PNG_BUILD_EXTRA_CMAKE_ARGS -D PNG_PREFIX=oiio)
+endif ()
 string (MAKE_C_IDENTIFIER ${PNG_BUILD_VERSION} PNG_VERSION_IDENT)
 
 unset (PNG_FOUND)
+unset (PNG_LIBRARY)
+unset (PNG_LIBRARY_RELEASE)
+unset (PNG_LIBRARY_DEBUG)
 unset (PNG_LIBRARIES)
 unset (PNG_INCLUDE_DIRS)
 unset (PNG_INCLUDE_DIR)
+unset (PNG_PNG_INCLUDE_DIR)
 unset (PNG_VERSION_STRING)
 unset (PNG_DEFINITIONS)
 unset (PNG_VERSION)
 
 build_dependency_with_cmake (PNG
     VERSION         ${PNG_BUILD_VERSION}
-    GIT_REPOSITORY  ${PNG_GIT_REPOSITORY}
-    GIT_TAG         ${PNG_GIT_TAG}
+    GIT_REPOSITORY  ${PNG_BUILD_GIT_REPOSITORY}
+    GIT_TAG         ${PNG_BUILD_GIT_TAG}
     CMAKE_ARGS
         -D PNG_SHARED=${PNG_BUILD_SHARED_LIBS}
         -D PNG_STATIC=ON
@@ -36,17 +44,25 @@ build_dependency_with_cmake (PNG
         -D PNG_FRAMEWORK=OFF
         -D CMAKE_POSITION_INDEPENDENT_CODE=ON
         -D CMAKE_INSTALL_LIBDIR=lib
-        -D PNG_PREFIX=oiio
+        ${PNG_BUILD_EXTRA_CMAKE_ARGS}
     )
 
     
 set (PNG_REFIND FALSE)
 set (PNG_REFIND_VERSION ${PNG_BUILD_VERSION})
-
-
 set (PNG_REFIND_ARGS EXACT REQUIRED)
-set (PNG_DIR ${PNG_LOCAL_INSTALL_DIR}/lib/cmake/PNG)
 set (PNG_FIND_VERSION_EXACT ON)
+unset (PNG_FOUND)
+unset (PNG_LIBRARY)
+unset (PNG_LIBRARY_RELEASE)
+unset (PNG_LIBRARY_DEBUG)
+unset (PNG_LIBRARIES)
+unset (PNG_INCLUDE_DIRS)
+unset (PNG_INCLUDE_DIR)
+unset (PNG_PNG_INCLUDE_DIR)
+unset (PNG_VERSION_STRING)
+unset (PNG_DEFINITIONS)
+unset (PNG_VERSION)
 
 if (PNG_BUILD_VERSION VERSION_GREATER 1.6.43)
     list (APPEND PNG_REFIND_ARGS CONFIG)
