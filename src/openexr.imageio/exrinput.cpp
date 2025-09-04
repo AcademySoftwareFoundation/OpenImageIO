0ddf30780cf7c8518606622326e2f4d69d5a5761
Larry Gritz <lg@larrygritz.com>
2025-09-03 21:01:11 -0700

    build: several OpenEXR and OpenJPH build related fixes
    Original changes and goal:
    
    * Bump our "latest releases" CI tests to OpenEXR 3.4.0
    * cleanup: dead guard removal for openexr -- our minimum OpenEXR
      release supports float vectors, so we don't need any `#if` guards
      for it anymore.
    * Fix warnings related to mixing Core and C++ library constants for
      H2J2K compression enums within a switch statement (only noticed on
      ARM Linux with Clang 18).
    
    But it seems that OpenEXR 3.4's auto-building of OpenJPH (to support
    exr's new h2j2k compression methods) revealed that our CI wasn't
    building and testing against OpenJPG as well as we thought. So:
    
    * Clean up some openjph API use and type mismatches
    * Suppress warnings on clang and recent gcc versions had with the
      OpenJPH headers and our use of them.
    * Fix our test scripts and reference output for the htj2k tests; I
      think maybe they weren't running at all before, and nad some
      incorrect file paths.
    
    Finally,
    
    * When no OpenEXR is found, update the auto-build version to 3.3.5.
      I wish I could go all the way to 3.4.0, but the OpenEXR 3.4.0
      currently auto-builds OpenJPH in a way that (I think incorrectly)
      installs the OpenJPH components in ways that can overwrite or break
      existing installations.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/openexr.imageio/exrinput.cpp
+++ src/openexr.imageio/exrinput.cpp
@@ -41,10 +41,8 @@ OIIO_GCC_PRAGMA(GCC diagnostic ignored "-Wunused-parameter")
 #include <OpenEXR/ImfDoubleAttribute.h>
 #include <OpenEXR/ImfEnvmapAttribute.h>
 #include <OpenEXR/ImfFloatAttribute.h>
+#include <OpenEXR/ImfFloatVectorAttribute.h>
 #include <OpenEXR/ImfHeader.h>
-#if OPENEXR_HAS_FLOATVECTOR
-#    include <OpenEXR/ImfFloatVectorAttribute.h>
-#endif
 #include <OpenEXR/ImfInputPart.h>
 #include <OpenEXR/ImfIntAttribute.h>
 #include <OpenEXR/ImfKeyCodeAttribute.h>
@@ -432,10 +430,10 @@ OpenEXRInput::PartInfo::parse_header(OpenEXRInput* in,
         case Imf::DWAA_COMPRESSION: comp = "dwaa"; break;
         case Imf::DWAB_COMPRESSION: comp = "dwab"; break;
 #ifdef IMF_HTJ2K256_COMPRESSION
-        case EXR_COMPRESSION_HTJ2K256: comp = "htj2k256"; break;
+        case Imf::HTJ2K256_COMPRESSION: comp = "htj2k256"; break;
 #endif
 #ifdef IMF_HTJ2K32_COMPRESSION
-        case EXR_COMPRESSION_HTJ2K32: comp = "htj2k32"; break;
+        case Imf::HTJ2K32_COMPRESSION: comp = "htj2k32"; break;
 #endif
         default: break;
         }
@@ -459,9 +457,7 @@ OpenEXRInput::PartInfo::parse_header(OpenEXRInput* in,
         const Imf::KeyCodeAttribute* kcattr;
         const Imf::ChromaticitiesAttribute* crattr;
         const Imf::RationalAttribute* rattr;
-#if OPENEXR_HAS_FLOATVECTOR
         const Imf::FloatVectorAttribute* fvattr;
-#endif
         const Imf::StringVectorAttribute* svattr;
         const Imf::DoubleAttribute* dattr;
         const Imf::V2dAttribute* v2dattr;
@@ -529,8 +525,6 @@ OpenEXRInput::PartInfo::parse_header(OpenEXRInput* in,
                 ustrvec[i] = strvec[i];
             TypeDesc sv(TypeDesc::STRING, ustrvec.size());
             spec.attribute(oname, sv, &ustrvec[0]);
-#if OPENEXR_HAS_FLOATVECTOR
-
         } else if (type == "floatvector"
                    && (fvattr
                        = header->findTypedAttribute<Imf::FloatVectorAttribute>(
@@ -538,7 +532,6 @@ OpenEXRInput::PartInfo::parse_header(OpenEXRInput* in,
             std::vector<float> fvec = fvattr->value();
             TypeDesc fv(TypeDesc::FLOAT, fvec.size());
             spec.attribute(oname, fv, &fvec[0]);
-#endif
         } else if (type == "double"
                    && (dattr = header->findTypedAttribute<Imf::DoubleAttribute>(
                            name))) {
