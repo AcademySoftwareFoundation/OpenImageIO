7c2285bc8470f74381b237233d50eb01a77cc8f7
Zach Lewis <zachlewis@users.noreply.github.com>
2025-09-13 13:17:43 -0400

    Update testsuite/png/run.py
    Co-authored-by: Larry Gritz <lg@larrygritz.com>
    Signed-off-by: Zach Lewis <zachlewis@users.noreply.github.com>

--- testsuite/png/run.py
+++ testsuite/png/run.py
@@ -41,6 +41,6 @@ command += oiiotool ("-echo cicp: test16.png --eraseattrib Software --printinfo"
 command += oiiotool ("-echo removed_cicp: test16.png --eraseattrib Software --cicp \"\" --printinfo")
 
 # Test that "set_colorspace" removes CICP metadata
-command += oiiotool ("-echo remove_cicp_via_set_colorspace: test16.png --cicp 1,13 --iscolorspace g22_rec709_display --printinfo")
+command += oiiotool ("-echo remove_cicp_via_set_colorspace: test16.png --eraseattrib Software --cicp 1,13 --iscolorspace g22_rec709_display --printinfo")
 
 outputs = [ "test16.png", "out.txt" ]
