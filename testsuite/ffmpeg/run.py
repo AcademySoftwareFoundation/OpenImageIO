f24fa3c807a72a7e356d048dd38297c0c85e7bfb
Brecht Van Lommel <brecht@blender.org>
2025-09-14 01:50:42 +0200

    feat(ffmpeg): Read CICP metadata, and add test (#4882)
    The actual CICP reading code is trivial.
    
    Also recognize mkv (Matroska) extension for the test.
    
    Add test using Matroska + VP9, to maximize the chance of an ffmpeg build
    supporting them. They are royalty free, built into ffmpeg and relatively
    old.
    
    Signed-off-by: Brecht Van Lommel <brecht@blender.org>
    Signed-off-by: Zach Lewis <zachcanbereached@gmail.com>

---
+++ testsuite/ffmpeg/run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+# Copyright Contributors to the OpenImageIO project.
+# SPDX-License-Identifier: Apache-2.0
+# https://github.com/AcademySoftwareFoundation/OpenImageIO
+
+imagedir = "ref/"
+files = [ "vp9_display_p3.mkv" ]
+for f in files:
+    command = command + info_command (os.path.join(imagedir, f))
