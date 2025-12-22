645cfeb074d21d075b4ccd2d8b742a95300b3314
Brecht Van Lommel <brecht@blender.org>
2025-12-22 19:24:15 +0100

    feat(JXL): CICP read and write support for JPEG XL (#4968)
    The JPEG XL color encoding metadata only supports a subset of CICP. So
    for
    example `srgb_p3d65_display` and `pq_rec2020_display` are supported, but
    `g26_xyzd65_display` is not.
    
    Custom primaries, custom white point and arbitrary gamma could be used
    to
    support more, but I didn't implement that.
    
    Tests for read and write were added.
    
    Signed-off-by: Brecht Van Lommel <brecht@blender.org>
    Signed-off-by: Vlad (Kuzmin) Erium <libalias@gmail.com>
    Signed-off-by: Vlad <shaamaan@gmail.com>

--- testsuite/jxl/run.py
+++ testsuite/jxl/run.py
@@ -10,6 +10,9 @@ command += oiiotool ("../common/tahoe-tiny.tif --iccread ref/test-jxl.icc -o tah
 command += info_command ("tahoe-icc.jxl", safematch=True)
 command += oiiotool ("tahoe-icc.jxl --iccwrite test-jxl.icc")
 
+command += oiiotool ("../common/tahoe-tiny.tif --cicp \"9,16,9,1\" -o tahoe-cicp-pq.jxl")
+command += info_command ("tahoe-cicp-pq.jxl", safematch=True)
+
 outputs = [
             "test-jxl.icc",
             "out.txt"
