a0426d6e6e8edaaf9736acce75992173d71f36df
Larry Gritz <lg@larrygritz.com>
2026-02-01 12:33:10 -0800

    testing: Adjust test comparision thresholds for Mac ARM (#5026)
    Even though we have CI testing on Mac with ARM CPU that were passing,
    after getting a new laptop, I saw some test failures that were due to
    just a few pixels on a few tests needing a higher comparision threshold.
    Results are correct, just different due to the math. I guess this
    machine (CPU? build flags? specific compiler or library versions?) is
    ever so slightly different than the CI Macs, so I caught a few more
    instances that needed to be adjusted.
    
    I tried to increase the thresholds as little as possible to fix the
    problem.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/texture-uint8/run.py
+++ testsuite/texture-uint8/run.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 
+hardfail = 0.021
 
 command = testtex_command ("../common/textures/grid.tx")
 outputs = [ "out.exr" ]
