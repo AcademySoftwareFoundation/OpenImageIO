5ef4870e0d43bc988f83a5fbb27ff59c0c6c95d6
Larry Gritz <lg@larrygritz.com>
2026-02-08 02:04:10 -0800

    fix(tiff): Improve TIFF robustness for non-matching tag/metadata types
    Fixes #5023
    
    This was crashing when writing TIFF information that was supposed to
    be arrays of more than one rational, it was reading past the end of
    a memory array.
    
    But upon looking at it, I realized that for regular ints and floats as
    well, we were only handling scalars properly. And furthermore, there
    were some places where we were dropping metadata on the floor when the
    types didn't exactly match, but that we *could* handle with a little
    conversion.
    
    More specifically, the new cases that we handle are:
    
    * Exif RESOLUTIONUNIT tag is a short, but by convention we store it by
      the name as a string in OIIO metadata, so we need to convert back to
      a code (we did so for the main TIFF metadata, but not for Exif in
      TIFF).
    * Handle Exif "version" and "flashpixversion" metadata which have
      unusual encoding in TIFF files (they are 4-character strings, but
      must be stored in a TIFF tag of type BYTES, not as the usual type
      ASCII that most strings use.
    * Handle things that TIFF insists are ASCII but that come to us as
      metadata that's strings. Easy -- our `ParamValue.get_string()`
      automatically converts ther things like ints or floats into string
      representation.
    * Much more flexibility in automatically converting among the signed
      and unsigned, 16 and 32 bit, integer types when the metadata in our
      ImageSpec is integer but not the specific type of integer that
      TIFF/Exif thinks it should be.
    
    This doesn't appear to change the results of anything in our
    testsuite, but it's possible that some non-TIFF-to-TIFF image
    conversions that contain Exif data may now correctly do certain type
    conversins properly instead of just silently dropping the metadata
    that needed certain type conversions.
    
    Additionally, to do this nicely, I ended up adding a new TypeURational
    alias in typedesc.h (similar to TypeRational, but the case where both
    numerator and denominator are unsigned ints).
    
    And also fixed a random comment typo I noticed in tiffinput.cpp.
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/include/OpenImageIO/typedesc.h
+++ src/include/OpenImageIO/typedesc.h
@@ -385,6 +385,7 @@ inline constexpr TypeDesc TypeHalf (TypeDesc::HALF);
 inline constexpr TypeDesc TypeTimeCode (TypeDesc::UINT, TypeDesc::SCALAR, TypeDesc::TIMECODE, 2);
 inline constexpr TypeDesc TypeKeyCode (TypeDesc::INT, TypeDesc::SCALAR, TypeDesc::KEYCODE, 7);
 inline constexpr TypeDesc TypeRational(TypeDesc::INT, TypeDesc::VEC2, TypeDesc::RATIONAL);
+inline constexpr TypeDesc TypeURational(TypeDesc::UINT, TypeDesc::VEC2, TypeDesc::RATIONAL);
 inline constexpr TypeDesc TypePointer(TypeDesc::PTR);
 inline constexpr TypeDesc TypeUstringhash(TypeDesc::USTRINGHASH);
 
@@ -648,6 +649,7 @@ using v3_1::TypeHalf;
 using v3_1::TypeTimeCode;
 using v3_1::TypeKeyCode;
 using v3_1::TypeRational;
+using v3_1::TypeURational;
 using v3_1::TypePointer;
 using v3_1::TypeUstringhash;
 #endif
