05787adbc21edc955a005a3fd9a01f4e8cf8bcac
Aleksandr Motsjonov <soswow@gmail.com>
2026-03-13 17:59:30 +1100

    add more of ROI support
    Signed-off-by: Aleksandr Motsjonov <soswow@gmail.com>

--- src/python-nanobind/nanobind_experimental.cpp
+++ src/python-nanobind/nanobind_experimental.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/AcademySoftwareFoundation/OpenImageIO
 
+#include <OpenImageIO/imagebuf.h>
 #include <OpenImageIO/imageio.h>
 #include <OpenImageIO/oiioversion.h>
 #include <OpenImageIO/strutil.h>
@@ -27,6 +28,34 @@ bool
 roi_contains_roi(const ROI& roi, const ROI& other)
 {
     return roi.contains(other);
+}
+
+
+ROI
+imagespec_get_roi(const ImageSpec& spec)
+{
+    return get_roi(spec);
+}
+
+
+ROI
+imagespec_get_roi_full(const ImageSpec& spec)
+{
+    return get_roi_full(spec);
+}
+
+
+void
+imagespec_set_roi(ImageSpec& spec, const ROI& roi)
+{
+    set_roi(spec, roi);
+}
+
+
+void
+imagespec_set_roi_full(ImageSpec& spec, const ROI& roi)
+{
+    set_roi_full(spec, roi);
 }
 
 }  // namespace
@@ -66,7 +95,29 @@ NB_MODULE(_nanobind_experimental, m)
         .def(nb::self == nb::self)
         .def(nb::self != nb::self);
 
+    nb::class_<ImageSpec>(m, "ImageSpec")
+        .def(nb::init<>())
+        .def_rw("x", &ImageSpec::x)
+        .def_rw("y", &ImageSpec::y)
+        .def_rw("z", &ImageSpec::z)
+        .def_rw("width", &ImageSpec::width)
+        .def_rw("height", &ImageSpec::height)
+        .def_rw("depth", &ImageSpec::depth)
+        .def_rw("full_x", &ImageSpec::full_x)
+        .def_rw("full_y", &ImageSpec::full_y)
+        .def_rw("full_z", &ImageSpec::full_z)
+        .def_rw("full_width", &ImageSpec::full_width)
+        .def_rw("full_height", &ImageSpec::full_height)
+        .def_rw("full_depth", &ImageSpec::full_depth)
+        .def_rw("nchannels", &ImageSpec::nchannels)
+        .def_prop_ro("roi", &imagespec_get_roi)
+        .def_prop_ro("roi_full", &imagespec_get_roi_full);
+
     m.def("union", &roi_union);
     m.def("intersection", &roi_intersection);
+    m.def("get_roi", &get_roi);
+    m.def("get_roi_full", &get_roi_full);
+    m.def("set_roi", &imagespec_set_roi);
+    m.def("set_roi_full", &imagespec_set_roi_full);
     m.attr("__version__") = OIIO_VERSION_STRING;
 }
