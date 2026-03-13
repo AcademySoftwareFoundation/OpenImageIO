0bc9aff5f36f78d166401d1803ce3e12e50d312f
Aleksandr Motsjonov <soswow@gmail.com>
2026-03-13 17:59:30 +1100

    add more of ROI support
    Signed-off-by: Aleksandr Motsjonov <soswow@gmail.com>

--- testsuite/python-nanobind-experimental/src/test_nanobind_experimental.py
+++ testsuite/python-nanobind-experimental/src/test_nanobind_experimental.py
@@ -38,6 +38,28 @@ def main() -> int:
     print("intersection:", nbexp.intersection(roi, other))
     print("ROI.All defined:", nbexp.ROI.All.defined)
     print("copy equals:", roi.copy() == roi)
+
+    spec = nbexp.ImageSpec()
+    spec.x = 10
+    spec.y = 20
+    spec.z = 0
+    spec.width = 30
+    spec.height = 40
+    spec.depth = 1
+    spec.nchannels = 3
+    spec.full_x = 8
+    spec.full_y = 18
+    spec.full_z = 0
+    spec.full_width = 34
+    spec.full_height = 44
+    spec.full_depth = 1
+
+    print("spec roi:", nbexp.get_roi(spec))
+    print("spec roi_full:", nbexp.get_roi_full(spec))
+    nbexp.set_roi(spec, nbexp.ROI(12, 16, 22, 26, 0, 1, 0, 2))
+    nbexp.set_roi_full(spec, nbexp.ROI(7, 19, 17, 29, 0, 1, 0, 4))
+    print("updated roi:", nbexp.get_roi(spec))
+    print("updated roi_full:", nbexp.get_roi_full(spec))
     return 0
 
 
