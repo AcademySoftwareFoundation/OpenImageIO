ac8b61d36d9ef25a5cbfcc20c074f274862df570
Larry Gritz <lg@larrygritz.com>
2026-03-30 19:28:26 -0700

    ci: update container versions, move 3 variants from 2022 to Ubuntu (#5119)
    TL;DR:
    * Bump ASWF containers to the latest versions for their years.
    * Move the "oldest" and "hobbled" tests off the 2022 containers
    entirely, do as close as possible to equivalent on Unbuntu.
    
    Detailed explanation follows:
    
    First of all, bump to the latest ASWF docker containers for each year.
    We had been locking down, but hadn't bumped to the latest for a while.
    (Exception: leave the sanitizer test where it is -- the newer containers
    have additional checks that are catching UB that I am tracking down
    separately.)
    
    Also, we are trying to reduce the surface area of the CentOS 7-based
    2022 aswf-docker containers, which are getting long in the tooth, cannot
    upgrade to the latest "actions" (because of a tricky interplay between
    runner configuration, node version the actions require, and glibc on the
    old containers), and may have a limited time that they will continue
    working.
    
    In most film studios, CentOS is long gone or taking its last breaths, so
    being able to support and test on 2022 era dependencies and OS distros
    is of dwindling relevance. We do, however, want to keep testing versus
    the oldest supported versions we can muster for many major dependencies.
    
    So to this end, this PR also does the following:
    
    * Move three tests from running in the 2022 containers to just running
    on bare Ubutu and by hook or by crook using the right dependencies:
    - the "oldest" versions we claim to support for as much of the toolchain
    toolchain and dependencies as is easily achievable, built with gcc
      - and the same, built with clang
    - the "hobbled" test, which is similar to the "oldest" test but with as
    many of the optional dependencies missing or disabled, to make sure we
    really do build with only required dependencies (this also is the test
    build for no SIMD use)
    * Keep two last tests running on 2022 containers, these are only for the
    purpose of testing "pure" CentOS VFX Platform 2022 compliance for just a
    bit longer.
    * A few minor related touch-ups necessary to force some older
    dependencies on the bare Ubuntu runner.
    
    ---------
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/cmake/build_TIFF.cmake
+++ src/cmake/build_TIFF.cmake
@@ -9,7 +9,8 @@
 set_cache (TIFF_BUILD_VERSION 4.7.1 "TIFF version for local builds")
 set (TIFF_GIT_REPOSITORY "https://gitlab.com/libtiff/libtiff.git")
 set_cache (TIFF_GIT_TAG "v${TIFF_BUILD_VERSION}" "Git branch or tag")
-set (TIFF_GIT_COMMIT "5fe20d0e9aba49a6a350ed533459d1505203838f")
+set_cache (TIFF_GIT_COMMIT "5fe20d0e9aba49a6a350ed533459d1505203838f"
+           "commit hash to verify tag against")
 set_cache (TIFF_BUILD_SHARED_LIBS  ${LOCAL_BUILD_SHARED_LIBS_DEFAULT}
            "Should a local TIFF build, if necessary, build shared libraries" ADVANCED)
 
@@ -54,6 +55,7 @@ build_dependency_with_cmake(TIFF
 
 set (TIFF_ROOT ${TIFF_LOCAL_INSTALL_DIR})
 set (TIFF_DIR ${TIFF_LOCAL_INSTALL_DIR})
+set (TIFF_VERSION ${TIFF_BUILD_VERSION})
 
 # Signal to caller that we need to find again at the installed location
 # set (TIFF_REFIND TRUE)
