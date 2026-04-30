4c8aa6bf051787f52acaed9b853a9f49244e4f22
Larry Gritz <lg@larrygritz.com>
2026-04-29 17:25:05 -0700

    fix(rla): immediate fix for PR #5153 error (#5172)
    Caught in review, but I didn't see the comment before I merged.
    
    ---------
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- src/rla.imageio/rlainput.cpp
+++ src/rla.imageio/rlainput.cpp
@@ -509,9 +509,9 @@ RLAInput::decode_rle_span(span<unsigned char> buf, int n, int stride,
             ++e;
         } else {
             // run count negative: repeat bytes literally
-            if (count > n)
-                break;       // asking for a count that will overrun the buffer
             count = -count;  // make it positive
+            if (count > n)
+                break;  // asking for a count that will overrun the buffer
             for (; count && n > 0 && e < elen; --count, b += stride, --n)
                 buf[b] = encoded[e++];
         }
