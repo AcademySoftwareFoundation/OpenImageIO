3e2de76dcccea682c495ca10d9ecc6ecfb8cf963
Larry Gritz <lg@larrygritz.com>
2026-06-20 12:34:55 -0700

    fix(cineon): more robust to invalid numbers of channels & bit depth
    Guard against corrupted images, which if they said they had more than
    the 8 channels allowed by Cineon format, would overrun array bounds
    while reading the file. And check for valid bit depths.
    
    Also opportunistically fix a bug I spotted where a red channel was
    incrementing the count of greyscale channels rather than the count of
    red channels -- which must have been a copy and paste error from
    literally 15 years ago. (But those counters are only used for channel
    naming, and only if there is more than one greyscale or red channel in
    the file. So I doubt anybody has ever hit this bug, or ever would have
    in the future.)
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>

--- testsuite/cineon/run.py
+++ testsuite/cineon/run.py
@@ -4,7 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 # https://github.com/AcademySoftwareFoundation/OpenImageIO
 
+# save the error output
+redirect = " >> out.txt 2>&1 "
 
 files = [ "checker.cin" ]
 for f in files:
     command += info_command ("../oiio-images/cineon/" + f)
+
+
+# Regression tests for broken files
+command += info_command ("src/broken_bitdepth.cin", verbose=False, failureok=True)
+
+outputs = [ "out.txt" ]
