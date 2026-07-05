c8f2c70b54ec6ed967c94224a8df20e0103afc16
Aleksandr Motsjonov <soswow@users.noreply.github.com>
2026-07-05 11:32:26 +1000

    python/testing: Improve python test coverage (#5260)
    While working on
    https://github.com/AcademySoftwareFoundation/OpenImageIO/pull/5254,
    where we're introducing changes that could surface bugs in pybind11 code
    that's in production today, I figured I'd try adding some tests.
    
    This was a fairly loose-handed effort. To be upfront: most of it was
    LLM-generated. I did some research into which modules were lacking
    coverage the most, and those are what this PR targets. I haven't gone
    through every line of the generated code.
    
    I hate being asked to review something that was clearly vibe-coded with
    no real effort put in, so I'm saying that here rather than pretending
    otherwise. I can try to review it properly, but without deep codebase
    knowledge it'll be slow and painful. Since these are just tests, my
    thinking was: even if there are silly bits here and there, it shouldn't
    hurt much, as long as things go green and we cover more logic and
    branches. Worth offering as-is.
    
    ---------
    
    Signed-off-by: Aleksandr Motsjonov <soswow@gmail.com>

---
+++ testsuite/python-oiio/run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Copyright Contributors to the OpenImageIO project.
+# SPDX-License-Identifier: Apache-2.0
+# https://github.com/AcademySoftwareFoundation/OpenImageIO
+
+
+command += pythonbin + " src/test_oiio.py > out.txt"
