2de466b4e12bcc35f8ab9542fc9583407676d7ba
Larry Gritz <lg@larrygritz.com>
2026-08-05 14:59:08 -0700

    int: improve ImageSpec::default_channel_names -- nchannels safety (#5368)
    Since ImageSpec::default_channel_names does an allocation
    (`channelnames.reserve(nchannels)`), it seems prudent to do a validity
    check on nchannels first.
    
    This is similar in spirit to check_open, but of course this happens
    during construction of the ImageSpec, whereas check_open can't do its
    magic until after the ImageSpec already exists and has done this
    allocation.
    
    I also took the opportunity to change a change a push_back to an
    emplace_back, possibly saving an occasional std::string allocation.
    
    ---------
    
    Signed-off-by: Larry Gritz <lg@larrygritz.com>
    Co-authored-by: Jinnie Kim <jinhgkim@gmail.com>

--- src/libOpenImageIO/formatspec.cpp
+++ src/libOpenImageIO/formatspec.cpp
@@ -196,9 +196,11 @@ void
 ImageSpec::default_channel_names() noexcept
 {
     channelnames.clear();
-    channelnames.reserve(nchannels);
     alpha_channel = -1;
     z_channel     = -1;
+    if (nchannels < 1 || (limit_channels && nchannels > limit_channels))
+        return;  // early out for invalid channel counts
+    channelnames.reserve(nchannels);
     if (nchannels == 1) {  // Special case: 1-channel is named "Y"
         channelnames.emplace_back("Y");
         return;
@@ -215,7 +217,7 @@ ImageSpec::default_channel_names() noexcept
         alpha_channel = 3;
     }
     for (int c = 4; c < nchannels; ++c)
-        channelnames.push_back(Strutil::fmt::format("channel{}", c));
+        channelnames.emplace_back(Strutil::fmt::format("channel{}", c));
 }
 
 
